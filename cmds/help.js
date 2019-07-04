556241748093805ab51c15ec19723f3860a58520
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-04 15:23:30 +0200

    --- Auto-Git Commit ---

--- cmds/help.js
+++ cmds/help.js
@@ -14,7 +14,7 @@ module.exports.run = async (gideon, message, args) => {
         .addField('!batwoman', 'fetches the latest EP from arrowverse.info (coming soon)')
         .addField('!constantine', 'fetches the latest EP from arrowverse.info (coming soon)')
         .addField('Gideon, show me the future!', 'displays an easter egg')
-    	.setTimestamp()
+    	.setTimestamp(`\n\n**[Click here to read the full article](https://arrow.fandom.com${type.url} 'https://arrow.fandom.com${type.url}')**`)
     	.setFooter('Gideon - The Arrowverse Bot | Developed by adrifcastr', 'https://i.imgur.com/3RihwQS.png');
 
         message.channel.send(help);
