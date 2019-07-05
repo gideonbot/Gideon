d8b5a2ff7f3e92bf01ee182bae697ff2c8848ae9
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-05 12:34:12 +0200

    --- Auto-Git Commit ---

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ gideon.on('message', async message => {
     let messageArray = message.content.split(" ");
     let command = messageArray[0];
     let args = messageArray.slice(1);
-    const eaegg = `Gideon, show me the future!`
-    let cmd = gideon.commands.get(command.slice(prefix.length) && message.content.slice(thingy.length).trim().split(" "));
+
+    let cmd = gideon.commands.get(command.slice(prefix.length));
     if(cmd) cmd.run(gideon, message, args);
 
     if (message.content === `Gideon, show me the future!`) {
