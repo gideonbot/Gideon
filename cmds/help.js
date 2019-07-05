aaa245181054be3235587c8c05fd0ed93b7c55f2
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-05 12:48:59 +0200

    --- Auto-Git Commit ---

--- cmds/help.js
+++ cmds/help.js
@@ -3,7 +3,7 @@ const Discord = module.require("discord.js");
 
 module.exports.run = async (gideon, message, args) => {
     const fsurl = 'https://discordapp.com/channels/595318490240385037/595935089070833708';
-    
+
     const help = new Discord.RichEmbed()
 	    .setColor('#2791D3')
 	    .setTitle('__You can check the list of available commands below:__')
@@ -17,7 +17,7 @@ module.exports.run = async (gideon, message, args) => {
         .addField('!constantine', 'fetches the latest EP from arrowverse.info (coming soon)')
         .addField('Gideon, show me the future!', 'displays an easter egg')
         .addField('!donate', 'displays info to support maintainance and hosting of Gideon')
-        .addField(`\n\n**[Click here to suggest a feature](${message.guild.channels.find("name", "feature-suggestions")})**`)
+        .addField(`\n\n**[Click here to suggest a feature](${fs})**`)
         .setTimestamp()
     	.setFooter('Gideon - The Arrowverse Bot | Developed by adrifcastr', 'https://i.imgur.com/3RihwQS.png');
 
