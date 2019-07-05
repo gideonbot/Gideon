40bd7c39513d91466c289731e65a3d3acdff1cd3
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-05 12:22:04 +0200

    --- Auto-Git Commit ---

--- cmds/future.js
+++ cmds/future.js
@@ -1,5 +1,5 @@
 const Discord = module.require("discord.js");
-const eaeggtr = message.content.slice(eaegg.length).trim().split(" "));
+const eaeggtr = message.content.slice(eaegg.length).trim().split(" ");
 
 module.exports.run = async (gideon, message, args) => {
 
