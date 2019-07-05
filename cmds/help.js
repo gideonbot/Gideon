5596eb81c7419065a8e3cbc44c05a18dc891ad0a
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-05 12:54:30 +0200

    --- Auto-Git Commit ---

--- cmds/help.js
+++ cmds/help.js
@@ -17,6 +17,7 @@ module.exports.run = async (gideon, message, args) => {
         .addField('!constantine', 'fetches the latest EP from arrowverse.info (coming soon)')
         .addField('Gideon, show me the future!', 'displays an easter egg')
         .addField('!donate', 'displays info to support maintainance and hosting of Gideon')
+        .addField()
         .addField(`\n\n**[Click here to suggest a feature](${fsurl})**`)
         .setTimestamp()
     	.setFooter('Gideon - The Arrowverse Bot | Developed by adrifcastr', 'https://i.imgur.com/3RihwQS.png');
