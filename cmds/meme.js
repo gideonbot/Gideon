10601300d120f0941676c6e0730849030ee94ce4
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-06 22:35:10 +0200

    --- Auto-Git Commit ---

--- cmds/meme.js
+++ cmds/meme.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 module.exports.run = async (gideon, message, args) => {  
      __parentDir = path.dirname(module.parent.filename);
-    fs.readFile(__parentDir + '/avm');
+    
     var files = fs.readFile(path.join(__dirname, '../', 'avm'));
     let chosenFile = files[Math.floor(Math.random() * files.length)]
 
