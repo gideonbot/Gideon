cd5503308636677f2c177938c7bdbee4d65d3c66
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-06 23:27:15 +0200

    --- Auto-Git Commit ---

--- cmds/meme.js
+++ cmds/meme.js
@@ -11,7 +11,7 @@ module.exports.run = async (gideon, message, args) => {
     const meme = new Discord.RichEmbed()
 	    .setColor('#2791D3')
 	    .attachFiles([chosenFile])
-	    .setImage(`attachment://$`)
+	    .setImage(`attachment://${chosenFile}`)
     	.setTimestamp()
     	.setFooter('Gideon - The Arrowverse Bot | Developed by adrifcastr', 'https://i.imgur.com/3RihwQS.png');
 
