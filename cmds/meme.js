8c991acae2737e4ef013ae2cdaf22395a70b61d2
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-07 00:01:42 +0200

    --- Auto-Git Commit ---

--- cmds/meme.js
+++ cmds/meme.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 module.exports.run = async (gideon, message, args) => {  
     avmpath = path.join, __dirname + '/avm';
-    var files = fs.readdirSync(__parentDir + '/avm/');
+    var files = fs.readdirSync(av;
     let chosenFile = files[Math.floor(Math.random() * files.length)]
 
     const meme = new Discord.RichEmbed()
