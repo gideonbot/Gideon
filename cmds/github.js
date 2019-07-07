712f4f819158c42fac34733f42b78b01e2d974f7
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-07 15:36:30 +0200

    --- Auto-Git Commit ---

--- cmds/github.js
+++ cmds/github.js
@@ -7,7 +7,9 @@ module.exports.run = async (gideon, message, args) => {
     snekfetch.get(api).then(r => {
         console.log(r.body);
         let body = r.body;   
-             
+        SimpleDateFormat parser = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX");
+        parser.setTimeZone(TimeZone.getTimeZone("UTC"));
+        Date parsed = parser.parse(utcDateString);     
         const github = new Discord.RichEmbed()
 	    .setColor('#2791D3')
 	    .setTitle(body.name)
