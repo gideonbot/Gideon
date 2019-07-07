41e5bc57559bd48f6f6731831685ea036e7ee1f6
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-07 14:31:02 +0200

    --- Auto-Git Commit ---

--- cmds/github.js
+++ cmds/github.js
@@ -7,12 +7,12 @@ module.exports.run = async (gideon, message, args) => {
     snekfetch.get(api).then(r => {
         console.log(r.body);
         let body = r.body;   
-        const type = Object.values(body)[0];
+        //const type = Object.values(body)[1];
              
         const github = new Discord.RichEmbed()
 	    .setColor('#2791D3')
 	    .setTitle(body.name)
-        .setDescription(type.description)
+        //.setDescription(type.description)
 	    .setThumbnail()
     	.setTimestamp()
     	.setFooter('Gideon - The Arrowverse Bot | Developed by adrifcastr', 'https://i.imgur.com/3RihwQS.png');
