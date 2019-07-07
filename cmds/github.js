18d6a52a58449017ac416762c6143ca22b9b2919
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-07 15:12:02 +0200

    --- Auto-Git Commit ---

--- cmds/github.js
+++ cmds/github.js
@@ -11,7 +11,7 @@ module.exports.run = async (gideon, message, args) => {
         const github = new Discord.RichEmbed()
 	    .setColor('#2791D3')
 	    .setTitle(body.name)
-        .setDescription(body.description + `\nOwner: [adrifcastr](${body.owner.html_url} '${body.owner.html_url}') 
+        .setDescription(body.description + `\n\nOwner: [adrifcastr](${body.owner.html_url} '${body.owner.html_url}') 
                                             Repo: [Gideon](${body.html_url} '${body.html_url}')`)
 	    .setThumbnail(body.owner.avatar_url)
     	.setTimestamp()
