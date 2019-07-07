f446d3871a3c4fde62639026d1d84d5dc9f10710
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-07 14:08:21 +0200

    --- Auto-Git Commit ---

--- cmds/github.js
+++ cmds/github.js
@@ -11,7 +11,7 @@ module.exports.run = async (gideon, message, args) => {
              
         const github = new Discord.RichEmbed()
 	    .setColor('#2791D3')
-	    .setTitle(b)
+	    .setTitle(body.name)
         .setDescription()
 	    .setThumbnail()
     	.setTimestamp()
