1e4e409911a1e6fa74979283207be4e3cbb2dcb9
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-07 16:13:17 +0200

    --- Auto-Git Commit ---

--- cmds/github.js
+++ cmds/github.js
@@ -18,7 +18,7 @@ module.exports.run = async (gideon, message, args) => {
                                             Language: \`${body.language}\`
                                             Last Update: \`${upDate.toUTCString()}\`
                                             Open Issues: \`${body.open_issues_count}\`
-                                            Stargazers: \`${body.open_issues_count}\` Watchers: \`${body.open_issues_count}\` Forks: \`${body.open_issues_count}\`
+                                            Stargazers: \`${body.stargazers_count}\` Watchers: \`${body.open_issues_count}\` Forks: \`${body.open_issues_count}\`
                                             Git Clone: \`${body.clone_url}\``)
 	    .setThumbnail(body.owner.avatar_url)
     	.setTimestamp()
