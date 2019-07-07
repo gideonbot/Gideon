6f9c71f9c331a66eb39ec8a41286f7a4667ba5b2
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-07 15:41:18 +0200

    --- Auto-Git Commit ---

--- cmds/github.js
+++ cmds/github.js
@@ -7,7 +7,7 @@ module.exports.run = async (gideon, message, args) => {
     snekfetch.get(api).then(r => {
         console.log(r.body);
         let body = r.body;   
-        gitupdate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX");
+        gitupdate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
         parser.setTimeZone(TimeZone.getTimeZone("UTC"));
         Date gitupdate = parser.parse(utcDateString);
              
