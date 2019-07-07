cc4b749d39db8e98567dea2b322015966cd5cc18
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-07 15:09:19 +0200

    --- Auto-Git Commit ---

--- cmds/github.js
+++ cmds/github.js
@@ -12,7 +12,7 @@ module.exports.run = async (gideon, message, args) => {
 	    .setColor('#2791D3')
 	    .setTitle(body.name)
         .setDescription(body.description + `\nOwner: [adrifcastr](${body.owner.html_url} '${body.owner.html_url}') 
-                                            \nRepo: [adrifcastr](${body.html_url} '${body.html_url}')`)
+                                            \nRepo: [Gideon](${body.html_url} '${body.html_url}')`)
 	    .setThumbnail(body.owner.avatar_url)
     	.setTimestamp()
     	.setFooter('Gideon - The Arrowverse Bot | Developed by adrifcastr', 'https://i.imgur.com/3RihwQS.png');
