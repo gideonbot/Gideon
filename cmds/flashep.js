b52802266c9c5b55f4345f99d1ecf07424b78256
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-08 13:30:50 +0200

    --- Auto-Git Commit ---

--- cmds/flashep.js
+++ cmds/flashep.js
@@ -3,7 +3,7 @@ const snekfetch = require("snekfetch");
 
 module.exports.run = async (gideon, message, args) => {
     let season = str.substr(0, 1);
-let episode = str.substr(2, 2);
+    let episode = str.substr(2, 2);
     const api = `http://api.tvmaze.com/shows/13/episodebynumber?season=${season}&number=${episode}`;
     let sen = args[0];
     if(!sen) return message.channel.send("You must supply the season and episode number!");
