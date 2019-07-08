e3192e0a09b23626b05162a49b702e049655de45
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-08 12:08:10 +0200

    --- Auto-Git Commit ---

--- cmds/flashep.js
+++ cmds/flashep.js
@@ -10,7 +10,7 @@ module.exports.run = async (gideon, message, args) => {
         .setTimestamp()
         .setFooter('Gideon - The Arrowverse Bot | Developed by adrifcastr', 'https://i.imgur.com/3RihwQS.png');
 
-        message.channel.send(meme);
+        message.channel.send(flashep);
 }
 
 module.exports.help = {
