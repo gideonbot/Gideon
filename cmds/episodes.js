916aafeb48d3f1c26a8f1bd045ac94ca744d4516
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-08 16:28:17 +0200

    --- Auto-Git Commit ---

--- cmds/episodes.js
+++ cmds/episodes.js
@@ -16,13 +16,20 @@ module.exports.run = async (gideon, message, args) => {
         let sum = body.summary.substring(3);
         let desc = sum.substring(0, sum.length - 4);
         //if (!Object.keys(body.id).length) message.channel.send(`There was no data for this episode!`).catch(console.error);  
-
-        let cstdate = new Date( airdate.getTime() + (airdate.getTimezoneOffset() * 70000));
+        
+        let localTime = airdate.getTime();
+        let localOffset = airdate.getTimezoneOffset() * 60000;  
+        let utc = localTime + localOffset;
+        let offset = -5;   
+        let cst = utc + (3600000*offset);
+        let nd = new Date(cst); 
+        let convdate = (nd.toLocaleString());  
+        let newdate = new Date( airdate.getTime() + (airdate.getTimezoneOffset() * 60000));
 
         const flashep = new Discord.RichEmbed()
         .setColor('#2791D3')
         .setTitle(`The Flash ${body.season}x${body.number<10?"0"+body.number:body.number} - ${body.name}`)
-        .setDescription(desc + `\n\nAirdate: \`${cstdate + ' CST'}\` \nRuntime: \`${body.runtime} Minutes\``)
+        .setDescription(desc + `\n\nAirdate: \`${newdate + ' CST'}\` \nRuntime: \`${body.runtime} Minutes\``)
         .setImage(body.image.original)     
         .setTimestamp()
         .setFooter('Gideon - The Arrowverse Bot | Developed by adrifcastr', 'https://i.imgur.com/3RihwQS.png');
