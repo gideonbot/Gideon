d7eea820033d2e6ddb121604da3757ce9a561925
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-08 13:17:48 +0200

    --- Auto-Git Commit ---

--- cmds/flashep.js
+++ cmds/flashep.js
@@ -13,7 +13,7 @@ module.exports.run = async (gideon, message, args) => {
 
         const flashep = new Discord.RichEmbed()
         .setColor('#2791D3')
-        .setTitle(`The Flash ${body.season}x${body.number.toFixed()} - ${body.name}`)
+        .setTitle(`The Flash ${body.season}x${body.number.padStart(2, '0'))} - ${body.name}`)
         .setDescription(body.summary + `\n\nAirdate: \`${airdate.toUTCString()}\` \nRuntime: \`${body.runtime} Minutes\``)
         .setImage(body.original)     
         .setTimestamp()
