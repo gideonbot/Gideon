7c7a7806e8a5e7f9acd442a083c49a65382701e9
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-08 14:14:58 +0200

    --- Auto-Git Commit ---

--- cmds/flashep.js
+++ cmds/flashep.js
@@ -18,7 +18,7 @@ module.exports.run = async (gideon, message, args) => {
         .setColor('#2791D3')
         .setTitle(`The Flash ${body.season}x${body.number<10?"0"+body.number:body.number} - ${body.name}`)
         .setDescription(body.summary + `\n\nAirdate: \`${airdate.toUTCString()}\` \nRuntime: \`${body.runtime} Minutes\``)
-        .setImage(body.original)     
+        .setThumbnail(body.original)     
         .setTimestamp()
         .setFooter('Gideon - The Arrowverse Bot | Developed by adrifcastr', 'https://i.imgur.com/3RihwQS.png');
 
