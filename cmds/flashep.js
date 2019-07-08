0c2df4cda875f493eab8d1decea2f8310819bc25
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-08 13:56:18 +0200

    --- Auto-Git Commit ---

--- cmds/flashep.js
+++ cmds/flashep.js
@@ -2,7 +2,7 @@ const Discord = module.require("discord.js");
 const snekfetch = require("snekfetch");
 
 module.exports.run = async (gideon, message, args) => {
-    let inputargs.substr(-4)
+    let input = args.substr(-4)
     let season = args[0];
     let episode = args[2] + args[3];
     const api = `http://api.tvmaze.com/shows/13/episodebynumber?season=${season}&number=${episode}`;
