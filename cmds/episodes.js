97f88739c33b26ad8bd2123bef6622b903bf5658
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-08 15:57:03 +0200

    --- Auto-Git Commit ---

--- cmds/episodes.js
+++ cmds/episodes.js
@@ -17,7 +17,7 @@ module.exports.run = async (gideon, message, args) => {
         let desc = sum.substring(0, sum.length - 4);
         //if (!Object.keys(body.id).length) message.channel.send(`There was no data for this episode!`).catch(console.error);  
         
-        localTime = d.getTime();
+        localTime = airdate.getTime();
 
         localOffset = d.getTimezoneOffset() * 60000;
     
