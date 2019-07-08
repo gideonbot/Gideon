f847913067777e2363490e4c384a89471f570214
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-08 23:39:53 +0200

    --- Auto-Git Commit ---

--- cmds/episodes.js
+++ cmds/episodes.js
@@ -41,6 +41,7 @@ module.exports.run = async (gideon, message, args) => {
         let body = r.body;   
         let airdate = new Date(body.airstamp);
         let desc = '';
+        
         if (r.body.summary === null){
             desc = 'No summary available'
         }   else {
