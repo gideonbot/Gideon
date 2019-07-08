7414d50b69b3709528d85ab6dfbba8f80085bd9c
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-08 18:38:41 +0200

    --- Auto-Git Commit ---

--- cmds/episodes.js
+++ cmds/episodes.js
@@ -5,11 +5,12 @@ module.exports.run = async (gideon, message, args) => {
     let seip = args.toString().substr(-4)
     let season = seip[0];
     let episode = seip[2] + seip[3];
-    let fl
+    let fl = ""
     let ar
     let sg
     let lo
     let ct
+    let bw
     let show = args.toString().substring(4, args.length-5)
     if (show.includes(substring))
     let showid;
