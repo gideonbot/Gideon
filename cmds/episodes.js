7346fa474ed5476b31081911d1d9324ed1eee4a1
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-08 16:55:48 +0200

    --- Auto-Git Commit ---

--- cmds/episodes.js
+++ cmds/episodes.js
@@ -6,6 +6,7 @@ module.exports.run = async (gideon, message, args) => {
     let season = seip[0];
     let episode = seip[2] + seip[3];
     let show = args.toString().substring(4, args.length-5)
+    
     const api = `http://api.tvmaze.com/shows/13/episodebynumber?season=${season}&number=${episode}`;
     let sen = args[0];
     if(!sen) return message.channel.send("You must supply the shows name, season and its episode number!");
