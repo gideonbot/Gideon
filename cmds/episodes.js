ae58e962ebd37cc8106dbe05c8a253eb371cfc3e
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-08 21:46:41 +0200

    --- Auto-Git Commit ---

--- cmds/episodes.js
+++ cmds/episodes.js
@@ -56,7 +56,7 @@ module.exports.run = async (gideon, message, args) => {
         const epinfo = new Discord.RichEmbed()
         .setColor('#2791D3')
         .setTitle(`${showtitle} ${body.season}x${body.number<10?"0"+body.number:body.number} - ${body.name}`)
-        .setDescription(desc + `\n\nAirdate: \`${cstdate}\`\nAirtime: \`${csttime + ' CST'}\`\nRuntime: \`${body.runtime} Minutes\`\n\n**[Click here to read the full article](https://arrow.fandom.com${url} 'https://arrow.fandom.com${url}')**)`)
+        .setDescription(desc + `\n\nAirdate: \`${cstdate}\`\nAirtime: \`${csttime + ' CST'}\`\nRuntime: \`${body.runtime} Minutes\`\n\n**[Click here to read the full article](https://arrow.fandom.com${url} 'https://arrow.fandom.com${url}')**`)
         .setImage(body.image.original)     
         .setTimestamp()
         .setFooter('Gideon - The Arrowverse Bot | Developed by adrifcastr', 'https://i.imgur.com/3RihwQS.png');
