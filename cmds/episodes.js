c97d466218e998874a63d333b538f03b3cfa0b20
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-08 15:45:37 +0200

    --- Auto-Git Commit ---

--- cmds/episodes.js
+++ cmds/episodes.js
@@ -15,12 +15,12 @@ module.exports.run = async (gideon, message, args) => {
         let airdate = new Date(body.airstamp);
         let sum = body.summary.substring(3);
         let desc = sum.substring(0, sum.length - 4);
-        if (!Object.keys(body).length) message.channel.send(`There was no data for this episode!`).catch(console.error);  
+        if (!Object.keys(body.id).length) message.channel.send(`There was no data for this episode!`).catch(console.error);  
          
         const flashep = new Discord.RichEmbed()
         .setColor('#2791D3')
         .setTitle(`The Flash ${body.season}x${body.number<10?"0"+body.number:body.number} - ${body.name}`)
-        .setDescription(desc + `\n\nAirdate: \`${airdate.toLocaleString()}\` \nRuntime: \`${body.runtime} Minutes\``)
+        .setDescription(desc + `\n\nAirdate: \`${airdate.toUTCString().toLocaleString()}\` \nRuntime: \`${body.runtime} Minutes\``)
         .setImage(body.image.original)     
         .setTimestamp()
         .setFooter('Gideon - The Arrowverse Bot | Developed by adrifcastr', 'https://i.imgur.com/3RihwQS.png');
