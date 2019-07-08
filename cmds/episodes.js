c3ffd40264f8df7fda83e8b2ee2f768dbcee6dac
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-08 23:11:02 +0200

    --- Auto-Git Commit ---

--- cmds/episodes.js
+++ cmds/episodes.js
@@ -44,7 +44,7 @@ module.exports.run = async (gideon, message, args) => {
         let desc = sum.substring(0, sum.length -4);
         if(desc == null){
             desc = "No description available"
-            
+            .catch(console.)
         }
 
         let localTime = airdate.getTime();
