f28bceef5667b3742506f97c4c256c0333226932
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-08 16:25:35 +0200

    --- Auto-Git Commit ---

--- cmds/episodes.js
+++ cmds/episodes.js
@@ -17,7 +17,7 @@ module.exports.run = async (gideon, message, args) => {
         let desc = sum.substring(0, sum.length - 4);
         //if (!Object.keys(body.id).length) message.channel.send(`There was no data for this episode!`).catch(console.error);  
 
-        let cstdate = new Date( airdate.getTime() + (airdate.getTimezoneOffset() * 60000));
+        let cstdate = new Date( airdate.getTime() + (airdate.getTimezoneOffset() * 70000));
 
         const flashep = new Discord.RichEmbed()
         .setColor('#2791D3')
