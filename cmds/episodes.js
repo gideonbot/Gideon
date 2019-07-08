742d70b109f2599aa5c1c2720fa9ee3cf6cc01c9
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-08 16:05:30 +0200

    --- Auto-Git Commit ---

--- cmds/episodes.js
+++ cmds/episodes.js
@@ -27,7 +27,7 @@ module.exports.run = async (gideon, message, args) => {
 
         // Now we can access our time at date[1], and monthdayyear @ date[0]
         let time = newdate[1];
-        var mdy = date[0];
+        var mdy = newdate[0];
 
         // We then parse  the mdy into parts
         mdy = mdy.split('/');
