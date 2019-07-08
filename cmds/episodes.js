1c780b63414ed643566f41878ada15ab7c8259cc
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-08 18:50:40 +0200

    --- Auto-Git Commit ---

--- cmds/episodes.js
+++ cmds/episodes.js
@@ -17,7 +17,9 @@ module.exports.run = async (gideon, message, args) => {
         showid = "";
     }   else if(show.includes(lg)){
         showid = ""
-    }   else if(show.includes(ct)){}
+    }   else if(show.includes(ct)){
+        showid
+    }
     
 
     const api = `http://api.tvmaze.com/shows/13/episodebynumber?season=${season}&number=${episode}`;
