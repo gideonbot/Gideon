74c570d924c422bf8a1e9eed19b506b08768ec33
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-08 14:19:05 +0200

    --- Auto-Git Commit ---

--- cmds/flashep.js
+++ cmds/flashep.js
@@ -14,7 +14,7 @@ module.exports.run = async (gideon, message, args) => {
         let body = r.body;   
         let airdate = new Date(body.airstamp);
         let sums = body.summary.substring(3)
-        let sme
+        let sume = sums
 
         const flashep = new Discord.RichEmbed()
         .setColor('#2791D3')
