0342e48128f1ab8bd41a5711df1177973a91d81c
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-08 12:39:46 +0200

    --- Auto-Git Commit ---

--- cmds/flashep.js
+++ cmds/flashep.js
@@ -7,7 +7,7 @@ module.exports.run = async (gideon, message, args) => {
         
       })  
       const flashep = new Discord.RichEmbed()
-        .setTitle(sh.name)
+        .setTitle(tvmaze.name)
         //.setTitle(`The Flash ${result.season}x${result.number} - ${result.name}`)
         .setColor('#2791D3')
         .setTimestamp()
