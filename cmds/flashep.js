b67bfb2cf92afb2aa2ce450428b468c5291d8e17
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-08 12:23:14 +0200

    --- Auto-Git Commit ---

--- cmds/flashep.js
+++ cmds/flashep.js
@@ -6,8 +6,6 @@ module.exports.run = async (gideon, message, args) => {
         // code
         const flashep = new Discord.RichEmbed()
         .setColor('#2791D3')
-        .attachFile(attachment)
-        .setImage(`attachment://${chosenFile}`)
         .setTimestamp()
         .setFooter('Gideon - The Arrowverse Bot | Developed by adrifcastr', 'https://i.imgur.com/3RihwQS.png');
 
