8fe58ed4e2ed65b79b8ff8f98d5b0e33d1c1699c
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-08 23:29:16 +0200

    --- Auto-Git Commit ---

--- cmds/episodes.js
+++ cmds/episodes.js
@@ -40,7 +40,7 @@ module.exports.run = async (gideon, message, args) => {
         console.log(r.body);
         let body = r.body;   
         let airdate = new Date(body.airstamp);
-        let sum = body.summary.substring(3).catch(() => { body.summary = null;;
+        let sum = body.summary.substring(3)
         let desc = sum.substring(0, sum.length -4);           
 
         let localTime = airdate.getTime();
