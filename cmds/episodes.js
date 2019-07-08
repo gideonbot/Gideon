675af6bf3b4c50589b2e2ae7091a337e0e82446b
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-08 23:33:11 +0200

    --- Auto-Git Commit ---

--- cmds/episodes.js
+++ cmds/episodes.js
@@ -43,10 +43,9 @@ module.exports.run = async (gideon, message, args) => {
         if (r.body.summary === null){
 
         }   else {
-            
+
         }
-        let sum = body.summary.substring(3)
-        let desc = sum.substring(0, sum.length -4);           
+                  
 
         let localTime = airdate.getTime();
         let localOffset = airdate.getTimezoneOffset("America/New_York") * 60000;  
