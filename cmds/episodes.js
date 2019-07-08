aae730551464c1357061efe0669d89e98e27f622
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-08 14:38:30 +0200

    --- Auto-Git Commit ---

--- cmds/episodes.js
+++ cmds/episodes.js
@@ -5,7 +5,7 @@ module.exports.run = async (gideon, message, args) => {
     let input = args.toString();
     let inpstr = input.substr(-4)
     console.log(input)
-    let season = input[0];
+    let season = inpstr[0];
     let episode = input[2] + args[3];
     const api = `http://api.tvmaze.com/shows/13/episodebynumber?season=${season}&number=${episode}`;
     let sen = args[0];
