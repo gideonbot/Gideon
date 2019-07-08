0968dd4d210d61dc06a5dcee6aa77fb69f625930
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-08 12:22:25 +0200

    --- Auto-Git Commit ---

--- cmds/flashep.js
+++ cmds/flashep.js
@@ -2,7 +2,7 @@ const Discord = module.require("discord.js");
 const tvmaze = require('tvmaze-api-ts')
 
 module.exports.run = async (gideon, message, args) => {  
-    tvmaze.shows.episodebynumber('13', '1', '1').then(result => {
+    tvmaze.shows.episodebynumber('13', '5', '14').then(result => {
         // code
       })
     const flahsep = new Discord.RichEmbed()
