5b3277cd95713b88827f187a3c6b97aab9f470df
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-08 12:29:56 +0200

    --- Auto-Git Commit ---

--- cmds/flashep.js
+++ cmds/flashep.js
@@ -4,7 +4,7 @@ const tvmaze = require('tvmaze-api-ts')
 module.exports.run = async (gideon, message, args) => {  
     tvmaze.shows.episodebynumber('13', '5', '14').then(result => {
         const flashep = new Discord.RichEmbed()
-        .setTitle(`The Flash ${shows.season}x${shows.number} - ${}`)
+        .setTitle(`The Flash ${shows.season}x${shows.number} - ${shows.na}`)
         .setColor('#2791D3')
         .setTimestamp()
         .setFooter('Gideon - The Arrowverse Bot | Developed by adrifcastr', 'https://i.imgur.com/3RihwQS.png');
