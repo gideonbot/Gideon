abec45f883c14ef628a836bb9b703ecc40c0edfa
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-08 14:58:39 +0200

    --- Auto-Git Commit ---

--- cmds/episodes.js
+++ cmds/episodes.js
@@ -14,7 +14,7 @@ module.exports.run = async (gideon, message, args) => {
         let body = r.body;   
         let airdate = new Date(body.airstamp);
         let sum1 = body.summary.substring(3);
-        let sum= sum1.substring(0, sum1.length - 4);
+        //let sum= sum1.substring(0, sum1.length - 4);
 
         const flashep = new Discord.RichEmbed()
         .setColor('#2791D3')
