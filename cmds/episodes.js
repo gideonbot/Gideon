a577eae793059d3214f10b9843dc6d900c981f2e
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-08 16:44:40 +0200

    --- Auto-Git Commit ---

--- cmds/episodes.js
+++ cmds/episodes.js
@@ -23,7 +23,7 @@ module.exports.run = async (gideon, message, args) => {
         let offset = -5;   
         let cst = utc + (3600000*offset);
         let nd = new Date(cst); 
-        let cstdate = (nd.toLocaleDateString());
+        let cstdate = (nd.toDateString());
         let csttime = (nd.toLocaleTimeString());  
 
         const flashep = new Discord.RichEmbed()
