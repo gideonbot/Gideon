e6129a58e5d2c2eb994be8f249de2bdd90b4b6fb
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-08 22:43:22 +0200

    --- Auto-Git Commit ---

--- cmds/episodes.js
+++ cmds/episodes.js
@@ -16,7 +16,7 @@ module.exports.run = async (gideon, message, args) => {
     if (agc.match(/(?:flash)/)){
         showid = "13";
         showtitle = "The Flash";
-   / }   else if(show.includes(ar)){
+   /* }   else if(show.includes(ar)){
         showid = "4";
         showtitle = "Arrow";
     }   else if(show.includes(sg)){
@@ -30,7 +30,7 @@ module.exports.run = async (gideon, message, args) => {
         showtitle = "Constantine";
     }   else if(show.includes(bw)){
         showid = "37776";
-        showtitle = "Batwoman";
+        showtitle = "Batwoman"; */
     }   else{
         return message.channel.send(`"${show}" is not a valid argument!`)
     }  
