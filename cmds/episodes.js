c6ad5b8c0c71bbdf6175f1a4bcd4d6a8d50d9630
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-09 15:48:50 +0200

    --- Auto-Git Commit ---

--- cmds/episodes.js
+++ cmds/episodes.js
@@ -64,9 +64,9 @@ module.exports.run = async (gideon, message, args) => {
 
         var timeString = "18:00:00";
         var H = +timeString.substr(0, 2);
-var h = H % 12 || 12;
-var ampm = (H < 12 || H === 24) ? "AM" : "PM";
-timeString = h + timeString.substr(2, 3) + ampm;
+        var h = H % 12 || 12;
+        var ampm = (H < 12 || H === 24) ? "AM" : "PM";
+        timeString = h + timeString.substr(2, 3) + ampm;
 
         const epinfo = new Discord.RichEmbed()
         .setColor('#2791D3')
