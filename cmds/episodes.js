824c03e00b40274347201f852955e4e8ec9e9cee
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-09 15:27:35 +0200

    --- Auto-Git Commit ---

--- cmds/episodes.js
+++ cmds/episodes.js
@@ -50,7 +50,7 @@ module.exports.run = async (gideon, message, args) => {
         console.log(r.body);
         let body = r.body;   
         let airdate = new Date(body.airdate);
-        let airtime = new 
+        let airtime = new Time
         let desc = '';
         let img = '';
 
