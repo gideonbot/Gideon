749c287af3c58b5ebebb6abad4beaa2fda0f2e0c
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-10 18:04:17 +0200

    --- Auto-Git Commit ---

--- cmds/quote.js
+++ cmds/quote.js
@@ -7,6 +7,7 @@ module.exports.run = async (gideon, message, args) => {
     snekfetch.get(api).then(r => {
         console.log(r.body);
         let body = r.body;
+        const type = body.sections[0].content;
         let min = 0;
         let max = res.images.length - 1;
         let ranum = Math.floor(Math.random()*(max - min + 1)) + min;
