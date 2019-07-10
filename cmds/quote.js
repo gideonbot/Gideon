edc48be9d6c018f50743e3646adc581e50b88ae3
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-10 17:47:51 +0200

    --- Auto-Git Commit ---

--- cmds/quote.js
+++ cmds/quote.js
@@ -3,13 +3,27 @@ const snekfetch = require("snekfetch");
 
 module.exports.run = async (gideon, message, args) => {
 
-    const quote = new Discord.RichEmbed()
+    const api = `https://arrow.fandom.com/api/v1/Articles/Details?ids=50&titles=${args.join('%20')}&abstract=500&width=200&height=200`;
+    let term = args[0];
+    if(!term) return message.channel.send("You must supply a search term!");
+
+    snekfetch.get(api).then(r => {
+        console.log(r.body);
+        let body = r.body;
+        const type = Object.values(body.items)[0];       
+        if (!Object.keys(body.items).length) return message.channel.send(`There was no result for ${args.join(' ')} on the Arrowverse Wiki!`).catch(console.error);
+        const url = type.url.replace(/\(/g, '%28').replace(/\)/g, '%29');       
+               
+        const wikiart = new Discord.RichEmbed()
 	    .setColor('#2791D3')
-	    .setTitle('__You can check the list of available commands below:__')
-        .setTimestamp()
+	    .setTitle(type.title)
+        .setDescription(type.abstract + `\n\n**[Click here to read the full article](https://arrow.fandom.com${url} 'https://arrow.fandom.com${url}')**`)
+	    .setThumbnail(type.thumbnail)
+    	.setTimestamp()
     	.setFooter('Gideon - The Arrowverse Bot | Developed by adrifcastr', 'https://i.imgur.com/3RihwQS.png');
 
-        message.channel.send(quote);
+        message.channel.send(wikiart); 
+    });      
 }
 
 module.exports.help = {
