db8dc7e2c09aaa4e023e516694d6e0d1068a9e11
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-10 16:43:48 +0200

    --- Auto-Git Commit ---

--- cmds/meme.js
+++ cmds/meme.js
@@ -3,6 +3,11 @@ const Imgur = require('imgur-node');
 const imgclient = new Imgur.Client('fbe1de02978b3b4');
 
 module.exports.run = async (gideon, message, args) => {  
+    //var files = fs.readdirSync('./avm');
+    //let chosenFile = files[Math.floor(Math.random() * files.length)];
+      
+   // const attachment = new Discord.Attachment(`./avm/${chosenFile}`, chosenFile);
+
     imgclient.album.get('NVHwdNg', (err, res) => {
     if (err) console.error(err);
     console.log(res);
@@ -12,6 +17,7 @@ module.exports.run = async (gideon, message, args) => {
 
     const meme = new Discord.RichEmbed()
         .setColor('#2791D3')
+        //.attachFile(attachment)
         .setImage(ravm)
         .setTimestamp()
         .setFooter('Gideon - The Arrowverse Bot | Developed by adrifcastr', 'https://i.imgur.com/3RihwQS.png');
