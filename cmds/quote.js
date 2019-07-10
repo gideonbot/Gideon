8db9bad1027a6ad6f41b8793cbabd6c2c0b31532
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-10 19:13:12 +0200

    --- Auto-Git Commit ---

--- cmds/quote.js
+++ cmds/quote.js
@@ -15,7 +15,7 @@ module.exports.run = async (gideon, message, args) => {
         const chosenQuote = rq[ranum].text;
         console.log(chosenQuote);               
 
-        let
+        let qp = 
                
         const quote = new Discord.RichEmbed()
         .setColor('#2791D3')
