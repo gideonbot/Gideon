b8ebba4056a4de04a4209c03a6e55e94206d1f63
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-10 21:22:26 +0200

    --- Auto-Git Commit ---

--- cmds/quote.js
+++ cmds/quote.js
@@ -3,40 +3,40 @@ const snekfetch = require("snekfetch");
 
 module.exports.run = async (gideon, message, args) => {
 
-    const idapi = `https://dc.fandom.com/api/v1/Articles/Details?ids=50&titles=Category%3ACaitlin_Snow_(Arrow)%2FQuotes&abstract=100&width=200&height=200`;
-    
-    snekfetch.get(idapi).then(r => {
-    console.log(r.body);
-    let body = r.body;
-    const type = Object.values(body.items)[0];
-    let id = type.id;
-    const api = `https://dc.fandom.com/api/v1/Articles/AsSimpleJson?id=${id}`;
+        const idapi = `https://dc.fandom.com/api/v1/Articles/Details?ids=50&titles=Category%3ACaitlin_Snow_(Arrow)%2FQuotes&abstract=100&width=200&height=200`;
+        
+        snekfetch.get(idapi).then(r => {
+        console.log(r.body);
+        let body = r.body;
+        const type = Object.values(body.items)[0];
+        let id = type.id;
+        const api = `https://dc.fandom.com/api/v1/Articles/AsSimpleJson?id=${id}`;
 
-        snekfetch.get(api).then(r => {
-            console.log(r.body);
-            let body = r.body;
-            let rq = body.sections[0].content;
-            let min = 0;
-            let max = rq.length - 1;
-            console.log(rq);
-            let ranum = Math.floor(Math.random()*(max - min + 1)) + min;
-            const chosenQuote = rq[ranum].text;
-            console.log(chosenQuote);               
+            snekfetch.get(api).then(r => {
+                console.log(r.body);
+                let body = r.body;
+                let rq = body.sections[0].content;
+                let min = 0;
+                let max = rq.length - 1;
+                console.log(rq);
+                let ranum = Math.floor(Math.random()*(max - min + 1)) + min;
+                const chosenQuote = rq[ranum].text;
+                console.log(chosenQuote);               
 
-            let qp = body.sections[0].title;
-            let qpe = qp.replace(/(?:\/Quotes)/,'');
-            let qpb = qpe.replace(/(?:Arrow)/, 'Arrowverse') 
-               
-            const quote = new Discord.RichEmbed()
-            .setColor('#2791D3')
-            .setDescription(`**${chosenQuote}\n\n~${qpb}**`)
-            .setThumbnail(type.thumbnail)
-    	    .setTimestamp()
-    	    .setFooter('Gideon - The Arrowverse Bot | Developed by adrifcastr', 'https://i.imgur.com/3RihwQS.png');
+                let qp = body.sections[0].title;
+                let qpe = qp.replace(/(?:\/Quotes)/,'');
+                let qpb = qpe.replace(/(?:Arrow)/, 'Arrowverse') 
+                
+                const quote = new Discord.RichEmbed()
+                .setColor('#2791D3')
+                .setDescription(`**${chosenQuote}\n\n~${qpb}**`)
+                .setThumbnail(type.thumbnail)
+                .setTimestamp()
+                .setFooter('Gideon - The Arrowverse Bot | Developed by adrifcastr', 'https://i.imgur.com/3RihwQS.png');
 
-            message.channel.send(quote); 
-        });      
-    });  
+                message.channel.send(quote); 
+            });      
+        });  
 }
 
 module.exports.help = {
