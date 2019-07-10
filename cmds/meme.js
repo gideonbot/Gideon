ee561efb3b99e22a8804db8f7b0dda4dd1da4b3a
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-10 16:04:08 +0200

    --- Auto-Git Commit ---

--- cmds/meme.js
+++ cmds/meme.js
@@ -8,7 +8,7 @@ module.exports.run = async (gideon, message, args) => {
       
    // const attachment = new Discord.Attachment(`./avm/${chosenFile}`, chosenFile);
 
-
+    
 
     const meme = new Discord.RichEmbed()
         .setColor('#2791D3')
