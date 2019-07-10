86158d0a9878c8aebbb95d530df3d9a16172c73b
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-10 16:41:24 +0200

    --- Auto-Git Commit ---

--- cmds/meme.js
+++ cmds/meme.js
@@ -11,6 +11,7 @@ module.exports.run = async (gideon, message, args) => {
     imgclient.album.get('NVHwdNg', (err, res) => {
     if (err) console.error(err);
     console.log(res);
+    
     let ravm = res.images[.length].link;
     console.log(ravm);
    // const type = Object.values(body.items)[0];
