f4ce18b4a32fab9bece96b0356785c264d06e795
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-10 16:32:29 +0200

    --- Auto-Git Commit ---

--- cmds/quote.js
+++ cmds/quote.js
@@ -3,15 +3,14 @@ const Discord = module.require("discord.js");
 
 module.exports.run = async (gideon, message, args) => {
 
-    const help = new Discord.RichEmbed()
+    const quote = new Discord.RichEmbed()
 	    .setColor('#2791D3')
 	    .setTitle('__You can check the list of available commands below:__')
         .setTimestamp()
     	.setFooter('Gideon - The Arrowverse Bot | Developed by adrifcastr', 'https://i.imgur.com/3RihwQS.png');
 
-        message.channel.send(help);
+        message.channel.send(quote);
 }
 
 module.exports.help = {
     name: "quote"
-}
