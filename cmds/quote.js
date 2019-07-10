db98069058fd423fdd3bc0efbec70dd0f7b67917
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-10 21:26:45 +0200

    --- Auto-Git Commit ---

--- cmds/quote.js
+++ cmds/quote.js
@@ -4,6 +4,7 @@ const snekfetch = require("snekfetch");
 module.exports.run = async (gideon, message, args) => {
     snekfetch.get(api).then(r => {
     console.log(r.body);
+    let body = r.body;
     let titles = '';
     const idapi = `https://dc.fandom.com/api/v1/Articles/Details?ids=50&titles=${titles}&abstract=100&width=200&height=200`;
         
