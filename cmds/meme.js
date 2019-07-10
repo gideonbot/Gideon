6bfc0f5ee1e557dcaf9ae04845601d5209603a7c
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-10 16:42:06 +0200

    --- Auto-Git Commit ---

--- cmds/meme.js
+++ cmds/meme.js
@@ -3,11 +3,6 @@ const Imgur = require('imgur-node');
 const imgclient = new Imgur.Client('fbe1de02978b3b4');
 
 module.exports.run = async (gideon, message, args) => {  
-    //var files = fs.readdirSync('./avm');
-    //let chosenFile = files[Math.floor(Math.random() * files.length)];
-      
-   // const attachment = new Discord.Attachment(`./avm/${chosenFile}`, chosenFile);
-
     imgclient.album.get('NVHwdNg', (err, res) => {
     if (err) console.error(err);
     console.log(res);
