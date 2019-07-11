811eba846149b7975742741b99077cbf699fff0d
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-11 16:57:12 +0200

    --- Auto-Git Commit ---

--- cmds/attack.js
+++ cmds/attack.js
@@ -19,7 +19,7 @@ module.exports.run = async (gideon, message, args) => {
         }
     }
 
-    const user = getUserFromMention(args[]);
+    const user = getUserFromMention(args[1]);
 		if (!user) {
 			return message.channel.send('You must use a proper mention if you want to attack someone!');
         }
