174b789887b0070f31285641bf8adae0ffa8d20b
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-11 15:06:21 +0200

    --- Auto-Git Commit ---

--- cmds/attack.js
+++ cmds/attack.js
@@ -1,7 +1,35 @@
 const Discord = module.require("discord.js");
 
 module.exports.run = async (gideon, message, args) => {
+    const auth = message.author;
 
+    function getUserFromMention(mention) {
+        if (!mention) return;
+    
+        if (mention.startsWith('<@') && mention.endsWith('>')) {
+            mention = mention.slice(2, -1);
+    
+            if (mention.startsWith('!')) {
+                mention = mention.slice(1);
+            }
+    
+            return gideon.users.get(mention);
+        }
+    }
+
+    const user = getUserFromMention(args[0]);
+		if (!user) {
+			return message.channel.send('Please use a proper mention if you want to cuddle someone.');
+		}
+
+    const cuddle = new Discord.RichEmbed()
+	.setColor('#2791D3')
+    .setDescription(`**${auth} you have cuddled ${user}!**\n\nA Beebo-tastic cuddle always brigthens the mood!`)
+	.setImage('https://i.imgur.com/IOpmt2j.gif')
+    .setTimestamp()
+    .setFooter('Gideon - The Arrowverse Bot | Developed by adrifcastr', 'https://i.imgur.com/3RihwQS.png');
+
+    message.channel.send(cuddle);
 }
 
 module.exports.help = {
