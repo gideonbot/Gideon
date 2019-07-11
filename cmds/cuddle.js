f46f25f5daada67f628b268051b41bba17a87b7a
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-11 15:09:39 +0200

    --- Auto-Git Commit ---

--- cmds/cuddle.js
+++ cmds/cuddle.js
@@ -2,7 +2,7 @@ const Discord = module.require("discord.js");
 
 module.exports.run = async (gideon, message, args) => {
     const auth = message.author;
-    let c
+    let cus
     function getUserFromMention(mention) {
         if (!mention) return;
     
