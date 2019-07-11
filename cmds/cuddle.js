d0731a1620461db55175cd6c3581a2beeb5b0a58
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-11 14:43:18 +0200

    --- Auto-Git Commit ---

--- cmds/cuddle.js
+++ cmds/cuddle.js
@@ -21,7 +21,7 @@ module.exports.run = async (gideon, message, args) => {
     const donate = new Discord.RichEmbed()
 	.setColor('#2791D3')
 	.setTitle('Donations')
-    .setDescription(`Donato my [Paypal.me](https://www.paypal.me/adrifcastr 'https://www.paypal.me/adrifcastr'). \nDonating supports the development, maintenance and hosting of this project. \nThank you!`)
+    .setDescription(`Dpment, maintenance and hosting of this project. \nThank you!`)
 	.setThumbnail('https://i.imgur.com/lWSoZQB.png')
     .setTimestamp()
     .setFooter('Gideon - The Arrowverse Bot | Developed by adrifcastr', 'https://i.imgur.com/3RihwQS.png');
