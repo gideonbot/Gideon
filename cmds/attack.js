3837ed2207976e71e41c795c6a52368edf699f35
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-11 15:10:57 +0200

    --- Auto-Git Commit ---

--- cmds/attack.js
+++ cmds/attack.js
@@ -19,7 +19,7 @@ module.exports.run = async (gideon, message, args) => {
 
     const user = getUserFromMention(atc);
 		if (!user) {
-			return message.channel.send('Please use a proper mention if you want to cuddle someone.');
+			return message.channel.send('Please use a proper mention if you want to attack someone.');
         }
         
     
