8f2fb239b134c65e35d26e5d5a1bb6572aabe8c3
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-11 16:35:23 +0200

    --- Auto-Git Commit ---

--- cmds/attack.js
+++ cmds/attack.js
@@ -47,7 +47,7 @@ module.exports.run = async (gideon, message, args) => {
     }   else if(atc.match(/(?:shootarrow)/)){
         chosenattack = '';
         chosendesc = '';
-        attackgif = '';
+        attackgif = 'https://i.imgur.com/De98uf0.gif';
     }   else if(atc.match(/(?:heatvision)/)){
         chosenattack = 'you used your heat vision on';
         chosendesc = '';
