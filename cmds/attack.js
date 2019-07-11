f78b7ff3bba98bb077654e2855f3eb7b33d7d56c
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-11 16:51:53 +0200

    --- Auto-Git Commit ---

--- cmds/attack.js
+++ cmds/attack.js
@@ -59,7 +59,7 @@ module.exports.run = async (gideon, message, args) => {
 
     const attack = new Discord.RichEmbed()
 	.setColor('#2791D3')
-    .setDescription(`**${emote}${auth} ${chosenattack} ${user}**\n\n${chosendesc}`)
+    .setDescription(`**${emote}${auth} ${chosenattack} ${user}${emote}**\n\n${chosendesc}`)
 	.setImage(attackgif)
     .setTimestamp()
     .setFooter('Gideon - The Arrowverse Bot | Developed by adrifcastr', 'https://i.imgur.com/3RihwQS.png');
