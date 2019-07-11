022dfbfbe92e5678585cbd8ec46a2a1289d8c1a9
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-11 15:09:57 +0200

    --- Auto-Git Commit ---

--- cmds/cuddle.js
+++ cmds/cuddle.js
@@ -17,7 +17,7 @@ module.exports.run = async (gideon, message, args) => {
         }
     }
 
-    const user = getUserFromMention(args[0]);
+    const user = getUserFromMention(cus);
 		if (!user) {
 			return message.channel.send('Please use a proper mention if you want to cuddle someone.');
 		}
