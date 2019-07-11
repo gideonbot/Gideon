2f8896578cc6e3f30e8db6df335c6d92c3518e75
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-11 15:01:26 +0200

    --- Auto-Git Commit ---

--- cmds/cuddle.js
+++ cmds/cuddle.js
@@ -24,7 +24,7 @@ module.exports.run = async (gideon, message, args) => {
 
     const cuddle = new Discord.RichEmbed()
 	.setColor('#2791D3')
-	.setTitle(`${auth} you have cuddled ${user}!`)
+	.addField(`${auth} you have cuddled ${user}!`)
     .setDescription(`A Beebo-tastic cuddle always brigthens the mood!`)
 	.setImage('https://i.imgur.com/IOpmt2j.gif')
     .setTimestamp()
