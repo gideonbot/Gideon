af02e84da558fe1841376156c26165d4a4a9c90d
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-11 16:52:23 +0200

    --- Auto-Git Commit ---

--- cmds/attack.js
+++ cmds/attack.js
@@ -37,10 +37,12 @@ module.exports.run = async (gideon, message, args) => {
     }   else if(atc.match(/(?:lthrow)/)){
         chosenattack = 'you threw a lightning bolt at';
         chosendesc = 'Maybe this\'ll give you abs.';
+        emote = '';
         attackgif = 'https://i.imgur.com/6ECd0ty.gif';
     }   else if(atc.match(/(?:reverseflash)/)){
         chosenattack = 'you reverse-flashed';
         chosendesc = 'I\'m sorry.';
+        emote = '';
         attackgif = 'https://i.imgur.com/h7orTAQ.gif';
     }   else if(atc.match(/(?:vibeblast)/)){
         chosenattack = 'you vibe-blasted';
