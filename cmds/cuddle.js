2ca3f4ea6a2038bdd0d6f4381895d557b5873cae
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-11 14:50:55 +0200

    --- Auto-Git Commit ---

--- cmds/cuddle.js
+++ cmds/cuddle.js
@@ -2,6 +2,21 @@ const Discord = module.require("discord.js");
 
 module.exports.run = async (gideon, message, args) => {
     const auth = message.author;
+
+    function getUserFromMention(mention) {
+        if (!mention) return;
+    
+        if (mention.startsWith('<@') && mention.endsWith('>')) {
+            mention = mention.slice(2, -1);
+    
+            if (mention.startsWith('!')) {
+                mention = mention.slice(1);
+            }
+    
+            return client.users.get(mention);
+        }
+    }
+    
     const user = getUserFromMention(args[0]);
 		if (!user) {
 			return message.reply('Please use a proper mention if you want to cuddle someone.');
