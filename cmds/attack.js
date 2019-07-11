f920b3064cf0649c98713c2a68a25c7669790420
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-11 15:25:34 +0200

    --- Auto-Git Commit ---

--- cmds/attack.js
+++ cmds/attack.js
@@ -43,7 +43,7 @@ module.exports.run = async (gideon, message, args) => {
         chosenattack = '';
         chosendesc = '';
     }   else {
-        
+        return message.channel.send();
     }
 
     const attack = new Discord.RichEmbed()
