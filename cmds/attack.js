000b1d504b7bd3db376ab56d6187e451ac6dbc8d
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-11 16:51:12 +0200

    --- Auto-Git Commit ---

--- cmds/attack.js
+++ cmds/attack.js
@@ -27,6 +27,7 @@ module.exports.run = async (gideon, message, args) => {
     let chosenattack = '';
     let chosendesc = '';
     let attackgif = '';
+    let
 
     if(atc.match(/(?:iceblast)/)){
         chosenattack = 'you ice-blasted';
