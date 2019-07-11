e06b4634d93a2dedad94040806ff0c729fdc933f
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-11 15:19:58 +0200

    --- Auto-Git Commit ---

--- cmds/attack.js
+++ cmds/attack.js
@@ -30,7 +30,7 @@ module.exports.run = async (gideon, message, args) => {
     if(atc.match(/(?:iceblast)/)){
         chosenattack = '';
         chosendesc = '';
-    }   el
+    }   else if()
 
     const attack = new Discord.RichEmbed()
 	.setColor('#2791D3')
