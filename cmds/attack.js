144292edced781563abbc6429469d70f681e6fcf
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-11 15:32:17 +0200

    --- Auto-Git Commit ---

--- cmds/attack.js
+++ cmds/attack.js
@@ -47,7 +47,7 @@ module.exports.run = async (gideon, message, args) => {
     }   else if(atc.match(/(?:heatvision)/)){
         chosenattack = 'you used your heat vision on';
         chosendesc = '';
-        attackgif = '';
+        attackgif = 'https://i.imgur.com/0WxrvMp.gif';
     }   else {
         return message.channel.send('You must supply a valid attack!');
     }
