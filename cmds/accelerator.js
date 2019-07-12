152cdb789b2be408e36b04e925bca2fcb5b7b733
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-12 14:24:54 +0200

    --- Auto-Git Commit ---

--- cmds/accelerator.js
+++ cmds/accelerator.js
@@ -10,6 +10,14 @@ module.exports.run = async (gideon, message, args) => {
         });
     }
 
+    let accstart = [
+        message.channel.send("Engaging S.T.A.R. Labs. particle accelerator..."),
+        message.channel.send("3"),
+        message.channel.send("2"),
+        message.channel.send("1"),
+        message.channel.send("ALERT SYSTEM FAILURE")
+        ];    
+
     sendMsgs(msgs, 1000);
 
     const explosion = new Discord.RichEmbed()
