25c19b23a32ee6bbca0dec0e7d4d512a78fde349
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-12 14:08:59 +0200

    --- Auto-Git Commit ---

--- cmds/accelerator.js
+++ cmds/accelerator.js
@@ -10,7 +10,7 @@ module.exports.run = async (gideon, message, args) => {
     let ravm = res.images[ranum].link;
     console.log(ravm);
 
-    const meme = new Discord.RichEmbed()
+    const explosion = new Discord.RichEmbed()
         .setColor('#2791D3')
         .setImage(ravm)
         .setTimestamp()
