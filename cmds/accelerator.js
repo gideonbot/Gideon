4d5a2ae9c96feff22fe3ea704444413067929420
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-12 14:51:27 +0200

    --- Auto-Git Commit ---

--- cmds/accelerator.js
+++ cmds/accelerator.js
@@ -5,7 +5,7 @@ module.exports.run = async (gideon, message, args) => {
         countDown(); 
     }, 1000);
 
-    message.channel.send("Engaging S.T.A.R. Labs. particle accelerator...").then(set);
+    message.channel.send("Engaging S.T.A.R. Labs. particle accelerator...").then(setTimeout());
     message.channel.send("3");
     message.channel.send("2");
     message.channel.send("1");
