f4a4cf0fadb0c4002af81ad5df5ba0d5785f12ea
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-12 14:10:11 +0200

    --- Auto-Git Commit ---

--- cmds/accelerator.js
+++ cmds/accelerator.js
@@ -2,6 +2,8 @@ const Discord = module.require("discord.js");
 
 module.exports.run = async (gideon, message, args) => {      
     message.channel.send("Engaging S.T.A.R. Labs. particle accelerator...")
+    message.channel.send("Engaging S.T.A.R. Labs. particle accelerator...")
+    message.channel.send("Engaging S.T.A.R. Labs. particle accelerator...")
 
     const explosion = new Discord.RichEmbed()
         .setColor('#2791D3')
