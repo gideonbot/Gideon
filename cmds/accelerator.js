3d451ce1c8232d0d670362cea0ad610de9b18192
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-12 18:28:31 +0200

    --- Auto-Git Commit ---

--- cmds/accelerator.js
+++ cmds/accelerator.js
@@ -38,7 +38,7 @@ module.exports.run = async (gideon, message, args) => {
     let vbrdesc = 'Congratulations! You are a Viber now!';
     let vbrgif = 'https://i.imgur.com/gmqggYB.gif';
     
-    
+    let ptra
     const pwrtitle = '';
     const pwrdesc = '';
     const pwrgif = '';
