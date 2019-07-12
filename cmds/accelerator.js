8926bac7b169367b080a6b8a7a8d8e4b1f8dd35d
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-12 18:40:30 +0200

    --- Auto-Git Commit ---

--- cmds/accelerator.js
+++ cmds/accelerator.js
@@ -38,6 +38,7 @@ module.exports.run = async (gideon, message, args) => {
     let vbrdesc = 'Congratulations! You are a Viber now!';
     let vbrgif = 'https://i.imgur.com/gmqggYB.gif';
     
+    
     const power = new Discord.RichEmbed()
         .setColor('#2791D3')
         .setTitle(pwrtitle)
