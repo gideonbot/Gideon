d2a38dd978ced6b9c7134c6c3961b829f9895a0e
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-12 18:22:30 +0200

    --- Auto-Git Commit ---

--- cmds/accelerator.js
+++ cmds/accelerator.js
@@ -34,7 +34,7 @@ module.exports.run = async (gideon, message, args) => {
     let fsdesc = 'Congratulations! You are now a part of Firestorm!';
     let fsgif = '';
 
-    let vbrtle = 'It appears, that you have developed a connection to the Speed Force!';
+    let vbrtle = 'It appears, that you have aquired!';
     let vbrdesc = 'Congratulations! You are a Speedster now!';
     let vbrgif = '';
 
