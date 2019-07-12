536bb6aad6a85ed6a865a0b92cfc9cfb1062b7b6
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-12 17:38:22 +0200

    --- Auto-Git Commit ---

--- cmds/accelerator.js
+++ cmds/accelerator.js
@@ -24,7 +24,7 @@ module.exports.run = async (gideon, message, args) => {
 
     const power = new Discord.RichEmbed()
         .setColor('#2791D3')
-        .setTitle(``)
+        .setTitle(`It appea`)
         .setDescription(``)
         .setImage('https://i.imgur.com/w9eLDty.gif')
         .setTimestamp()
