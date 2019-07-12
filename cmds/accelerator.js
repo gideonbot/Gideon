6c4900f477a33c1ca43110f334824d84244456f5
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-12 18:13:12 +0200

    --- Auto-Git Commit ---

--- cmds/accelerator.js
+++ cmds/accelerator.js
@@ -31,7 +31,7 @@ module.exports.run = async (gideon, message, args) => {
     let kfgif = 'https://i.imgur.com/w9eLDty.gif';
 
     let fsstle = 'It appears, that you have developed a connection to the Speed Force!';
-    let spdsdesc = 'Congratulations! You are a Speedster now!';
+    let fsdesc = 'Congratulations! You are a Speedster now!';
     let spdsgif = 'https://i.imgur.com/w9eLDty.gif';
 
     let spdstle = 'It appears, that you have developed a connection to the Speed Force!';
