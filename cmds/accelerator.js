1596f8025ac97cc920019d79eb182892e10ca9f3
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-12 17:38:46 +0200

    --- Auto-Git Commit ---

--- cmds/accelerator.js
+++ cmds/accelerator.js
@@ -24,7 +24,7 @@ module.exports.run = async (gideon, message, args) => {
 
     const power = new Discord.RichEmbed()
         .setColor('#2791D3')
-        .setTitle(`It appears, that you have developed a connection to the Speed Force`)
+        .setTitle(`It appears, that you have developed a connection to the Speed Force!`)
         .setDescription(``)
         .setImage('https://i.imgur.com/w9eLDty.gif')
         .setTimestamp()
