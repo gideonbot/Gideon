c02b33b150e81934269858d1708cb1387b52eeb0
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-12 14:22:47 +0200

    --- Auto-Git Commit ---

--- cmds/accelerator.js
+++ cmds/accelerator.js
@@ -5,8 +5,8 @@ module.exports.run = async (gideon, message, args) => {
     message.channel.send("Engaging S.T.A.R. Labs. particle accelerator..."),
     message.channel.send("3"),
     message.channel.send("2"),
-    message.channel.send("1");
-    message.channel.send("ALERT SYSTEM FAILURE");
+    message.channel.send("1"),
+    message.channel.send("ALERT SYSTEM FAILURE")
    ];
     const explosion = new Discord.RichEmbed()
         .setColor('#2791D3')
