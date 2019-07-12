756868400e6654017b6d69596d719c4088ae8605
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-12 15:12:34 +0200

    --- Auto-Git Commit ---

--- cmds/future.js
+++ cmds/future.js
@@ -13,9 +13,6 @@ module.exports.run = async (gideon, message, args) => {
     	.setFooter('Gideon - The Arrowverse Bot | Developed by adrifcastr', 'https://i.imgur.com/3RihwQS.png');
 
         message.channel.send(future);
-    }   else{
-        return message.channel.send('');
-    }
 }
 
 module.exports.help = {
