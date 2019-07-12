81a240f93685e545183798f38376ebecf4d055c2
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-12 18:42:25 +0200

    --- Auto-Git Commit ---

--- cmds/accelerator.js
+++ cmds/accelerator.js
@@ -40,6 +40,8 @@ module.exports.run = async (gideon, message, args) => {
     
     let chosenpw = Math.floor(Math.random()*(4-1+1)+1);
 
+
+    
     const power = new Discord.RichEmbed()
         .setColor('#2791D3')
         .setTitle(pwrtitle)
