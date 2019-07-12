345f21f0196a4fb37842ae10967c49ca1988d0b1
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-12 18:24:06 +0200

    --- Auto-Git Commit ---

--- cmds/accelerator.js
+++ cmds/accelerator.js
@@ -34,7 +34,7 @@ module.exports.run = async (gideon, message, args) => {
     let fsdesc = 'Congratulations! You are now a part of Firestorm!';
     let fsgif = '';
 
-    let vbrtle = 'It appears, that you have aquired a connection to the Multiverse\'s intradin!';
+    let vbrtle = 'It appears, that you have aquired a connection to the Multiverse\'s intradimensional !';
     let vbrdesc = 'Congratulations! You are a Speedster now!';
     let vbrgif = '';
 
