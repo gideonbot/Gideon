7bd2d0d5b8e1943a252db0ddb19b28d7164da5b8
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-12 17:26:41 +0200

    --- Auto-Git Commit ---

--- cmds/course.js
+++ cmds/course.js
@@ -27,7 +27,7 @@ module.exports.run = async (gideon, message, args) => {
                 let ratime = Math.floor(Math.random()*(max - min + 1)) + min;
 
                 const time = `${body[1].date}`;
-                const year = time[myString.length -1];
+                const year = time[time.length -4];
 
                 const course = new Discord.RichEmbed()
                 .setColor('#2791D3')
