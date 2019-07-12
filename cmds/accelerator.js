7c503af074b817430b391aef8b53eac4f67537a6
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-12 14:29:06 +0200

    --- Auto-Git Commit ---

--- cmds/accelerator.js
+++ cmds/accelerator.js
@@ -18,7 +18,7 @@ module.exports.run = async (gideon, message, args) => {
         message.channel.send("ALERT SYSTEM FAILURE")
         ];    
 
-    sendMsgs(accstart, 2000);
+    sendMsgs(accstart, 10000);
 
     const explosion = new Discord.RichEmbed()
         .setColor('#2791D3')
