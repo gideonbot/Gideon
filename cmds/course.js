51520b58707819880a84b811b1146ab9ed6ffd4f
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-12 15:42:20 +0200

    --- Auto-Git Commit ---

--- cmds/course.js
+++ cmds/course.js
@@ -2,7 +2,7 @@ const Discord = module.require("discord.js");
 const snekfetch = require("snekfetch");
 
 module.exports.run = async (gideon, message, args) => {
-
+    
     message.channel.send('Yes Captain Lance!');
 
         const dateapi = 'https://api.myjson.com/bins/p4zc7';
