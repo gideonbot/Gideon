c36b671c4798961ac019bb23e099069e5aa58664
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-12 14:46:50 +0200

    --- Auto-Git Commit ---

--- cmds/accelerator.js
+++ cmds/accelerator.js
@@ -3,8 +3,8 @@ const Discord = module.require("discord.js");
 module.exports.run = async (gideon, message, args) => {      
     message.channel.send("Engaging S.T.A.R. Labs. particle accelerator...").delay(2000);
     message.channel.send("3").delay(2000);
-    message.channel.send("2");
-    message.channel.send("1");
+    message.channel.send("2").delay(2000);
+    message.channel.send("1").delay(2000);
     message.channel.send("ALERT SYSTEM FAILURE");
 
     const explosion = new Discord.RichEmbed()
