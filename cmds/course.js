c179dde48c12e3a06d679b84b8042814c1343141
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-12 16:59:17 +0200

    --- Auto-Git Commit ---

--- cmds/course.js
+++ cmds/course.js
@@ -11,7 +11,8 @@ module.exports.run = async (gideon, message, args) => {
             console.log(r.body);
             let body = r.body;   
             
-            let destination
+            let destination = '';
+            let 
             const future = new Discord.RichEmbed()
             .setColor('#2791D3')
             .setTitle(`Course set to ${destination}, ${time}`)
