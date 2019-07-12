df9ad1b7ec59da6459409939e117a1dc9915a768
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-12 14:14:43 +0200

    --- Auto-Git Commit ---

--- cmds/accelerator.js
+++ cmds/accelerator.js
@@ -9,7 +9,7 @@ module.exports.run = async (gideon, message, args) => {
 
     const explosion = new Discord.RichEmbed()
         .setColor('#2791D3')
-        .setImage('ravm')
+        .setImage('https://i.imgur.com/opCbZTn.gif')
         .setTimestamp()
         .setFooter('Gideon - The Arrowverse Bot | Developed by adrifcastr', 'https://i.imgur.com/3RihwQS.png');
 
