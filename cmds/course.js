a38a4b553515227764068a04a75e5c30c75183b4
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-12 17:07:51 +0200

    --- Auto-Git Commit ---

--- cmds/course.js
+++ cmds/course.js
@@ -12,7 +12,7 @@ module.exports.run = async (gideon, message, args) => {
             let body = r.body;   
             
             let min = 0;
-            let max = rt.length - 1;
+            let max = body.length - 1;
             console.log(rt);
             let ranum = Math.floor(Math.random()*(max - min + 1)) + min;
 
