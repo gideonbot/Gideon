360491efc51ea294f217bf60fceab7dc4e8d4e1c
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-14 01:11:19 +0200

    --- Auto-Git Commit ---

--- cmds/countdown.js
+++ cmds/countdown.js
@@ -8,12 +8,6 @@ module.exports.run = async (gideon, message, args) => {
     const legendsapi = 'http://api.tvmaze.com/shows/1851?embed=nextepisode';
     const bwomanapi = 'http://api.tvmaze.com/shows/37776?embed=nextepisode';
 
-    function getDayName(dateStr, locale)
-    {
-    var date = new Date(dateStr);
-    return date.toLocaleDateString(locale, { weekday: 'long' });        
-    }
-
     snekfetch.get(flashapi).then(r => {
         console.log(r.body);
         let body = r.body;   
