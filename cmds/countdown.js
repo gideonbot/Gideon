48ded60e512df082a3f3cf918011e02bd208842a
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-14 01:43:38 +0200

    --- Auto-Git Commit ---

--- cmds/countdown.js
+++ cmds/countdown.js
@@ -25,7 +25,11 @@ module.exports.run = async (gideon, message, args) => {
         const fladate = body._embedded.airdate;
         const fladay = getDayName(fladate, "en-US");
 
-        var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+        var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+var firstDate = new Date(2008,01,12);
+var secondDate = new Date(2008,01,22);
+
+var diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
 
         const countdown = new Discord.RichEmbed()
 	    .setColor('#2791D3')
