d3fa7f4926b17d4736ec63a2d10a844575dbed53
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-14 00:45:30 +0200

    --- Auto-Git Commit ---

--- cmds/countdown.js
+++ cmds/countdown.js
@@ -14,7 +14,7 @@ module.exports.run = async (gideon, message, args) => {
 
         const countdown = new Discord.RichEmbed()
 	    .setColor('#2791D3')
-	    .setTitle('')
+	    .setTitle('____')
         .setDescription()
 	    .setThumbnail()
     	.setTimestamp()
