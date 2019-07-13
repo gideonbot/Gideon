262d2caf7c08b8b59fbe50d3bc2dd9a5ee466484
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-13 19:50:12 +0200

    --- Auto-Git Commit ---

--- cmds/crossovers.js
+++ cmds/crossovers.js
@@ -23,7 +23,7 @@ module.exports.run = async (gideon, message, args) => {
         .setTimestamp()
     	.setFooter('Gideon - The Arrowverse Bot | Developed by adrifcastr', 'https://i.imgur.com/3RihwQS.png');
 
-        message.channel.send(help);
+        message.channel.send(crossovers);
 }
 
 module.exports.help = {
