61dbf3c389eb3b22762d16bd82693bd19eaa6b41
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-14 01:27:13 +0200

    --- Auto-Git Commit ---

--- cmds/countdown.js
+++ cmds/countdown.js
@@ -24,7 +24,7 @@ module.exports.run = async (gideon, message, args) => {
         }
 
         let dateStr = '1400796000';
-        const fladay = getDayName(dateStr, "en-NL");
+        const fladay = getDayName(dateStr, "en-US");
 
         const countdown = new Discord.RichEmbed()
 	    .setColor('#2791D3')
