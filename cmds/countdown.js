e3f5131e9240bfffb8fb365fa951ad44113572cf
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-14 01:10:31 +0200

    --- Auto-Git Commit ---

--- cmds/countdown.js
+++ cmds/countdown.js
@@ -8,6 +8,8 @@ module.exports.run = async (gideon, message, args) => {
     const legendsapi = 'http://api.tvmaze.com/shows/1851?embed=nextepisode';
     const bwomanapi = 'http://api.tvmaze.com/shows/37776?embed=nextepisode';
 
+    
+
     snekfetch.get(flashapi).then(r => {
         console.log(r.body);
         let body = r.body;   
