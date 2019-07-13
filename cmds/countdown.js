283237abbf2cc565f5b2c5a8e22d1d52724d4007
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-14 01:43:56 +0200

    --- Auto-Git Commit ---

--- cmds/countdown.js
+++ cmds/countdown.js
@@ -26,8 +26,8 @@ module.exports.run = async (gideon, message, args) => {
         const fladay = getDayName(fladate, "en-US");
 
         var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
-var firstDate = new Date(2008,01,12);
-var secondDate = new Date(2008,01,22);
+        var firstDate = new Date(2008,01,12);
+        var secondDate = new Date(2008,01,22);
 
 var diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
 
