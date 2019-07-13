f178318983cb2b7c13a604d75b8d5c5d79a1d2aa
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-14 00:52:24 +0200

    --- Auto-Git Commit ---

--- cmds/countdown.js
+++ cmds/countdown.js
@@ -15,7 +15,7 @@ module.exports.run = async (gideon, message, args) => {
         const countdown = new Discord.RichEmbed()
 	    .setColor('#2791D3')
 	    .setTitle('__Upcoming episodes:__')
-        .addTitle(`${flaeptitle} ${body.season}x${body.number<10?"0"+body.number:body.number} - ${body.name}`)
+        .addTitle(`${flaeptitle} ${flaseason}x${body.number<10?"0"+body.number:body.number} - ${body.name}`)
         .addTitle(`${areptitle} ${body.season}x${body.number<10?"0"+body.number:body.number} - ${body.name}`)
         .addTitle(`${sgeptitle} ${body.season}x${body.number<10?"0"+body.number:body.number} - ${body.name}`)
         .addTitle(`${lgeptitle} ${body.season}x${body.number<10?"0"+body.number:body.number} - ${body.name}`)
