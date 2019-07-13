3d0cb81f87a44c3f4ddaead6b938e2751f076dd7
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-14 01:26:19 +0200

    --- Auto-Git Commit ---

--- cmds/countdown.js
+++ cmds/countdown.js
@@ -18,10 +18,10 @@ module.exports.run = async (gideon, message, args) => {
         const flaepname = body._embedded.name;
 
         function getDayName(dateStr, locale)
-{
+        {
         var date = new Date(dateStr);
         return date.toLocaleDateString(locale, { weekday: 'long' });        
-}
+        }
 
         var dateStr = '1400796000';
         var day = getDayName(dateStr, "nl-NL");
