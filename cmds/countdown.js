caacc2835c28866fc4b52c0508abc6a30a8ab744
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-14 01:27:38 +0200

    --- Auto-Git Commit ---

--- cmds/countdown.js
+++ cmds/countdown.js
@@ -16,8 +16,7 @@ module.exports.run = async (gideon, message, args) => {
         const flaseason = body._embedded.season;
         const flanumber = body._embedded.number;
         const flaepname = body._embedded.name;
-
-        
+        let fladate = 
 
         function getDayName(dateStr, locale)
         {
