3b76213caf9a5a0355b389bb00594af36a2b581c
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-13 19:53:18 +0200

    --- Auto-Git Commit ---

--- cmds/crossovers.js
+++ cmds/crossovers.js
@@ -9,7 +9,7 @@ module.exports.run = async (gideon, message, args) => {
 	    .setTitle('__Below you\'ll find a list of all Arrowverse crossover episodes in ther respective watching order:__')
         .addField('Flash vs. Arrow', 'The Flash 1x08 - Flash vs. Arrow\nArrow 3x08 - The Brave and the Bold')  
         .addField('Heroes Join Forces', 'The Flash 2x08 - Legends of Today\nArrow 4x08 - Legends of Yesterday')  
-        .addField('Worlds Finest', 'The Flash 2x18 - Versus ZoomSupergirl 1x18 - Worlds Finest')  
+        .addField('Worlds Finest', 'The Flash 2x18 - Versus Zoom\nSupergirl 1x18 - Worlds Finest')  
         .addField('!meme', 'Displays a random Arrowverse meme')  
         .addField('!quote', 'Displays a random Arrowverse quote')  
         .addField('!accelerator', 'Blows up the S.T.A.R. labs particle accelerator to gain a methuman ability')  
