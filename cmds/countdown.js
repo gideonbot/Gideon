1cbbb2a1884201e826f74f2b1e834a6f0a9fcb77
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-14 00:40:36 +0200

    --- Auto-Git Commit ---

--- cmds/countdown.js
+++ cmds/countdown.js
@@ -3,7 +3,7 @@ const snekfetch = require("snekfetch");
 
 module.exports.run = async (gideon, message, args) => {
     const flashapi = 'http://api.tvmaze.com/shows/13?embed=nextepisode';
-    const arrowapi = 'https://api.github.com/repos/adrifcastr/Gideon';
+    const arrowapi = 'http://api.tvmaze.com/shows/13?embed=nextepisode';
     const supergirlapi = 'https://api.github.com/repos/adrifcastr/Gideon';
     const legendsapi = 'https://api.github.com/repos/adrifcastr/Gideon';
     const bwomanapi = 'https://api.github.com/repos/adrifcastr/Gideon';
