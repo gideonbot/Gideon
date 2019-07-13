1fa0ab5c813482d1190856f09162ee73cd22e322
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-14 01:33:33 +0200

    --- Auto-Git Commit ---

--- cmds/countdown.js
+++ cmds/countdown.js
@@ -22,7 +22,7 @@ module.exports.run = async (gideon, message, args) => {
         const flaseason = body._embedded.season;
         const flanumber = body._embedded.number;
         const flaepname = body._embedded.name;
-        let fladate = body._embedded.airdate;
+        const fladate = body._embedded.airdate;
         const fladay = getDayName(fladate, "en-US");
 
         const countdown = new Discord.RichEmbed()
