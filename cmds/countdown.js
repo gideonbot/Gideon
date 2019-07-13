48aea761cdef3ed79f81b98993e39b67e3dc4216
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-14 00:53:42 +0200

    --- Auto-Git Commit ---

--- cmds/countdown.js
+++ cmds/countdown.js
@@ -19,7 +19,7 @@ module.exports.run = async (gideon, message, args) => {
         .addTitle(`${areptitle} ${areason}x${arnumber<10?"0"+arnumber:body.number} - ${body.name}`)
         .addTitle(`${sgeptitle} ${sgeason}x${sgnumber<10?"0"+sgnumber:body.number} - ${body.name}`)
         .addTitle(`${lgeptitle} ${lgseason}x${lgnumber<10?"0"+lgnumber:body.number} - ${body.name}`)
-        .addTitle(`${bweptitle} ${bwseason}x${bwnumber<10?"0"+body.number:body.number} - ${body.name}`)
+        .addTitle(`${bweptitle} ${bwseason}x${bwnumber<10?"0"+bwnumber:body.number} - ${body.name}`)
 	    .setThumbnail()
     	.setTimestamp()
     	.setFooter('Gideon - The Arrowverse Bot | Developed by adrifcastr', 'https://i.imgur.com/3RihwQS.png');
