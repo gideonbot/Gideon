87a04d63ca085b150efe893795c78874c28a96f8
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-14 00:32:54 +0200

    --- Auto-Git Commit ---

---
+++ cmds/countdown.js
@@ -0,0 +1,25 @@
+const Discord = module.require("discord.js");
+const snekfetch = require("snekfetch");
+
+module.exports.run = async (gideon, message, args) => {
+    const api = 'https://api.github.com/repos/adrifcastr/Gideon';
+
+    snekfetch.get(api).then(r => {
+        console.log(r.body);
+        let body = r.body;   
+
+        const countdown = new Discord.RichEmbed()
+	    .setColor('#2791D3')
+	    .setTitle()
+        .setDescription()
+	    .setThumbnail()
+    	.setTimestamp()
+    	.setFooter('Gideon - The Arrowverse Bot | Developed by adrifcastr', 'https://i.imgur.com/3RihwQS.png');
+
+        message.channel.send(countdown); 
+    });       
+}
+
+module.exports.help = {
+    name: "cd"
+}
