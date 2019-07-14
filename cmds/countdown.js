66cfeefb0dc87c23292a5fc725a29d1b805496b6
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-14 04:18:25 +0200

    --- Auto-Git Commit ---

--- cmds/countdown.js
+++ cmds/countdown.js
@@ -193,6 +193,7 @@ module.exports.run = async (gideon, message, args) => {
                         nxbwep1 = `${bwseason}x${bwnumber<10?"0"+bwnumber:bwnumber} - ${bwepname}`;
                         nxbwep2 = `Will air in ${bwdiffDays} ${d} on ${bwad.toDateString()} at ${bwtimeString} ET on ${bwchannel}`;
                         }
+                        
                         const countdown = new Discord.RichEmbed()
 	                    .setColor('#2791D3')
 	                    .setTitle('__Next upcoming Arrowverse episodes:__')
