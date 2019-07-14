5b144cb541fa138d9c88c983c43335a1c8c575b4
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-14 15:36:25 +0200

    --- Auto-Git Commit ---

--- cmds/prefix.js
+++ cmds/prefix.js
@@ -5,7 +5,7 @@ module.exports.run = async (gideon, message, args) => {
     if(!message.member.hasPermission('ADMINISTRATOR')){
         return message.channel.send('You do not have the required permissions to use this command!')
     }   else{
-        db.set(`prefix_${}`)
+        db.set(`prefix_${message.guild.id}`)
     }
 
 }
