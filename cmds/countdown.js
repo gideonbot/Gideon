e117c2057aba69b721d91bfe5062a4b76d83fc2c
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-14 02:49:40 +0200

    --- Auto-Git Commit ---

--- cmds/countdown.js
+++ cmds/countdown.js
@@ -29,7 +29,7 @@ module.exports.run = async (gideon, message, args) => {
         let ampm = (H < 12 || H === 24) ? " AM" : " PM";
         flatimeString = h + flatimeString.substr(2, 3) + ampm;*/
 
-        const fladiffDays = Math.round(Math.abs((today.getTime() - fladate.getTime())/(oneDay)));
+        //const fladiffDays = Math.round(Math.abs((today.getTime() - fladate.getTime())/(oneDay)));
 
         if(fladiffDays === 1){
             d = 'day';
