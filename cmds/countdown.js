6adc96103078e17524975fe4da1b22908b8dfd13
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-14 03:46:24 +0200

    --- Auto-Git Commit ---

--- cmds/countdown.js
+++ cmds/countdown.js
@@ -99,7 +99,7 @@ module.exports.run = async (gideon, message, args) => {
                 snekfetch.get(legendsapi).then(r => {
                     console.log(r.body);
                     let body = r.body;   
-                    const nxep
+                    const nxlgep
                     const lgtitle = body.name;
 
                     if (r.body._embedded.nextepisode === null){
