19edcc9bac5344c9b72d5fb23115aa1e18fbbfa6
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-14 03:28:06 +0200

    --- Auto-Git Commit ---

--- cmds/countdown.js
+++ cmds/countdown.js
@@ -47,8 +47,8 @@ module.exports.run = async (gideon, message, args) => {
             const artitle = body.name;
             const arseason = body._embedded.nextepisode.season;
             const arnumber = body._embedded.nextepisode.number;
-            const flaepname = body._embedded.nextepisode.name;
-            const fladate = body._embedded.nextepisode.airdate;
+            const arepname = body._embedded.nextepisode.name;
+            const aradate = body._embedded.nextepisode.airdate;
             const ad = new Date(fladate);
             console.log(fladate);
             let flatime = body._embedded.nextepisode.airtime;
