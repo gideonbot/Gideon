a4902df618f21e74f04576c6dd753eadd9452fca
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-14 03:26:48 +0200

    --- Auto-Git Commit ---

--- cmds/countdown.js
+++ cmds/countdown.js
@@ -124,7 +124,36 @@ module.exports.run = async (gideon, message, args) => {
                         d = 'days';
                     }
             
-            
+                    snekfetch.get(flashapi).then(r => {
+                        console.log(r.body);
+                        let body = r.body;   
+                
+                        const flatitle = body.name;
+                        const flaseason = body._embedded.nextepisode.season;
+                        const flanumber = body._embedded.nextepisode.number;
+                        const flaepname = body._embedded.nextepisode.name;
+                        const fladate = body._embedded.nextepisode.airdate;
+                        const ad = new Date(fladate);
+                        console.log(fladate);
+                        let flatime = body._embedded.nextepisode.airtime;
+                        const flachannel = body.network.name;
+                
+                        let flatimeString = flatime;
+                        let H = +flatimeString.substr(0, 2);
+                        let h = H % 12 || 12;
+                        let ampm = (H < 12 || H === 24) ? " AM" : " PM";
+                        flatimeString = h + flatimeString.substr(2, 3) + ampm;
+                
+                        const fladiffDays = Math.round(Math.abs((today.getTime() - ad.getTime())/(oneDay)));
+                
+                        if(fladiffDays === 1){
+                            d = 'day';
+                        }   else{
+                            d = 'days';
+                        }
+                
+                
+                    });
                 });
             });
         });
