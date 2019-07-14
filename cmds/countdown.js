bdd8e0a018d052a4d9e75d89589cf64bfb0e3a5a
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-14 02:52:52 +0200

    --- Auto-Git Commit ---

--- cmds/countdown.js
+++ cmds/countdown.js
@@ -40,7 +40,7 @@ module.exports.run = async (gideon, message, args) => {
         const countdown = new Discord.RichEmbed()
 	    .setColor('#2791D3')
 	    .setTitle('__Next upcoming Arrowverse episodes:__')
-        .addTitle(`${flatitle} ${flaseason}x${flanumber<10?"0"+flanumber:flanumber} - ${flaepname}`, `airs in  at ${flatimeString} ET on ${flachannel}`)
+        .addTitle(`${flatitle} ${flaseason}x${flanumber<10?"0"+flanumber:flanumber} - ${flaepname}`, `airs in ${fladiffDays} ${d} on ${fladate.toDateString()} at ${flatimeString} ET on ${flachannel}`)
         //.addTitle(`${artitle} ${areason}x${arnumber<10?"0"+arnumber:arnumber} - ${arepname}`)
         //.addTitle(`${sgtitle} ${sgeason}x${sgnumber<10?"0"+sgnumber:sgnumber} - ${sgepname}`)
         //.addTitle(`${lgtitle} ${lgseason}x${lgnumber<10?"0"+lgnumber:lgnumber} - ${lgepname}`)
