12e2d6c0be04f394bf2b9a3cb16a01204440289f
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-14 03:44:48 +0200

    --- Auto-Git Commit ---

--- cmds/countdown.js
+++ cmds/countdown.js
@@ -160,6 +160,7 @@ module.exports.run = async (gideon, message, args) => {
                         const nxsgep
                         const nxlgep
                         const nxbwep
+                        
                         const countdown = new Discord.RichEmbed()
 	                    .setColor('#2791D3')
 	                    .setTitle('__Next upcoming Arrowverse episodes:__')
