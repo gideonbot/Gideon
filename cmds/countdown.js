d4ccb20d96702013a9b3e0c09c48a6e817550113
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-14 04:01:12 +0200

    --- Auto-Git Commit ---

--- cmds/countdown.js
+++ cmds/countdown.js
@@ -146,7 +146,7 @@ module.exports.run = async (gideon, message, args) => {
                     }
 
                     nxlgep = `${lgseason}x${lgnumber<10?"0"+lgnumber:lgnumber} - ${lgepname}`
-                    `Will air in ${lgdiffDays} ${d} on ${lgad.toDateString()} at ${lgtimeString} ET on ${lgchannel}`;
+                    nxlgep2 = `Will air in ${lgdiffDays} ${d} on ${lgad.toDateString()} at ${lgtimeString} ET on ${lgchannel}`;
             
                     snekfetch.get(bwomanapi).then(r => {
                         console.log(r.body);
