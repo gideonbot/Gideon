44037519a3c1a6dc5df755a4bac4dba3998cd6fb
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-14 02:58:59 +0200

    --- Auto-Git Commit ---

--- cmds/countdown.js
+++ cmds/countdown.js
@@ -20,6 +20,7 @@ module.exports.run = async (gideon, message, args) => {
         const flanumber = body._embedded.number;
         const flaepname = body._embedded.name;
         const fladate = body._embedded.airdate;
+        console.log(r.body);
         let flatime = body._embedded.airtime;
         const flachannel = body.network.name;
 /*
