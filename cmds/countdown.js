277b0328bad83887bf01c899b51791c829615334
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-14 04:31:31 +0200

    --- Auto-Git Commit ---

--- cmds/countdown.js
+++ cmds/countdown.js
@@ -19,7 +19,7 @@ module.exports.run = async (gideon, message, args) => {
         var nxflaep1 = '';
         var nxflaep2 = '';
         
-        if(r.body._embedded === null){
+        if(r.body._embedded == "undefined"){
             nxflaep2 = 'No Episode data available yet';
         }   else{
         
