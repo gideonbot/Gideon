1779e3356525721ad540784e90dd276a4a849af6
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-14 02:03:09 +0200

    --- Auto-Git Commit ---

--- cmds/countdown.js
+++ cmds/countdown.js
@@ -23,7 +23,7 @@ module.exports.run = async (gideon, message, args) => {
         let today = new Date().format('m-d-Y h:i:s');
         const fladiffDays = Math.round(Math.abs((today.getTime() - fladate.getTime())/(oneDay)));
         if(fladiffDays === 1){
-            
+            d = ''
         }
         const countdown = new Discord.RichEmbed()
 	    .setColor('#2791D3')
