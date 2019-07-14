db553537ef4557db5fa582b439013321183a5cef
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-14 03:26:59 +0200

    --- Auto-Git Commit ---

--- cmds/countdown.js
+++ cmds/countdown.js
@@ -152,7 +152,7 @@ module.exports.run = async (gideon, message, args) => {
                             d = 'days';
                         }
                 
-                
+                        
                     });
                 });
             });
