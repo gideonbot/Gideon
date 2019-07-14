3e58d7e4493ad789832a5dc461536de463e6d507
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-14 04:10:54 +0200

    --- Auto-Git Commit ---

--- cmds/countdown.js
+++ cmds/countdown.js
@@ -83,7 +83,7 @@ module.exports.run = async (gideon, message, args) => {
             }
             
             nxarep1 = `${arseason}x${arnumber<10?"0"+arnumber:arnumber} - ${arepname}`;
-            nxarep2 = `Will air in ${ardiffDays} ${d} on ${arad.toDateString()} at ${artimeString} ET on ${lgchannel}`;
+            nxarep2 = `Will air in ${ardiffDays} ${d} on ${arad.toDateString()} at ${artimeString} ET on ${archannel}`;
 
             snekfetch.get(supergirlapi).then(r => {
                 console.log(r.body);
