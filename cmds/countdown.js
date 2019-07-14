120bd047c819b5e042f7ff1bc53930df9c64b216
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-14 03:51:48 +0200

    --- Auto-Git Commit ---

--- cmds/countdown.js
+++ cmds/countdown.js
@@ -46,7 +46,8 @@ module.exports.run = async (gideon, message, args) => {
         snekfetch.get(arrowapi).then(r => {
             console.log(r.body);
             let body = r.body;   
-    
+            
+            
             const artitle = body.name;
             const arseason = body._embedded.nextepisode.season;
             const arnumber = body._embedded.nextepisode.number;
