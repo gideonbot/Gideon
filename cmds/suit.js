359c235940cbab5ee0aade44970e5defc0187d54
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-17 18:42:59 +0200

    add new suits

--- cmds/suit.js
+++ cmds/suit.js
@@ -6,12 +6,21 @@ module.exports.run = async (gideon, message, args) => {
 
     let kfsuit = 'https://i.imgur.com/0XI5gxr.jpg';
     let sgsuit = 'https://i.imgur.com/fZ5iQ9A.jpg';
+    let flasuit = 'https://i.imgur.com/2JDfNlE.jpg';
+    let arsuit = 'https://i.imgur.com/NsnDVC1.jpg';
+    let bwsuit = 'https://i.imgur.com/2vkMLrp.jpg';
 
     let kftitle = 'Killer Frost';
     let sgtitle = 'Supergirl';
+    let flatitle = 'The Flash';
+    let artitle = 'The Green Arrow';
+    let bwtitle = 'Batwoman';
 
-    let kfdesc = 'This is Danielle Panabaker\'s new suit for The Flash Season 5!';
+    let kfdesc = 'This is Danielle Panabaker\'s new suit for The Flash Season 6!';
     let sgdesc = 'This is Melissa Benoist\'s new suit for Supergirl Season 5!';
+    let fladesc = 'This is Grant Gustin\'s new suit for The Flash Season 6!';
+    let ardesc = 'This is Stephen Amell\'s new suit for Arrow Season 8!';
+    let bwdesc = 'This is Ruby Rose\'s new suit for Batwoman Season 1!';
 
     let suitimg;
     let suittle;
@@ -25,6 +34,18 @@ module.exports.run = async (gideon, message, args) => {
         suitimg = sgsuit;
         suittle = sgtitle;
         suitdesc = sgdesc;
+    }   else if(args[0].match(/(?:flash)/i)){
+        suitimg = flasuit;
+        suittle = flatitle;
+        suitdesc = fladesc;
+    }   else if(args[0].match(/(?:arrow)/i)){
+        suitimg = arsuit;
+        suittle = artitle;
+        suitdesc = ardesc;
+    }   else if(args[0].match(/(?:batwoman)/i)){
+        suitimg = bwsuit;
+        suittle = bwtitle;
+        suitdesc = bwdesc;
     }   else{
         return message.channel.send('You must supply a valid character!');
     }
