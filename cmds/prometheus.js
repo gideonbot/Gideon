e930dd648149ac5b5f5c3425f9485948771182fc
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-07-23 20:19:26 +0200

    add more prometheus

---
+++ cmds/prometheus.js
@@ -0,0 +1,27 @@
+const Discord = module.require("discord.js");
+const Imgur = require('imgur-node');
+const imgclient = new Imgur.Client('fbe1de02978b3b4');
+
+module.exports.run = async (gideon, message, args) => {      
+    imgclient.album.get('ZU6IgLo', (err, res) => {
+    if (err) console.error(err);
+    console.log(res);  
+    let min = 0;
+    let max = res.images.length - 1;
+    let ranum = Math.floor(Math.random()*(max - min + 1)) + min;
+    let ravm = res.images[ranum].link;
+    console.log(ravm);
+
+    const meme = new Discord.RichEmbed()
+        .setColor('#2791D3')
+        .setImage(ravm)
+        .setTimestamp()
+        .setFooter('The Arrowverse Bot | Time Vault Discord | Developed by adrifcastr', 'https://i.imgur.com/3RihwQS.png');
+
+        message.channel.send(meme);
+  });   
+}
+
+module.exports.help = {
+    name: "prometheus"
+}
