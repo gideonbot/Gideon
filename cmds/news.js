d95da0e69c58ae319d4cb75296a6297e71325569
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-08-13 18:10:17 +0200

    update dependencies

--- cmds/news.js
+++ cmds/news.js
@@ -2,9 +2,8 @@ const Discord = module.require("discord.js");
 
 module.exports.run = async (gideon, message, args) => {
     if(message.guild.id !== '595318490240385037') return message.channel.send('This command only works at the Time Vault!\nhttps://discord.gg/h9SEQaU');
-    if(!message.member.roles.has('602311948809273344')){
-        return message.channel.send('You don\'t have the required permissions to use this command!');
-    }   else{
+    if(!message.member.roles.has('602311948809273344')) return message.channel.send('You don\'t have the required permissions to use this command!');
+  
     const auth = message.author.id;
     const flash = message.guild.roles.get('596074712682070061');
     const arrow = message.guild.roles.get('596075000151277568');
@@ -148,7 +147,6 @@ module.exports.run = async (gideon, message, args) => {
         if(reason === 'time') message.channel.send("You ran out of time!");
       });
     }
-}
 
 module.exports.help = {
     name: "news"
