4514838a751931dba7b0fac8bb68adbc2dce26a3
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-08-14 18:39:29 +0200

    fix !demo

--- cmds/demo.js
+++ cmds/demo.js
@@ -1,13 +1,14 @@
 const Discord = module.require("discord.js");
 
 module.exports.run = async (gideon, message, args) => {
+    const avih = gideon.guilds.get('474179239068041237');
 
     const github = new Discord.MessageEmbed()
     .setColor('#2791D3')
     .setTitle('Arrowverse: Infinite Heroes - Demo Version')
     .setDescription('Click the link below to download the Arrowverse: Infinite Heroes demo version!')
-    .setThumbnail('https://cdn.discordapp.com/icons/474179239068041237/guild_icon.png')
-    .addField('AVIH Demo:', `**[Download]('https://serebii.net/index2.shtml' 'https://serebii.net/index2.shtml')**`)
+    .setThumbnail(avih.iconURL())
+    .addField('AVIH Demo:', `**[Download](https://serebii.net/index2.shtml 'https://serebii.net/index2.shtml')**`)
     .setTimestamp()
     .setFooter('The Arrowverse Bot | Time Vault Discord | Developed by adrifcastr', 'https://i.imgur.com/3RihwQS.png')
 
