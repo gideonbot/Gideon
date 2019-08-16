de0f8afc9788bd031adb40c5a62cbdf746d3bb14
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-08-16 21:55:27 +0200

    simplify !course

--- cmds/course.js
+++ cmds/course.js
@@ -5,24 +5,18 @@ module.exports.run = async (gideon, message, args) => {
     if(args.join("") === 'acourse!'){
     message.channel.send('Yes Captain Lance!');
 
-    const destapi = 'https://api.myjson.com/bins/ca0o7';
-
-    const dbody = await fetch(destapi).then(res => res.json()); 
-        
+    const destapi1 = 'http://geodb-free-service.wirefreethought.com/v1/geo/cities?hateoasMode=off';
+    const dbody1 = await fetch(destapi1).then(res => res.json()); 
+    const totalcount = dbody1.metadata.totalCount;
     let dmin = 0;
-    let dmax = dbody.length - 1;
-    let radest = Math.floor(Math.random()*(dmax - dmin + 1)) + dmin;
-    const destination = `${dbody[radest].city}, ${dbody[radest].country}`;
-    
-    const timeapi = 'https://api.myjson.com/bins/p4zc7';
-
-    const tbody = await fetch(timeapi).then(res => res.json());
+    let dmax = totalcount - 1;
+    const offset = Math.floor(Math.random()*(dmax - dmin + 1)) + dmin;;
+    const destapi2 = `http://geodb-free-service.wirefreethought.com/v1/geo/cities?limit=1&offset=0${offset}&hateoasMode=off`;
+    const dbody2 = await fetch(destapi2).then(res => res.json());
+    const destination = `${dbody2.data[0].city}, ${dbody2.data[0].country}`;
     let tmin = 0;
-    let tmax = tbody.length - 1;
-    let ratime = Math.floor(Math.random()*(tmax - tmin + 1)) + tmin;
-
-    const time = `${tbody[ratime].date}`;
-    const year = time.substr(-4);
+    let tmax = 3000;
+    let year = Math.floor(Math.random()*(tmax - tmin + 1)) + tmin;
 
     const course = new Discord.MessageEmbed()
     .setColor('#2791D3')
