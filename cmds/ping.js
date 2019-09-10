4afd529f80184e20c6f7f4093ce754b76c758923
MBR <buildmachineubuntu@gmail.com>
2019-09-10 19:54:34 +0200

    ping uses toFixed to prevent weird stuff

--- cmds/ping.js
+++ cmds/ping.js
@@ -4,7 +4,7 @@ module.exports.run = async (gideon, message, args) => {
     const ping = new Discord.MessageEmbed()
     .setColor('#2791D3')
     .setTitle('The fastest bot alive!')
-    .setDescription(`${gideon.ws.ping} ms`)
+    .setDescription(`${gideon.ws.ping.toFixed(2)} ms`)
     .setTimestamp()
     .setFooter('The Arrowverse Bot | Time Vault Discord | Developed by adrifcastr', gideon.user.avatarURL())
 
