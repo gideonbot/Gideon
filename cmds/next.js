dd56415a43a597522b7004a1831ee3fabb51b7c6
MBR <buildmachineubuntu@gmail.com>
2019-09-11 18:45:47 +0200

    rewrite subs to be less messy, change next to work a bit differently because API it is fetching info from is broken lmao

--- cmds/next.js
+++ cmds/next.js
@@ -5,8 +5,8 @@ const Util = require("../Util");
 module.exports.run = async (gideon, message, args) => {
     let agc = args[0];
     if (!agc) return message.channel.send("You must supply the shows name, season and its episode number!");
-    const api = 'https://arrowverse.info/api?newest_first=False&hide_show=freedom-fighters&hide_show=vixen&from_date=&to_date=';
     const url = 'https://arrowverse.info';
+    const api = 'https://arrowverse.info/api';
     let showtitle;
     let thimg;
 
@@ -31,47 +31,51 @@ module.exports.run = async (gideon, message, args) => {
     else if (agc.match(/(?:constantine)/i)) showtitle = "Constantine";
     else if (agc.match(/(?:batwoman)/i)) showtitle = "Batwoman";
     else if (agc.match(/(?:blacklightning)/i)) showtitle = "Black Lightning";
-    else if (agc.match(/(?:av2020)/i)) {
-        showtitle = "av2020"; 
-        thimg = '';
-    } 
+    else if (agc.match(/(?:av2020)/i)) showtitle = "av2020";
     else return message.channel.send(as);
 
-    const body = await fetch(api).then(res => res.json());
-    const fiep = Util.ParseStringToId(args[1]);
-    if (!fiep) return message.channel.send(es);
+    try {
+        const fiep = Util.ParseStringToId(args[1]);
+        if (!fiep) return message.channel.send(es);
 
-    let filtered = body.filter(x => x.series == showtitle);
+        const body = await fetch(api).then(res => res.json());
 
-    let f = filtered.find(x => x.episode_id == fiep);
-    if (!f) return message.channel.send(`${showtitle} ${fiep} is not a valid episode!`);
-    let rownum = f.row_number;
-    let next = body.find(x => x.row_number == rownum + 1);
+        let shows = body.filter(x => x.series != 'Vixen' && x.series != 'Freedom Fighters: The Ray');
+    
+        let f = shows.find(x => x.series == showtitle && x.episode_id == fiep);
+        if (!f) return message.channel.send(`${showtitle} ${fiep} is not a valid episode!`);
 
-    if (!next) return message.channel.send('Couldn\'t find that episode. Try again.');
+        let next = shows[shows.indexOf(f) + 1];
+        if (!next) return message.channel.send('Couldn\'t find that episode. Try again.');
+    
+        const nxep = `${next.series} ${next.episode_id} - ${next.episode_name}`;
+        const nxepard = `Airdate: ${next.air_date}`;
+    
+        if (next.series.match(/(?:flash)/i)) thimg = 'https://i.ytimg.com/vi/ghPatoChvV0/maxresdefault.jpg';
+        else if (next.series.match(/(?:arrow)/i)) thimg = 'http://www.greenarrowtv.com/wp-content/uploads/2017/10/Screen-Shot-2017-10-19-at-6.50.41-PM.jpg';
+        else if (next.series.match(/(?:supergirl)/i)) thimg = 'https://i0.wp.com/thegameofnerds.com/wp-content/uploads/2018/01/supergirl-title-card1.png?resize=560%2C315&ssl=1';
+        else if (next.series.match(/(?:legends)/i)) thimg = 'https://i.imgur.com/FLqwOYv.png';
+        else if (next.series.match(/(?:constantine)/i)) thimg = 'https://upload.wikimedia.org/wikipedia/en/b/b1/Constantine_TV_show_logo.jpg';
+        else if (next.series.match(/(?:batwoman)/i)) thimg = 'https://upload.wikimedia.org/wikipedia/en/c/c3/Batwoman_TV_series_logo.png';
+        else if (next.series.match(/(?:blacklightning)/i)) thimg = 'https://upload.wikimedia.org/wikipedia/en/e/ef/Black_Lightning_%28TV_series%29.png';
+        else if (next.series.match(/(?:av2020)/i)) thimg = '';
+    
+        const nextmsg = new Discord.MessageEmbed()
+        .setColor('#2791D3')
+        .setTitle('Your next episode:')
+        .setThumbnail(thimg)
+        .addField(nxep, nxepard)
+        .addField(`Powered by:`, `**[arrowverse.info](${url} '${url}')**`)
+        .setTimestamp()
+        .setFooter('The Arrowverse Bot | Time Vault Discord | Developed by adrifcastr', gideon.user.avatarURL());
+    
+        message.channel.send(nextmsg);
+    }
 
-    const nxep = `${next.series} ${next.episode_id} - ${next.episode_name}`;
-    const nxepard = `Airdate: ${next.air_date}`;
-
-    if (next.series.match(/(?:flash)/i)) thimg = 'https://i.ytimg.com/vi/ghPatoChvV0/maxresdefault.jpg';
-    else if (next.series.match(/(?:arrow)/i)) thimg = 'http://www.greenarrowtv.com/wp-content/uploads/2017/10/Screen-Shot-2017-10-19-at-6.50.41-PM.jpg';
-    else if (next.series.match(/(?:supergirl)/i)) thimg = 'https://i0.wp.com/thegameofnerds.com/wp-content/uploads/2018/01/supergirl-title-card1.png?resize=560%2C315&ssl=1';
-    else if (next.series.match(/(?:legends)/i)) thimg = 'https://i.imgur.com/FLqwOYv.png';
-    else if (next.series.match(/(?:constantine)/i)) thimg = 'https://upload.wikimedia.org/wikipedia/en/b/b1/Constantine_TV_show_logo.jpg';
-    else if (next.series.match(/(?:batwoman)/i)) thimg = 'https://upload.wikimedia.org/wikipedia/en/c/c3/Batwoman_TV_series_logo.png';
-    else if (next.series.match(/(?:blacklightning)/i)) thimg = 'https://upload.wikimedia.org/wikipedia/en/e/ef/Black_Lightning_%28TV_series%29.png';
-    else if (next.series.match(/(?:av2020)/i)) thimg = '';
-
-    const nextmsg = new Discord.MessageEmbed()
-    .setColor('#2791D3')
-    .setTitle('Your next episode:')
-    .setThumbnail(thimg)
-    .addField(nxep, nxepard)
-    .addField(`Powered by:`, `**[arrowverse.info](${url} '${url}')**`)
-    .setTimestamp()
-    .setFooter('The Arrowverse Bot | Time Vault Discord | Developed by adrifcastr', gideon.user.avatarURL());
-
-    message.channel.send(nextmsg);
+    catch (ex) {
+        console.log(ex);
+        return message.channel.send("Failed to fetch data, please try again later!");
+    }
 }
 
 module.exports.help = {
