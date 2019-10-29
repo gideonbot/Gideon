d0ae236a7f0114595c68233e01440b654777965b
7coil <leondrolio@gmail.com>
2019-10-29 23:56:20 +0000

    Ditto
    Refer back to #23 for details

--- cmds/eval.js
+++ cmds/eval.js
@@ -1,27 +1,43 @@
-const Discord = module.require("discord.js");
-
-module.exports.run = async (gideon, message, args) => {   
-    if(message.author.id !== '224617799434108928') return message.channel.send('You don\'t have the required permissions to use this command!');
-
-    function clean(text) {
-        if (typeof(text) === "string") return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-        else return text;
+module.exports.run = async (gideon, message, args) => {
+    if (message.author.id !== '224617799434108928') {
+        return message.channel.send('You do not have the required permissions to use this command!');
     }
 
     try {
-        const code = args.join(" ");
-        let evaluated = eval(code);
-   
-        if (typeof evaluated !== "string") evaluated = JSON.stringify(evaluated, null, 2);
-   
-        message.channel.send(clean(evaluated), {code:"xl"});
-    }
+        // Rejoin the parameters
+        const code = args.join(' ');
+        // Evaluate the JavaScript
+        const returnedValue = eval(code);
 
-    catch (err) {
-        message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+        // If nothing was returned, say so.
+        if (typeof returnedValue === 'undefined') {
+            message.channel.send('The evaluated code returned nothing.');
+            return;
+        }
+
+        // Generate a string worthy of being printed.
+        let printValue;
+
+        if (typeof returnedValue === 'string') {
+            // Return the string if the result was a string
+            printValue = returnedValue;
+        } else if (typeof returnedValue === 'object') {
+            // Return the object if the result is an object, an instance of a class, basically anything JavaScript loves.
+            printValue = JSON.stringify(returnedValue, null, 2);
+        } else {
+            // Force turn the thing into a String.
+            printValue = new String(returnedValue);
+        }
+
+        // Send the returned value.
+        message.channel.send(printValue, {
+            code: true
+        });
+    } catch (e) {
+        message.channel.send(`An error occured while processing your request: \`\`\`\n${e.stack}\n\`\`\``);
     }
 }
 
 module.exports.help = {
-    name: "eval"
+    name: 'eval'
 }
