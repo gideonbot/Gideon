fe3ebf4c38979a01a2bafb87295577f19cfe5438
adrifcastr <adrian.f.castro.mail@gmail.com>
2019-11-01 14:16:57 +0100

    add more deckerstar

--- Util.js
+++ Util.js
@@ -246,12 +246,47 @@ class Util {
     }
 
     /**
-     * Lucifer easter egg
+     * Lucifer easter eggs
      * @param {Discord.Message} message 
      */
     static async CSD(message) {
         const vid = 'https://cdn.discordapp.com/attachments/525341082435715085/638782331791867930/Crime_Solving_Devil.mp4';
         if (message.content.match(/(?:devil)/i)) message.channel.send(vid);
+
+        if (message.content.match(/(?:deckerstar)/i)) {
+            const Imgur = require('imgur-node');
+            const imgclient = new Imgur.Client(process.env.IMG_CL);
+
+            imgclient.album.get('rJpbLQx', (err, res) => {
+                if (err) {
+                    console.log(err);
+                    Util.log(err);
+        
+                    const er = new Discord.MessageEmbed()
+                    .setColor('#2791D3')
+                    .setTitle('An error occurred, please try again later!')
+                    .setTimestamp()
+                    .setFooter(Util.config.footer, gideon.user.avatarURL());
+                    return message.channel.send(er);
+                }
+        
+                let min = 0;
+                let max = res.images.length - 1;
+                let ranum = Math.floor(Math.random() * (max - min + 1)) + min;
+                let rds = res.images[ranum].link;
+            
+                const ds = new Discord.MessageEmbed()
+                .setColor('#2791D3')
+                .setImage(rds)
+                .setTimestamp()
+                .setFooter(Util.config.footer, gideon.user.avatarURL());
+            
+                message.channel.send(ds);
+            });
+        }
+        
+        const tls = 'https://twitter.com/LaurenGerman/status/996886094305050627\nhttps://twitter.com/tomellis17/status/996889307506864128';
+        if (message.content.match(/(?:muffin)/i) && message.content.match(/(?:top)/i)) message.channel.send(tls);
     }
 
     //more methods to come
