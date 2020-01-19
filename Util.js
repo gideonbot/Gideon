1317c4e93e5f0b76b9161614e3cafb5342bdf2e4
adrifcastr <adrian.f.castro.mail@gmail.com>
2020-01-19 19:52:10 +0100

    move GetNextEpisodeInfo to Util

--- Util.js
+++ Util.js
@@ -360,6 +360,77 @@ class Util {
             message.channel.send(imgembed);
         }
     }
+
+    /**
+     * Get episode info 
+     * @returns {Promise<{title: string, name: string, value: string, error: null}>}
+     * @param {string} api_url 
+     */
+    static async GetNextEpisodeInfo(api_url) {
+        return new Promise((resolve, reject) => {
+            if (!api_url) return reject({error: "Missing API URL"});
+            
+            fetch(api_url).then(res => {
+                if (res.status !== 200) return reject({error: res.statusText});
+
+                res.json().then(body => {
+                    let title = body.name;
+    
+                    let result = { title: title, name: null, value: null, error: null };
+    
+                    if (!body._embedded) {
+                        result.name = '';
+                        result.value = 'No Episode data available yet';
+                    }
+    
+                    else {
+                        let season = body._embedded.nextepisode.season;
+                        let number = body._embedded.nextepisode.number;
+                        let name = body._embedded.nextepisode.name;
+                        let date = new Date(body._embedded.nextepisode.airstamp);
+                        let channel = body.network.name;
+
+                        let time_diff_s = Math.abs(new Date() - date) / 1000;
+
+                        let airs_today = time_diff_s < 60 * 60 * 24;
+                        
+                        let res_value = `Airs in **${Util.secondsToDifferenceString(time_diff_s, {
+                            enableSeconds: false
+                        })}**`;
+
+                        if (!airs_today) {
+                            //this is how we turn
+                            //Wed, 09 Oct 2019 10:00:00 GMT
+                            //into
+                            //9 Oct 2019 10:00
+                            let _date = date.toUTCString().replace("GMT", "");
+                            //remove "Wed, " (5)
+                            _date = _date.substr(5);
+
+                            //remove the last :00
+                            _date = _date.split(":");
+                            _date.pop();
+                            _date = _date.join(":");
+
+                            //thankfully, the .replace method does not work as you would expect it to
+                            //you would expect it to remove all searchValues from the string, right?
+                            //WRONG, it only removes the first searchValue (lol)
+                            if (_date.startsWith("0")) _date = _date.replace("0", "");
+
+                            res_value += ` (\`${_date} UTC\`)`;
+                        }
+                        
+                        res_value += ` on ${channel}`;
+
+                        result.name = `${season}x${number < 10 ? `0` + number : number} - ${name}`;
+                        result.value = res_value;
+                    }
+    
+                    return resolve(result);
+                }, failed => reject({error: failed}));
+            }, failed => reject({error: failed}));
+        });
+    }
     //more methods to come
 }
 module.exports = Util;
