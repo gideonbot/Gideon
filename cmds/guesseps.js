24fd8f59f543bda349a84aceb8c9c02eea67698c
adrifcastr <adrian.f.castro.mail@gmail.com>
2020-01-25 23:45:19 +0100

    make guesseps.js run entirely using an embed

--- cmds/guesseps.js
+++ cmds/guesseps.js
@@ -6,14 +6,18 @@ const SQLite = require("better-sqlite3");
 const sql = new SQLite('./data/scores.sqlite');
 
 module.exports.run = async (gideon, message, args) => {
+    const url = 'https://arrowverse.info';
+    const api = "https://arrowverse.info/api";
     const emotes = ['stop', '▶️'];
     const stopid = '669309980209446912';
+    let s = ['guess', 'second', 'point', 'try', 'tries', 'got', 'had'];
     const auth = message.author.id;
     let chosenfilter;
     let tries = 3;
     let timerstart;
     let timerdiff;
     let timervalue;
+    timerstart = new Date();
 
     const as = new Discord.MessageEmbed()
     .setColor('#2791D3')
@@ -29,42 +33,24 @@ module.exports.run = async (gideon, message, args) => {
     .setFooter(Util.config.footer, gideon.user.avatarURL());
 
     let agc = args[0];
-    let filters = [
-        {
-            filter: (x => x.series !== 'Vixen' && x.series !== 'Freedom Fighters: The Ray')
-        },
-        {
-            filter: (x => x.series == 'The Flash')
-        },
-        {
-            filter: (x => x.series == 'Arrow')
-        },
-        {
-            filter: (x => x.series == 'DC\'s Legends of Tomorrow')
-        },
-        {
-            filter: (x => x.series == 'Supergirl')
-        },
-        {
-            filter: (x => x.series == 'Black Lightning')
-        },
-        {
-            filter: (x => x.series == 'Batwoman')
-        },
-        {
-            filter: (x => x.series == 'Constantine')
-        }
-    ]
+    let filters = [(x => x.series !== 'Vixen' && x.series !== 'Freedom Fighters: The Ray'),
+        (x => x.series == 'The Flash'),
+        (x => x.series == 'Arrow'),
+        (x => x.series == 'DC\'s Legends of Tomorrow'),
+        (x => x.series == 'Supergirl'),
+        (x => x.series == 'Black Lightning'),
+        (x => x.series == 'Batwoman'),
+        (x => x.series == 'Constantine')]
 
-    let score = gideon.getScore.get(message.author.id);
-    if (!score) {
-        score = {
-          id: `${message.author.id}`,
-          user: message.author.id,
-          guild: message.guild.id,
-          points: 0
+        let score = gideon.getScore.get(message.author.id);
+        if (!score) {
+            score = {
+            id: `${message.author.id}`,
+            user: message.author.id,
+            guild: message.guild.id,
+            points: 0
+            }
         }
-    }
 
     let command = message.content.toLowerCase().split(' ')[0];
 
@@ -80,30 +66,34 @@ module.exports.run = async (gideon, message, args) => {
         .setTitle(`Top 10 Leaderboard:`)
         .setTimestamp()
         .setFooter(Util.config.footer, gideon.user.avatarURL());
-
         
         for(let [i=0, data] of top10.entries()){
             i++;
             const guild = gideon.guilds.get(data.guild);
-            leaderboard.addField(`#\`${i < 10 ? "0" + i : i}\` User: \`${gideon.users.get(data.user).tag}\` Server: \`${guild.name}\``, `\`${data.points}\` point(s)`);
+            leaderboard.addField(`#\`${i < 10 ? "0" + i : i}\` User: \`${gideon.users.get(data.user).tag}\` Server: \`${guild.name}\``, `\`${data.points}\` ${data.points > 1 ? s[2] + "s" : s[2]}`);
         }
 
         return message.channel.send(leaderboard);
     }
 
-    if (!agc) chosenfilter = filters[0].filter;
-    else if (agc.match(/(?:flash)/i)) chosenfilter = filters[1].filter;
-    else if (agc.match(/(?:arrow)/i)) chosenfilter = filters[2].filter;
-    else if (agc.match(/(?:legends)/i)) chosenfilter = filters[3].filter;
-    else if (agc.match(/(?:supergirl)/i)) chosenfilter = filters[4].filter;
-    else if (agc.match(/(?:blacklightning)/i)) chosenfilter = filters[5].filter;
-    else if (agc.match(/(?:batwoman)/i)) chosenfilter = filters[6].filter;
-    else if (agc.match(/(?:constantine)/i)) chosenfilter = filters[7].filter;
+    if (!agc) chosenfilter = filters[0];
+    else if (agc.match(/(?:flash)/i)) chosenfilter = filters[1];
+    else if (agc.match(/(?:arrow)/i)) chosenfilter = filters[2];
+    else if (agc.match(/(?:legends)/i)) chosenfilter = filters[3];
+    else if (agc.match(/(?:supergirl)/i)) chosenfilter = filters[4];
+    else if (agc.match(/(?:blacklightning)/i)) chosenfilter = filters[5];
+    else if (agc.match(/(?:batwoman)/i)) chosenfilter = filters[6];
+    else if (agc.match(/(?:constantine)/i)) chosenfilter = filters[7];
     else return message.channel.send(as);
 
+    async function Countdown(){
+        let now = new Date();
+        timerdiff = (now.getTime() - timerstart.getTime()) / 1000;
+        timervalue = Math.round(30 - timerdiff);
+        return timervalue;
+    }
+
     async function GameEmbed(showfilter){
-        const url = 'https://arrowverse.info';
-        const api = "https://arrowverse.info/api";
         const body = await fetch(api).then(res => res.json());
 
         const shows = body.filter(showfilter);
@@ -115,7 +105,8 @@ module.exports.run = async (gideon, message, args) => {
 
         const gameembed = new Discord.MessageEmbed()
         .setColor('#2791D3')
-        .setTitle(`Guessing game started for ${message.author.tag}:`)
+        .setTitle(`Guessing game for ${message.author.tag}:`)
+        .setAuthor(`You've got ${tries} ${tries !== 1 ? s[4] : s[3]} and ${await Countdown()} ${await Countdown() > 1 ? s[1] + "s" : s[1]} left!`, message.author.avatarURL())
         .setDescription(`Please guess the following Arrowverse episode's name:\n\`${show} ${epnum}\`\n\n(Press :arrow_forward: to skip this episode or <:stop:669309980209446912> to end this round)`)
         .addField(`Powered by:`, `**[arrowverse.info](${url} '${url}')**`)
         .setTimestamp()
@@ -136,47 +127,100 @@ module.exports.run = async (gideon, message, args) => {
 
             const rfilter = (reaction, user) => emotes.includes(reaction.emoji.name) && user.id === auth;
             const rcollector = sent.createReactionCollector(rfilter, {time: 30 * 1000});
-            timerstart = new Date();
         
             rcollector.on('collect', async (reaction, user, reactionCollector) => {
                 if (reaction.emoji.name === '▶️') {
-                    let updateembed = await GameEmbed(chosenfilter);
-                    await sent.edit(updateembed[0]);
-                    sent.reactions.find(x => x.emoji.name === "▶️").users.remove(user.id);
-                    collector.resetTimer();
-                    rcollector.resetTimer();
-                    embed = updateembed;
                     tries = 3;
+                    let updateembed = await GameEmbed(chosenfilter);
+                    await sent.reactions.find(x => x.emoji.name === "▶️").users.remove(user.id);
+                    await collector.resetTimer();
+                    await rcollector.resetTimer();
+                    await sent.edit(updateembed[0]);
+                    timerstart = new Date();
+                    embed = updateembed;
                 }
                 if (reaction.emoji.name === 'stop') {
-                    collector.stop();
-                    return message.reply('your game round has been cancelled! :white_check_mark:');
+                    await collector.stop();
+                    await sent.reactions.removeAll();
+
+                    const stopembed = new Discord.MessageEmbed()
+                    .setColor('#2791D3')
+                    .setTitle(`Guessing game for ${message.author.tag}:`)
+                    .setAuthor(`You've had ${tries} ${tries !== 1 ? s[4] : s[3]} and ${await Countdown()} ${await Countdown() > 1 ? s[1] + "s" : s[1]} left!`, message.author.avatarURL())
+                    .setDescription(`Your game round has been cancelled! :white_check_mark:`)
+                    .addField(`Powered by:`, `**[arrowverse.info](${url} '${url}')**`)
+                    .setTimestamp()
+                    .setFooter(Util.config.footer, gideon.user.avatarURL());
+
+                    await sent.edit(stopembed);
+                    return;
                 }
             }); 
-        });
 
-        collector.on('collect', async message => {
-        const similarity = stringSimilarity.compareTwoStrings(embed[3].toLowerCase().replace(/\s/g, ""), message.content.toLowerCase().replace(/\s/g, ""));
+            collector.on('collect', async message => {
+                const similarity = stringSimilarity.compareTwoStrings(embed[3].toLowerCase().replace(/\s/g, ""), message.content.toLowerCase().replace(/\s/g, ""));
+        
+                    if (similarity >= 0.65) {
+                        await collector.stop();
+                        tries = tries -1;
+                        await score.points++;
+                        await gideon.setScore.run(score);
 
-            if (similarity >= 0.65) {
-                collector.stop();
-                score.points++;
-                gideon.setScore.run(score);
-                return message.reply(`that is correct! :white_check_mark:\n\`${embed[1]} ${embed[2]}\` is titled \`${embed[3]}\`\nYou have gained \`1\` point!`);
-            }
+                        const correctembed = new Discord.MessageEmbed()
+                        .setColor('#2791D3')
+                        .setTitle(`Guessing game for ${message.author.tag}:`)
+                        .setAuthor(`You've had ${tries} ${tries !== 1 ? s[4] : s[3]} and ${await Countdown()} ${await Countdown() > 1 ? s[1] + "s" : s[1]} left!`, message.author.avatarURL())
+                        .setDescription(`That is correct! :white_check_mark:\n\`${embed[1]} ${embed[2]} - ${embed[3]}\`\n\n**You have gained \`1\` point!**`)
+                        .addField(`Powered by:`, `**[arrowverse.info](${url} '${url}')**`)
+                        .setTimestamp()
+                        .setFooter(Util.config.footer, gideon.user.avatarURL());
 
-            if (similarity <= 0.64) {
-                tries = tries -1;
-                let now = new Date();
-                timerdiff = (now.getTime() - timerstart.getTime()) / 1000;
-                timervalue = Math.round(30 - timerdiff);
-                if (tries == 0) return collector.stop(), message.reply(`that is incorrect! :x:\n\`${embed[1]} ${embed[2]}\` is titled \`${embed[3]}\``);
-                else message.reply(`that is incorrect! :x:\nYou have \`${tries}\` guess(es) and \`${timervalue}\` second(s) left!`);
-            }
-        });
+                        await sent.edit(correctembed);
+                        await sent.reactions.removeAll();
+                        return;
+                    }
+        
+                    if (similarity <= 0.64) {
+                        tries = tries -1;
+                        let question = `\`${embed[1]} ${embed[2]}\``;
+                        let solution = `\`${embed[1]} ${embed[2]} - ${embed[3]}\``;
 
-        collector.on('end', (collected, reason) => {
-            if (reason === 'time') return message.reply(`You ran out of time!\n\`${embed[1]} ${embed[2]}\` is titled \`${embed[3]}\``);
+                        const incorrectembed = new Discord.MessageEmbed()
+                        .setColor('#2791D3')
+                        .setTitle(`Guessing game for ${message.author.tag}:`)
+                        .setAuthor(`You've ${tries == 0 ? s[6] : s[5]} ${tries} ${tries !== 1 ? s[4] : s[3]} and ${await Countdown()} ${await Countdown() > 1 ? s[1] + "s" : s[1]} left!`, message.author.avatarURL())
+                        .setDescription(`That is incorrect! :x:\n${tries == 0 ? solution : question}`)
+                        .addField(`Powered by:`, `**[arrowverse.info](${url} '${url}')**`)
+                        .setTimestamp()
+                        .setFooter(Util.config.footer, gideon.user.avatarURL());
+
+                        if (tries == 0){
+                            await collector.stop();
+                            await sent.reactions.removeAll();
+                            await sent.edit(incorrectembed);
+                            return; 
+                        } 
+
+                        else await sent.edit(incorrectembed);
+                    }  
+                });
+        
+                collector.on('end', async (collected, reason) => {
+                if (reason === 'time') {
+                    const timeouttembed = new Discord.MessageEmbed()
+                    .setColor('#2791D3')
+                    .setTitle(`Guessing game for ${message.author.tag}:`)
+                    .setAuthor(`You've had ${tries} ${tries !== 1 ? s[4] : s[3]} left!`, message.author.avatarURL())
+                    .setDescription(`You ran out of time!\n\`${embed[1]} ${embed[2]}\` is titled \`${embed[3]}\``)
+                    .addField(`Powered by:`, `**[arrowverse.info](${url} '${url}')**`)
+                    .setTimestamp()
+                    .setFooter(Util.config.footer, gideon.user.avatarURL());
+
+                    await sent.reactions.removeAll();
+                    await sent.edit(timeouttembed);
+                    return;
+                } 
+            });
         });
     }
     catch (ex) {
