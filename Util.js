40c6bb7dc4f0a6d39e8ec1d6592e68bb48fbbcf8
adrifcastr <adrian.f.castro.mail@gmail.com>
2020-02-01 19:16:41 +0100

    actually commit changed files

--- Util.js
+++ Util.js
@@ -433,6 +433,179 @@ class Util {
             }, failed => reject({error: failed}));
         });
     }
+
+    /**
+     * Leave current VoiceChannel 
+     * @param {Discord.Message} message 
+     */
+    static async LeaveVC(message) {
+        if (!message.guild.me.voice.channel) return;
+        let vcname = message.guild.me.voice.channel.name;
+        message.reply(`now leaving voice channel: \`${vcname}\`!`);
+        await message.guild.me.voice.channel.leave();
+    }
+
+    /**
+     * Voicechannel Speech-To-Text 
+     * @returns {JSON}
+     * @param {ReadableStream} speech 
+     */
+    static async SpeechRecognition(speech) {
+        const { Transform } = require('stream')
+
+        function convertBufferTo1Channel(buffer) {
+        const convertedBuffer = Buffer.alloc(buffer.length / 2)
+
+        for (let i = 0; i < convertedBuffer.length / 2; i++) {
+            const uint16 = buffer.readUInt16LE(i * 4)
+            convertedBuffer.writeUInt16LE(uint16, i * 2)
+        }
+
+        return convertedBuffer
+        }
+
+        class ConvertTo1ChannelStream extends Transform {
+        constructor(source, options) {
+            super(options)
+        }
+
+        _transform(data, encoding, next) {
+            next(null, convertBufferTo1Channel(data))
+        }
+        }
+
+        const convertTo1ChannelStream = new ConvertTo1ChannelStream()
+        const rawaudio = speech.pipe(convertTo1ChannelStream);
+
+        const api = 'https://api.wit.ai/speech';
+        const content_type = 'audio/raw;encoding=signed-integer;bits=16;rate=48000;endian=little';
+
+        const headers = {'Content-Type': content_type,
+                         'Authorization': 'Bearer ' + process.env.WITAI_TOKEN,
+                         'Accept': 'application/vnd.wit.' + '20170217'
+        }
+
+        const options = {
+            method: 'POST',
+            body: rawaudio,
+            headers: headers,
+        }
+
+        let result = await fetch(api, options)
+        .then(res => res.json());
+
+        console.log(result);
+        return result;
+    }
+
+    /**
+     * Audio Response/Voice cmd exec 
+     * @param {string} intent 
+     * @param {Discord.VoiceConnection} connection 
+     * @param {Discord.Message} message
+     * @param {Discord.Client} gideon
+     */
+    static async VoiceResponse(value, connection, message, gideon) {
+        const randomFile = require('select-random-file');
+        
+        if (value == 'wakeword') {
+            gideon.vcmdexec = true;
+            const orders = connection.play('./data/audio/captain/Awaiting orders, Captain.mp3');
+            orders.pause();
+            orders.resume();
+
+            orders.on('finish', async () => {
+                orders.destroy();
+                await message.reply('voice command succesfully executed!');
+                gideon.vcmdexec = false;
+            }); 
+            return;
+        }
+        if (value == 'talk') {
+            gideon.vcmdexec = true;
+            const dir = './data/audio/phrases';
+            await randomFile(dir, async (err, file) => {
+                let rfile = `${dir}/${file}`;
+                const phrase = connection.play(rfile);
+                phrase.pause();
+                phrase.resume();
+
+                phrase.on('finish', async () => {
+                    phrase.destroy();
+                    await message.reply('voice command succesfully executed!');
+                    gideon.vcmdexec = false;
+                }); 
+            })
+            return;
+        }
+        if (value == 'leave') {
+            gideon.vcmdexec = true;
+            const leave = connection.play('./data/audio/captain/Yes, Captain.mp3');
+            leave.pause();
+            leave.resume();
+
+            leave.on('finish', async () => {
+                leave.destroy();
+                await message.reply('voice command succesfully executed!');
+                await Util.LeaveVC(message);
+            });
+            return;
+        }
+        if (value == 'timejump') {
+            gideon.vcmdexec = true;
+            const confirm = connection.play('./data/audio/captain/Right away, Captain!.mp3');
+            confirm.pause();
+            confirm.resume();
+
+            confirm.on('finish', async () => {
+                confirm.destroy();
+
+                const timejump = connection.play('./data/audio/phrases/Executing timejump now.mp3');
+                timejump.pause();
+                timejump.resume();
+
+                timejump.on('finish', async () => {
+                    timejump.destroy();
+                    const command = gideon.commands.get('plot');
+                    if (command) await command.run(gideon, message);
+                    await message.reply('voice command succesfully executed!');
+                    gideon.vcmdexec = false;
+                });
+            });
+            return;
+        }
+        if (value == 'future') {
+            gideon.vcmdexec = true;
+            const confirm = connection.play('./data/audio/captain/Right away, Captain!.mp3');
+            confirm.pause();
+            confirm.resume();
+
+            confirm.on('finish', async () => {
+                confirm.destroy();
+                const command = gideon.commands.get('show');
+                if (command) await command.run(gideon, message);
+                await message.reply('voice command succesfully executed!');
+                gideon.vcmdexec = false;
+            });
+            return;
+        }
+        if (value == 'nxeps') {
+            gideon.vcmdexec = true;
+            const confirm = connection.play('./data/audio/captain/Right away, Captain!.mp3');
+            confirm.pause();
+            confirm.resume();
+
+            confirm.on('finish', async () => {
+                confirm.destroy();
+                const command = gideon.commands.get('nxeps');
+                if (command) await command.run(gideon, message);
+                await message.reply('voice command succesfully executed!');
+                gideon.vcmdexec = false;
+            });
+            return;
+        }
+        else return;
+    }
     //more methods to come
 }
 module.exports = Util;
--- cmds/voice.js
+++ cmds/voice.js
@@ -1,10 +1,18 @@
 const Discord = module.require("discord.js");
 const Util = require("../Util");
-const randomFile = require('select-random-file');
-const fetch = require('node-fetch');
+const { Readable } = require('stream');
+const SILENCE_FRAME = Buffer.from([0xF8, 0xFF, 0xFE]);
+
+class Silence extends Readable {
+  _read() {
+    this.push(SILENCE_FRAME);
+    this.destroy();
+  }
+}
 
 module.exports.run = async (gideon, message, args) => {
     let command = message.content.toLowerCase().split(' ')[0];
+    let awake = false;
 
     const er = new Discord.MessageEmbed()
     .setColor('#2791D3')
@@ -12,82 +20,79 @@ module.exports.run = async (gideon, message, args) => {
     .setTimestamp()
     .setFooter(Util.config.footer, gideon.user.avatarURL());
 
+    const voicehelp = new Discord.MessageEmbed()
+    .setColor('#2791D3')
+    .setAuthor(`Note: You must wake Gideon up before using any commands!`, message.author.avatarURL())
+    .setTitle(`Available Voice Commands:`)
+    .addField(`Wakeword:`,`Say something like:\n\`\`\`'Gideon'\n'Hey Gideon'\n'Hello Gideon'\n etc...\`\`\`\nGideon will be ready!`, true)
+    .addField(`Random Phrases:`,`Say something like:\n\`\`\`'Talk'\n'Talk to me'\n'Say something'\n etc...\`\`\`\nGideon will randomly respond!`, true)
+    .addField(`Time Travel:`,`Say something like:\n\`\`\`'Plot a course'\n'Time travel'\n'Timejump'\n etc...\`\`\`\nGideon will plot a course!`, true)
+    .addField(`Future:`,`Say something like:\n\`\`\`'Show me the future'\n'What's the future'\n'future'\n etc...\`\`\`\nGideon show you the future!`, true)
+    .addField(`Upcoming Eps:`,`Say something like:\n\`\`\`'Upcoming Episodes'\n'Next Arrowverse episodes'\n'Next episodes'\n etc...\`\`\`\nGideon will show you the next eps!`, true)
+    .addField(`Leave VC:`,`Say something like:\n\`\`\`'Leave'\n'Stop'\n'Get out'\n etc...\`\`\`\nGideon will leave the VC!`, true)
+    .setTimestamp()
+    .setFooter(Util.config.footer, gideon.user.avatarURL());
+
     if (command.endsWith('leave')) {
-        if (!message.guild.me.voice.channel) return;
-        let vcname = message.guild.me.voice.channel.name;
-        message.reply(`now leaving voice channel: \`${vcname}\`!`);
-        await message.guild.me.voice.channel.leave();
-        return;
+       await Util.LeaveVC(message);
+       return;
+    }
+    else if (args[0] == 'help') {
+       await message.channel.send(voicehelp);
+       return;
     }
     else 
 
     if (!message.member.voice.channel) return message.reply('You need to join a voice channel first!');
+
+    async function stopTimout() {
+        if (awake) {
+            clearTimeout(checkWake);
+        }
+    }
+
+    async function checkWake() {
+        if (awake == true){
+            await stopTimout();
+        } 
+        else {
+            await Util.LeaveVC(message);
+            return message.reply('no wakeword detected the last `20` seconds after joining VC!');
+        }
+    }
     
     try {
         let vcname = message.member.voice.channel.name;
         message.reply(`now joining voice channel: \`${vcname}\`!`);
         const connection = await message.member.voice.channel.join();
-/*
-        const enableinput = connection.play('./data/audio/misc/silence.mp3');
-        enableinput.pause();
-        enableinput.resume();
+        connection.play(new Silence(), { type: 'opus' });//enable voice receive by sending silence buffer
 
-        enableinput.on('finish', async () => {
-            enableinput.destroy();
-        }); 
+        await message.channel.send(Util.GetUserTag(message.author), { embed: voicehelp });
 
-        async function SpeechRecognition(speech){
-            const api = 'https://api.wit.ai/speech';
-            const content_type = 'audio/raw;encoding=signed-integer;bits=16;rate=48000;endian=little';
-
-            const headers = {'Content-Type': content_type,
-                             'Authorization': 'Bearer ' + process.env.WITAI_TOKEN,
-                             'Accept': 'application/vnd.wit.' + '20170217'
-            }
-
-            const options = {
-                method: 'POST',
-                body: speech,
-                headers: headers,
-            }
-
-            let result = await fetch(api, options)
-            .then(res => res.json());
-
-            return result;
-        }
+        setTimeout(checkWake, 20000); //check if wakeword has been used since joining
 
         connection.on('speaking', async (user, speaking) => {
-            console.log('Detecting Input');
-            const audio = connection.receiver.createStream(user, { mode: 'pcm' });
-            let wake = await SpeechRecognition(audio);
-            let entities = wake.entities;
-            if (!entities) return;
-            let value = Object.values(entities)[0];
-            if (!value) return;
-        
-            if (value[0].value == 'wakeword'){
-                console.log('awake!')
-                connection.removeListener('speaking', async (user, speaking) => {});
-                const orders = connection.play('./data/audio/captain/Awaiting orders, Captain.mp3');
-                orders.pause();
-                await orders.resume();
-                return message.reply('voice command succesfully executed!');
-            }  
-        }) */
+            if (gideon.vcmdexec == true) return; //disable speechrocgnition while voice command is running
+            if (speaking.has('SPEAKING')) {
+                console.log(`Scribe: listening to ${user.username}`);
+                console.log(`Scribe: SPEAKING ${speaking}`, speaking);
 
-        const dir = './data/audio/phrases';
-        await randomFile(dir, async (err, file) => {
-            let rfile = `${dir}/${file}`;
-            const dispatcher = connection.play(rfile);
-            dispatcher.pause();
-            dispatcher.resume();
+                const audio = connection.receiver.createStream(user, { mode: 'pcm' });
 
-            dispatcher.on('finish', async () => {
-                await message.reply('playback finished!');
-                dispatcher.destroy();
-                await message.guild.me.voice.channel.leave();
-            }); 
+                audio.on('end', () => {
+                    console.log(`Scribe: stopped listening to ${user.username}`);
+                });
+
+                let wake = await Util.SpeechRecognition(audio);
+
+                let entities = wake.entities;
+                if (!entities) return;
+                let intent = Object.values(entities)[0];
+                if (!intent) return;
+                let value = intent[0].value;
+                if (value == 'wakeword') awake = true;
+                await Util.VoiceResponse(value, connection, message, gideon); 
+            }
         }) 
     }
     catch (ex) {
