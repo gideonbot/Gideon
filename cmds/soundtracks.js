1ae3072ed3679a8d71f87ae9b722cd6afd26a642
adrifcastr <adrian.f.castro.mail@gmail.com>
2020-02-04 21:47:47 +0100

    make soundtracks.js embeds fields inline fields

--- cmds/soundtracks.js
+++ cmds/soundtracks.js
@@ -27,11 +27,11 @@ module.exports.run = async (gideon, message, args) => {
     const stracks = new Discord.MessageEmbed()
     .setColor('#2791D3')
     .setTitle('Arrowverse Soundtracks')
-    .addField('The Flash', `|[Season 1](${sturls[0]} '${sturls[0]}')|[Season 2](${sturls[1]} '${sturls[1]}')|[Season 3](${sturls[2]} '${sturls[2]}')|[Season 4](${sturls[3]} '${sturls[3]}')|`)
-    .addField('Arrow', `|[Season 1](${sturls[4]} '${sturls[4]}')|[Season 2](${sturls[5]} '${sturls[5]}')|[Season 3](${sturls[6]} '${sturls[6]}')|[Season 4](${sturls[7]} '${sturls[7]}')|[Season 5](${sturls[8]} '${sturls[8]}')|[Season 6](${sturls[9]} '${sturls[9]}')|`)
-    .addField('Supergirl', `|[Season 1](${sturls[10]} '${sturls[10]}')|[Season 2](${sturls[11]} '${sturls[11]}')|[Season 3](${sturls[12]} '${sturls[12]}')|`)
-    .addField('DC\'s Legends of Tomorrow', `|[Season 1](${sturls[13]} '${sturls[13]}')|[Season 2](${sturls[14]} '${sturls[14]}')|[Season 3](${sturls[15]} '${sturls[15]}')|`)
-    .addField('Crossovers', `[The Flash vs. Arrow: Music Selections from the Epic 2-Night Event](${sturls[16]} '${sturls[16]}')\n[The Flash – Music From the Special Episode: Duet](${sturls[17]} '${sturls[17]}')\n[Crisis on Earth-X (Original Television Score)](${sturls[18]} '${sturls[18]}')`)
+    .addField('The Flash', `[Season 1](${sturls[0]} '${sturls[0]}')\n[Season 2](${sturls[1]} '${sturls[1]}')\n[Season 3](${sturls[2]} '${sturls[2]}')\n[Season 4](${sturls[3]} '${sturls[3]}')`, true)
+    .addField('Arrow', `[Season 1](${sturls[4]} '${sturls[4]}')\n[Season 2](${sturls[5]} '${sturls[5]}')\n[Season 3](${sturls[6]} '${sturls[6]}')\n[Season 4](${sturls[7]} '${sturls[7]}')\n[Season 5](${sturls[8]} '${sturls[8]}')\n[Season 6](${sturls[9]} '${sturls[9]}')`, true)
+    .addField('Supergirl', `[Season 1](${sturls[10]} '${sturls[10]}')\n[Season 2](${sturls[11]} '${sturls[11]}')\n[Season 3](${sturls[12]} '${sturls[12]}')`, true)
+    .addField('DC\'s Legends of Tomorrow', `[Season 1](${sturls[13]} '${sturls[13]}')\n[Season 2](${sturls[14]} '${sturls[14]}')\n[Season 3](${sturls[15]} '${sturls[15]}')`, true)
+    .addField('Crossovers', `[The Flash vs. Arrow: Music Selections from the Epic 2-Night Event](${sturls[16]} '${sturls[16]}')\n[The Flash – Music From the Special Episode: Duet](${sturls[17]} '${sturls[17]}')\n[Crisis on Earth-X (Original Television Score)](${sturls[18]} '${sturls[18]}')`, true)
     .setTimestamp()
     .setFooter(Util.config.footer, gideon.user.avatarURL());
 
