e753de7a4c568dd4c7f397e1410d1038450ac6d9
adrifcastr <adrian.f.castro.mail@gmail.com>
2020-02-09 23:32:43 +0100

    add !android cmd

---
+++ cmds/android.js
@@ -0,0 +1,22 @@
+const Discord = module.require("discord.js");
+const Util = require("../Util");
+
+module.exports.run = async (gideon, message, args) => {
+    const android = new Discord.MessageEmbed()
+    .setColor('#2791D3')
+    .setTitle('Android Application:')
+    .setThumbnail('https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Android_logo_2019.svg/687px-Android_logo_2019.svg.png')
+    .setDescription(`You can find experimental Gideon Android™️ builds in:\n<#676186130764267572>\n\nMinimum SDK: \`API 24 (7.0 Oreo)\`\nDevelopment State: \`Pre-Alpha\`\n\nCurent functionality:`)
+    .addField('- Random Audio Playback:', 'Plays Gideon Voice™ audio files in random order')
+    .setTimestamp()
+    .setFooter(Util.config.footer, gideon.user.avatarURL());
+
+    message.channel.send(android);
+}
+
+module.exports.help = {
+    name: ["android", "app"],
+    type: "misc",
+    help_text: "android",
+    help_desc: "Displays Gideon Android info"
+}
