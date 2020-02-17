b747b6e408b38f68082c635d6fb3af79f1a249d1
adrifcastr <adrian.f.castro.mail@gmail.com>
2020-02-17 21:17:31 +0100

    add warning if 1000+ guilds detected

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ gideon.once('ready', async () => {
     
     console.log('Ready!');
     Util.log(`${gideon.user.tag} ready!\nServers:\n${gideon.guilds.cache.map(x => x.id + ' - `' + x.name + '`').join("\n")}`);
+    if (gideon.guilds.cache.size >= 1000) Util.log(`<@224617799434108928> <@351871113346809860>\n1000+ Guilds reached. Please refactor for sharding!`);
  /* let ids = gideon.guilds.cache.map(x => x.id);
 
     for (let i = 0; i < ids.length; i++) {
