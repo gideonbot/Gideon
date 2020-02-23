1cc4fe16f9909aeefe05b432610ad55bf8ec76e9
adrifcastr <adrian.f.castro.mail@gmail.com>
2020-02-23 17:39:58 +0100

    make soundtracks.js use our api

--- cmds/general/soundtracks.js
+++ cmds/general/soundtracks.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const fetch = require('node-fetch');
 const Util = require("../../Util");
 
 /**
@@ -7,34 +8,15 @@ const Util = require("../../Util");
  * @param {string[]} args
  */
 module.exports.run = async (gideon, message) => {
-    const sturls = [
-        'https://open.spotify.com/album/0xiJwfDcZRQ77bsbjS9TF5', 
-        'https://open.spotify.com/album/2EE7W74Rk1LDHzbYC9js2Z',
-        'https://open.spotify.com/album/0DCUAyJVmG1fNxkBaZqFLt', 
-        'https://open.spotify.com/album/12WS02J0JBAVeEGj6aJ7R8',
-        'https://open.spotify.com/album/0Hsit07149488kiRI1UiaJ', 
-        'https://open.spotify.com/album/234N1BIHMSxaKSVAM0JrOC', 
-        'https://open.spotify.com/album/2SYmYysAbloAQkkr2TEncT', 
-        'https://open.spotify.com/album/1IA3XNL6kv5zOpFG18M1au',
-        'https://open.spotify.com/album/7ivC8XKulyl3akjhiRtJjA', 
-        'https://open.spotify.com/album/2sT1ks3i5yn0ZAGGvZQv92', 
-        'https://open.spotify.com/album/0jSqj8Tr0BrQIdqV4DZ9jD', 
-        'https://open.spotify.com/album/7x4PDyJb3FJoYkRnF2hu2K',
-        'https://open.spotify.com/album/6zXns0k78HUh5UlNprKhvf',
-        'https://open.spotify.com/album/0ceTcs3rDUEiyzshxjJFdB', 
-        'https://open.spotify.com/album/4r6t4EyqVZskLi0jD9yOhU', 
-        'https://open.spotify.com/album/0FPZ6xZBcwsupOaxKULGAF',
-        'https://open.spotify.com/album/4AtHQFEn8Qrp9aSSt0ehob', 
-        'https://open.spotify.com/album/7vs9fu0ORNPmV2opBjLu5z', 
-        'https://open.spotify.com/album/1jFljSQGFqtKkFNURoEOeI'
-    ];
+    const api = 'https://gideonbot.co.vu/api/soundtracks';
+    const body = await fetch(api).then(res => res.json());
 
     const stracks = Util.CreateEmbed('Arrowverse Soundtracks')
-    .addField('The Flash', `[Season 1](${sturls[0]} '${sturls[0]}')\n[Season 2](${sturls[1]} '${sturls[1]}')\n[Season 3](${sturls[2]} '${sturls[2]}')\n[Season 4](${sturls[3]} '${sturls[3]}')`, true)
-    .addField('Arrow', `[Season 1](${sturls[4]} '${sturls[4]}')\n[Season 2](${sturls[5]} '${sturls[5]}')\n[Season 3](${sturls[6]} '${sturls[6]}')\n[Season 4](${sturls[7]} '${sturls[7]}')\n[Season 5](${sturls[8]} '${sturls[8]}')\n[Season 6](${sturls[9]} '${sturls[9]}')`, true)
-    .addField('Supergirl', `[Season 1](${sturls[10]} '${sturls[10]}')\n[Season 2](${sturls[11]} '${sturls[11]}')\n[Season 3](${sturls[12]} '${sturls[12]}')`, true)
-    .addField('DC\'s Legends of Tomorrow', `[Season 1](${sturls[13]} '${sturls[13]}')\n[Season 2](${sturls[14]} '${sturls[14]}')\n[Season 3](${sturls[15]} '${sturls[15]}')`, true)
-    .addField('Crossovers', `[The Flash vs. Arrow: Music Selections from the Epic 2-Night Event](${sturls[16]} '${sturls[16]}')\n[The Flash – Music From the Special Episode: Duet](${sturls[17]} '${sturls[17]}')\n[Crisis on Earth-X (Original Television Score)](${sturls[18]} '${sturls[18]}')`, true);
+    .addField('The Flash', `[Season 1](${body.flash.season1} '${body.flash.season1}')\n[Season 2](${body.flash.season2} '${body.flash.season2}')\n[Season 3](${body.flash.season3} '${body.flash.season3}')\n[Season 4](${body.flash.season4} '${body.flash.season4}')`, true)
+    .addField('Arrow', `[Season 1](${body.arrow.season1} '${body.arrow.season1}')\n[Season 2](${body.arrow.season2} '${body.arrow.season2}')\n[Season 3](${body.arrow.season3} '${body.arrow.season3}')\n[Season 4](${body.arrow.season4} '${body.arrow.season4}')\n[Season 5](${body.arrow.season5} '${body.arrow.season5}')\n[Season 6](${body.arrow.season6} '${body.arrow.season6}')`, true)
+    .addField('Supergirl', `[Season 1](${body.supergirl.season1} '${body.supergirl.season1}')\n[Season 2](${body.supergirl.season2} '${body.supergirl.season2}')\n[Season 3](${body.supergirl.season3} '${body.supergirl.season3}')`, true)
+    .addField('DC\'s Legends of Tomorrow', `[Season 1](${body.lot.season1} '${body.lot.season1}')\n[Season 2](${body.lot.season2} '${body.lot.season2}')\n[Season 3](${body.lot.season3} '${body.lot.season3}')`, true)
+    .addField('Crossovers', `[The Flash vs. Arrow: Music Selections from the Epic 2-Night Event](${body.crossovers.crossover1} '${body.crossovers.crossover1}')\n[The Flash – Music From the Special Episode: Duet](${body.crossovers.crossover2} '${body.crossovers.crossover2}')\n[Crisis on Earth-X (Original Television Score)](${body.crossovers.crossover3} '${body.crossovers.crossover3}')`, true);
     //peepee
     message.channel.send(stracks);  
 }
