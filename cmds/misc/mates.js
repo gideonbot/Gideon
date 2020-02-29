61da171ad88ac83e98dc9e4880c96da905ad67ac
adrifcastr <adrian.f.castro.mail@gmail.com>
2020-02-29 20:11:45 +0100

    updates mates.js a tiny bit, - still unusable

--- cmds/misc/mates.js
+++ cmds/misc/mates.js
@@ -1,5 +1,7 @@
 const Discord = require("discord.js");
 const fetch = require('node-fetch');
+const parsePodcast = require("@activediscourse/podcast-parser")
+const moment = require('moment');
 const Util = require("../../Util");
 
 /**
@@ -22,40 +24,118 @@ module.exports.run = async (gideon, message, args) => {
     if (gideon.listening.includes(message.author.id)) return message.channel.send(Util.CreateEmbed('A podcast episode is already playing!'));
     gideon.listening.push(message.author.id);
     
-    let epnum = args[0]
-    console.log(epnum);
-    const url = 'https://listennotes.com/';
+    let epnum = args[0];
     const emotes = ['⏯️','⏹️','⏮️','⏭️'];
 
     try {
-        async function GetCast(){
-            const api = `https://listen-api.listennotes.com/api/v2/search?q=${encodeURI(`"Episode ${epnum}"`)}&sort_by_date=0&type=episode&offset=0&published_after=0&only_in=title&language=English&ocid=0d0da5d93a484063ae92aef8b1be4c75&safe_mode=0`;
-            console.log(api);
-            const header = { 'X-ListenAPI-Key': process.env.PODCAST_API_KEY };
-            const body = await fetch(api, {headers: header}).then(res => res.json());
-            console.log(body);
-            return body.results[0];
-        }
+        const api = `https://matescast.com/rss/mates_archive.rss`;
+        const rss = await fetch(api).then(res => res.text());
+        const mates = await parsePodcast(rss).catch(ex => console.log(ex));
+        const episodes = mates.episodes.reverse();
+
+        let ep = episodes[epnum-1];
+        let title = mates.title;
+        let desc = mates.description.long;
+        let img = mates.image;
+        let eptitle = ep.title;
+        let epdesc = ep.rawDescription;
+        let stream = ep.enclosure.url;
+        let duration = moment.utc(ep.duration*1000).format('mm:ss');
+        let pubdate = moment.utc(ep.published).local().format('YYYY-MM-DD HH:mm:ss');
 
         let vcname = message.member.voice.channel.name;
         message.reply(`now joining voice channel: \`${vcname}\`!`);
         const connection = await message.member.voice.channel.join();
-        
-        const body = await GetCast();
-        const stream = body.audio;
 
-        const matesembed = Util.CreateEmbed(`Mike & Tom Eat Snacks ${body.title_original}`, {
-            description: body.description_original,
-            thumbnail: body.thumbnail,
+        const matesembed = Util.CreateEmbed(title, {
+            description: desc,
+            thumbnail: img,
             author: {
                 name: `Playing MATES for ${message.author.tag}:`,
                 icon: message.author.avatarURL()
             },
             fields: [
                 {
-                    name: `Powered by:`,
-                    value: `**[listennotes.com](${url} '${url}')**`
-                }
+                    name: `Currently playing:`,
+                    value: `\`${title} ${eptitle}\`\n\n\`\`\`\n${epdesc}\n\`\`\``
+                },
+                {
+                    name: `Episode duration:`,
+                    value: `\`${duration} min\``
+                },
+                {
+                    name: `Published at:`,
+                    value: `\`${pubdate}\``
+                },
+            ]
+        })
+
+        const stopembed = Util.CreateEmbed(title, {
+            description: desc,
+            thumbnail: img,
+            author: {
+                name: `Played MATES for ${message.author.tag}:`,
+                icon: message.author.avatarURL()
+            },
+            fields: [
+                {
+                    name: `Playback stopped:`,
+                    value: `\`${title} ${eptitle}\`\n\n\`\`\`\n${epdesc}\n\`\`\``
+                },
+                {
+                    name: `Episode duration:`,
+                    value: `\`${duration} min\``
+                },
+                {
+                    name: `Published at:`,
+                    value: `\`${pubdate}\``
+                },
+            ]
+        })
+
+        const pauseembed = Util.CreateEmbed(title, {
+            description: desc,
+            thumbnail: img,
+            author: {
+                name: `Playing MATES for ${message.author.tag}:`,
+                icon: message.author.avatarURL()
+            },
+            fields: [
+                {
+                    name: `Playback paused:`,
+                    value: `\`${title} ${eptitle}\`\n\n\`\`\`\n${epdesc}\n\`\`\``
+                },
+                {
+                    name: `Episode duration:`,
+                    value: `\`${duration} min\``
+                },
+                {
+                    name: `Published at:`,
+                    value: `\`${pubdate}\``
+                },
+            ]
+        })
+
+        const finishedembed = Util.CreateEmbed(title, {
+            description: desc,
+            thumbnail: img,
+            author: {
+                name: `Played MATES for ${message.author.tag}:`,
+                icon: message.author.avatarURL()
+            },
+            fields: [
+                {
+                    name: `Playback finished:`,
+                    value: `\`${title} ${eptitle}\`\n\n\`\`\`\n${epdesc}\n\`\`\``
+                },
+                {
+                    name: `Episode duration:`,
+                    value: `\`${duration} min\``
+                },
+                {
+                    name: `Published at:`,
+                    value: `\`${pubdate}\``
+                },
             ]
         })
 
@@ -69,120 +149,59 @@ module.exports.run = async (gideon, message, args) => {
         cast.resume();
 
         const rfilter = (reaction, user) => (emotes.includes(reaction.emoji.name) || emotes.includes(reaction.emoji.id)) && user.id === message.author.id;
-        const rcollector = sent.createReactionCollector(rfilter, {time: body.audio.audio_length_sec * 1000});
+        const rcollector = sent.createReactionCollector(rfilter, {time: ep.duration * 1000});
     
         rcollector.on('collect', async (reaction, user) => {
             if (reaction.emoji.name == emotes[0]) {
-                if (cast.paused) cast.resume(), message.reply('playback resumed!');
-                else cast.pause(), message.reply('playback paused!');
-                gideon.listening.remove(message.author.id);
+                if (cast.paused) cast.resume(), sent.edit(matesembed);
+                else cast.pause(), sent.edit(pauseembed);
+                sent.reactions.cache.find(x => x.emoji.name == emotes[0]).users.remove(user.id);
             }
 
             if (reaction.emoji.id == emotes[1]) {
                 cast.destroy();
+                gideon.listening.remove(message.author.id);
                 await sent.reactions.removeAll();
-                const stopembed = Util.CreateEmbed(`Mike & Tom Eat Snacks ${body.title_original}`, {
-                    description: body.description_original + '\n\n Playback stopped! :white_check_mark:',
-                    thumbnail: body.thumbnail,
-                    author: {
-                        name: `Played MATES for ${message.author.tag}:`,
-                        icon: message.author.avatarURL()
-                    },
-                    fields: [
-                        {
-                            name: `Powered by:`,
-                            value: `**[listennotes.com](${url} '${url}')**`
-                        }
-                    ]
-                })
                 return sent.edit(stopembed);
             }
 
             if (reaction.emoji.id == emotes[2]) {
                 cast.destroy();
                 epnum--;
-                const previousbody = await GetCast();
-                const previousstream = previousbody.audio;
 
-                const previousembed = Util.CreateEmbed(`Mike & Tom Eat Snacks ${previousbody.title_original}`, {
-                    description: previousbody.description_original,
-                    thumbnail: previousbody.thumbnail,
-                    author: {
-                        name: `Playing MATES for ${message.author.tag}:`,
-                        icon: message.author.avatarURL()
-                    },
-                    fields: [
-                        {
-                            name: `Powered by:`,
-                            value: `**[listennotes.com](${url} '${url}')**`
-                        }
-                    ]
-                })
-
-                cast = connection.play(previousstream);
+                cast = connection.play(stream);
                 cast.pause();
                 cast.resume();
 
-                sent.edit(previousembed);
-                gideon.listening.remove(message.author.id);
+                sent.reactions.cache.find(x => x.emoji.name == emotes[0]).users.remove(user.id);
+                sent.edit(matesembed);
             }
 
             if (reaction.emoji.id == emotes[2]) {
                 cast.destroy();
                 epnum++;
-                const nextbody = await GetCast();
-                const nextstream = nextbody.audio;
 
-                const nextembed = Util.CreateEmbed(`Mike & Tom Eat Snacks ${nextbody.title_original}`, {
-                    description: nextbody.description_original,
-                    thumbnail: nextbody.thumbnail,
-                    author: {
-                        name: `Playing MATES for ${message.author.tag}:`,
-                        icon: message.author.avatarURL()
-                    },
-                    fields: [
-                        {
-                            name: `Powered by:`,
-                            value: `**[listennotes.com](${url} '${url}')**`
-                        }
-                    ]
-                })
-
-                cast = connection.play(nextstream);
+                cast = connection.play(stream);
                 cast.pause();
                 cast.resume();
 
-                sent.edit(nextembed);
-                gideon.listening.remove(message.author.id);
+                sent.reactions.cache.find(x => x.emoji.name == emotes[0]).users.remove(user.id);
+                sent.edit(matesembed);
             }
         }); 
 
         cast.on('finish', async () => {
            cast.destroy();
            rcollector.stop();
-
-           const finishedembed = Util.CreateEmbed(`Mike & Tom Eat Snacks ${body.title_original}`, {
-            description: stripHtml(body.description + '\n\n Playback finished! :white_check_mark:'),
-            thumbnail: body.thumbnail,
-            author: {
-                name: `Played MATES for ${message.author.tag}:`,
-                icon: message.author.avatarURL()
-            },
-            fields: [
-                {
-                    name: `Powered by:`,
-                    value: `**[listennotes.com](${url} '${url}')**`
-                }
-            ]
-        })
-
-            sent.edit(finishedembed);
+           gideon.listening.remove(message.author.id);
+           sent.edit(finishedembed);
+           await sent.reactions.removeAll();
         }); 
-    }
+    } 
     
     catch (ex) {
-        console.log("Caught an exception while fetching API data: " + ex);
-        Util.log("Caught an exception while fetching API data: " + ex);
+        console.log("Caught an exception while playing podcast: " + ex);
+        Util.log("Caught an exception while playing podcast: " + ex);
         return message.channel.send(Util.CreateEmbed('An error occurred while fetching podcast data!'));
     }
 }
