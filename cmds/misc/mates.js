61a9e41851adf168120207fd205833832a5811e3
adrifcastr <adrian.f.castro.mail@gmail.com>
2020-03-01 17:34:58 +0100

    update mates.js, skipping still broken

--- cmds/misc/mates.js
+++ cmds/misc/mates.js
@@ -28,28 +28,34 @@ module.exports.run = async (gideon, message, args) => {
     const emotes = ['⏯️','⏹️','⏮️','⏭️'];
 
     try {
-        const api = `https://matescast.com/rss/mates_archive.rss`;
-        const rss = await fetch(api).then(res => res.text());
-        const mates = await parsePodcast(rss).catch(ex => console.log(ex));
-        const episodes = mates.episodes.reverse();
+        async function PodCast(num) {
+            const api = `https://matescast.com/rss/mates_archive.rss`;
+            const rss = await fetch(api).then(res => res.text());
+            const mates = await parsePodcast(rss).catch(ex => console.log(ex));
+            const episodes = mates.episodes.reverse();
 
-        let ep = episodes[epnum-1];
-        let title = mates.title;
-        let desc = mates.description.long;
-        let img = mates.image;
-        let eptitle = ep.title;
-        let epdesc = ep.rawDescription;
-        let stream = ep.enclosure.url;
-        let duration = moment.utc(ep.duration*1000).format('mm:ss');
-        let pubdate = moment.utc(ep.published).local().format('YYYY-MM-DD HH:mm:ss');
+            let ep = episodes[num-1];
+            let title = mates.title;
+            let desc = mates.description.long;
+            let img = mates.image;
+            let eptitle = ep.title;
+            let epdesc = ep.rawDescription;
+            let stream = ep.enclosure.url;
+            let duration = moment.utc(ep.duration*1000).format('mm:ss');
+            let pubdate = moment.utc(ep.published).local().format('YYYY-MM-DD HH:mm:ss');
+
+            return [title, desc, img, eptitle, epdesc, stream, duration, pubdate];
+        }
 
         let vcname = message.member.voice.channel.name;
         message.reply(`now joining voice channel: \`${vcname}\`!`);
         const connection = await message.member.voice.channel.join();
 
-        const matesembed = Util.CreateEmbed(title, {
-            description: desc,
-            thumbnail: img,
+        let matescast = await PodCast(epnum);
+
+        const matesembed = Util.CreateEmbed(matescast[0], {
+            description: matescast[1],
+            thumbnail: matescast[2],
             author: {
                 name: `Playing MATES for ${message.author.tag}:`,
                 icon: message.author.avatarURL()
@@ -57,22 +63,22 @@ module.exports.run = async (gideon, message, args) => {
             fields: [
                 {
                     name: `Currently playing:`,
-                    value: `\`${title} ${eptitle}\`\n\n\`\`\`\n${epdesc}\n\`\`\``
+                    value: `\`${matescast[0]} ${matescast[3]}\`\n\n\`\`\`\n${matescast[4]}\n\`\`\``
                 },
                 {
                     name: `Episode duration:`,
-                    value: `\`${duration} min\``
+                    value: `\`${matescast[6]} min\``
                 },
                 {
                     name: `Published at:`,
-                    value: `\`${pubdate}\``
+                    value: `\`${matescast[7]}\``
                 },
             ]
         })
 
-        const stopembed = Util.CreateEmbed(title, {
-            description: desc,
-            thumbnail: img,
+        const stopembed = Util.CreateEmbed(matescast[0], {
+            description: matescast[1],
+            thumbnail: matescast[2],
             author: {
                 name: `Played MATES for ${message.author.tag}:`,
                 icon: message.author.avatarURL()
@@ -80,22 +86,22 @@ module.exports.run = async (gideon, message, args) => {
             fields: [
                 {
                     name: `Playback stopped:`,
-                    value: `\`${title} ${eptitle}\`\n\n\`\`\`\n${epdesc}\n\`\`\``
+                    value: `\`${matescast[0]} ${matescast[3]}\`\n\n\`\`\`\n${matescast[4]}\n\`\`\``
                 },
                 {
                     name: `Episode duration:`,
-                    value: `\`${duration} min\``
+                    value: `\`${matescast[6]} min\``
                 },
                 {
                     name: `Published at:`,
-                    value: `\`${pubdate}\``
+                    value: `\`${matescast[7]}\``
                 },
             ]
         })
 
-        const pauseembed = Util.CreateEmbed(title, {
-            description: desc,
-            thumbnail: img,
+        const pauseembed = Util.CreateEmbed(matescast[0], {
+            description: matescast[1],
+            thumbnail: matescast[2],
             author: {
                 name: `Playing MATES for ${message.author.tag}:`,
                 icon: message.author.avatarURL()
@@ -103,22 +109,22 @@ module.exports.run = async (gideon, message, args) => {
             fields: [
                 {
                     name: `Playback paused:`,
-                    value: `\`${title} ${eptitle}\`\n\n\`\`\`\n${epdesc}\n\`\`\``
+                    value: `\`${matescast[0]} ${matescast[3]}\`\n\n\`\`\`\n${matescast[4]}\n\`\`\``
                 },
                 {
                     name: `Episode duration:`,
-                    value: `\`${duration} min\``
+                    value: `\`${matescast[6]} min\``
                 },
                 {
                     name: `Published at:`,
-                    value: `\`${pubdate}\``
+                    value: `\`${matescast[7]}\``
                 },
             ]
         })
 
-        const finishedembed = Util.CreateEmbed(title, {
-            description: desc,
-            thumbnail: img,
+        const finishedembed = Util.CreateEmbed(matescast[0], {
+            description: matescast[1],
+            thumbnail: matescast[2],
             author: {
                 name: `Played MATES for ${message.author.tag}:`,
                 icon: message.author.avatarURL()
@@ -126,15 +132,15 @@ module.exports.run = async (gideon, message, args) => {
             fields: [
                 {
                     name: `Playback finished:`,
-                    value: `\`${title} ${eptitle}\`\n\n\`\`\`\n${epdesc}\n\`\`\``
+                    value: `\`${matescast[0]} ${matescast[3]}\`\n\n\`\`\`\n${matescast[4]}\n\`\`\``
                 },
                 {
                     name: `Episode duration:`,
-                    value: `\`${duration} min\``
+                    value: `\`${matescast[6]} min\``
                 },
                 {
                     name: `Published at:`,
-                    value: `\`${pubdate}\``
+                    value: `\`${matescast[7]}\``
                 },
             ]
         })
@@ -144,55 +150,66 @@ module.exports.run = async (gideon, message, args) => {
             await sent.react(emoji).then(() => {}, failed => console.log("Failed to react with " + emoji + ": " + failed));
         }
 
-        let cast = connection.play(stream);
+        let cast = connection.play(matescast[5]);
         cast.pause();
         cast.resume();
 
         const rfilter = (reaction, user) => (emotes.includes(reaction.emoji.name) || emotes.includes(reaction.emoji.id)) && user.id === message.author.id;
-        const rcollector = sent.createReactionCollector(rfilter, {time: ep.duration * 1000});
+        const rcollector = sent.createReactionCollector(rfilter, {time: matescast[6] * 1000});
     
         rcollector.on('collect', async (reaction, user) => {
             if (reaction.emoji.name == emotes[0]) {
-                if (cast.paused) cast.resume(), sent.edit(matesembed);
-                else cast.pause(), sent.edit(pauseembed);
+                if (cast.paused) {
+                    cast.resume()
+                    sent.edit(matesembed)
+                }
+
+                else {
+                    cast.pause()
+                    sent.edit(pauseembed)
+                }
                 sent.reactions.cache.find(x => x.emoji.name == emotes[0]).users.remove(user.id);
             }
 
-            if (reaction.emoji.id == emotes[1]) {
-                cast.end();
+            if (reaction.emoji.name == emotes[1]) {
+                cast.destroy();
                 gideon.listening.remove(message.author.id);
                 await sent.reactions.removeAll();
                 await Util.LeaveVC(message);
                 return sent.edit(stopembed);
             }
 
-            if (reaction.emoji.id == emotes[2]) {
-                cast.end();
-                epnum--;
+            if (reaction.emoji.name == emotes[2]) {
+                cast.destroy();
+                epnum - 1;
 
-                cast = connection.play(stream);
+                matescast = await PodCast(epnum);
+
+                await sent.reactions.cache.find(x => x.emoji.name == emotes[0]).users.remove(user.id);
+                await sent.edit(matesembed);
+
+                cast = connection.play(matescast[5]);
                 cast.pause();
                 cast.resume();
-
-                sent.reactions.cache.find(x => x.emoji.name == emotes[0]).users.remove(user.id);
-                sent.edit(matesembed);
             }
 
-            if (reaction.emoji.id == emotes[2]) {
-                cast.end();
-                epnum++;
+            if (reaction.emoji.name == emotes[2]) {
+                cast.destroy();
+                epnum + 1;
 
-                cast = connection.play(stream);
+                matescast = await PodCast(epnum);
+
+                await sent.reactions.cache.find(x => x.emoji.name == emotes[0]).users.remove(user.id);
+                await sent.edit(matesembed);
+
+                cast = connection.play(matescast[5]);
                 cast.pause();
                 cast.resume();
-
-                sent.reactions.cache.find(x => x.emoji.name == emotes[0]).users.remove(user.id);
-                sent.edit(matesembed);
             }
         }); 
 
         cast.on('finish', async () => {
-           cast.end();
+           cast.destroy();
            rcollector.stop();
            gideon.listening.remove(message.author.id);
            sent.edit(finishedembed);
