b7d8798bc8e85e2545cf419a824e2142f2ac8ea6
adrifcastr <adrian.f.castro.mail@gmail.com>
2020-03-08 22:58:42 +0100

    add emojis.js

---
+++ cmds/misc/emojis.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const Util = require("../../Util");
+
+/**
+ * @param {Discord.Client} gideon
+ * @param {Discord.Message} message
+ * @param {string[]} args
+ */
+module.exports.run = async (gideon, message) => {     
+    const guild = message.guild;
+    const emojis = guild.emojis.cache.map(emojis => emojis.toString()).join(' ');
+
+    const embed = new Discord.MessageEmbed()
+    .setColor('#2791D3')
+    .addField('❯ Emojis:', emojis)
+    .addField('❯ Escaped Emojis:', `\`${emojis}\``)
+    .setFooter(Util.config.footer, Util.config.avatar)
+    message.channel.send(embed);
+}
+
+module.exports.help = {
+    name: ['emojis', 'emotes'],
+    type: "misc",
+    help_text: "guild",
+    help_desc: "Get current guild info"
+}
