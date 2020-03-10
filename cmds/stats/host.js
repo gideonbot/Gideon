b4e49bdd85df8e63608480e6e7bc06cc4222a5aa
adrifcastr <adrian.f.castro.mail@gmail.com>
2020-03-10 15:50:52 +0100

    add host.js

---
+++ cmds/stats/host.js
@@ -0,0 +1,61 @@
+const Discord = require("discord.js");
+const Util = require("../../Util");
+const si = require('systeminformation');
+
+/**
+ * @param {Discord.Client} gideon
+ * @param {Discord.Message} message
+ * @param {string[]} args
+ */
+module.exports.run = async (gideon, message) => {     
+    try {
+        let sent = await message.channel.send(Util.CreateEmbed('Gathering host info, please wait...'));
+
+        const cpu = await si.cpu().catch(console.error);
+        const curspeed = await si.cpuCurrentspeed().catch(console.error);
+        const curtemp = await si.cpuTemperature().catch(console.error);
+        const mem = await si.mem().catch(console.error);
+        const os = await si.osInfo().catch(console.error);
+        const load = await si.currentLoad().catch(console.error);
+        const net = await si.networkInterfaces().catch(console.error);
+
+        const embed = Util.CreateEmbed('Hosting server stats:', {
+            fields: [
+                {
+                    name: `❯ CPU:`,
+                    value: `CPU: \`${cpu.manufacturer} ${cpu.brand} @${cpu.speed} GHz\`\nCurrent Speed: \`${curspeed.avg} GHz\`\nCurrent Temperature: \`${curtemp.main === -1 ? 'Not available' : curtemp.main}\``
+                },
+                {
+                    name: `❯ Memory:`,
+                    value: `RAM Usage: \`${Math.round(mem.used/1e+6)} MB / ${Math.round(mem.total/1e+6)} MB\``
+                },
+                {
+                    name: `❯ OS:`,
+                    value: `OS: \`${os.platform}\`\nDistro: \`${os.distro}\`\nBuild: \`${os.build}\``
+                },
+                {
+                    name: `❯ Load:`,
+                    value: `Current CPU Load: \`${Math.round(load.currentload)} %\``
+                },
+                {
+                    name: `❯ Network:`,
+                    value: `Network Speed: \`${net[0].speed} Mbit/s\``
+                },
+            ]
+        })
+    
+        await sent.edit(embed);
+        
+    } catch (ex) {
+        console.log(ex);
+        Util.log("Caught an exception while running host.js: " + ex);
+        return message.channel.send(Util.CreateEmbed('An error occured while executing this command!'));
+    }
+}
+
+module.exports.help = {
+    name: ['host', 'server'],
+    type: "stats",
+    help_text: "host",
+    help_desc: "Hosting server stats"
+}
