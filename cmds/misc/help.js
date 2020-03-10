31c0a3061e61b71b741b61a76992f4a65d3fb1ca
adrifcastr <adrian.f.castro.mail@gmail.com>
2020-03-10 11:15:44 +0100

    add prefix syntax to help stuff

--- cmds/misc/help.js
+++ cmds/misc/help.js
@@ -14,6 +14,7 @@ module.exports.run = async (gideon, message, args) => {
 
     if (!args[0]) {
         const help = Util.CreateEmbed('__Use !help <module> to get a list of commands\nYou can check the list of available modules below:__')
+        .setDescription('Gideon\'s prefixes are: `!` | `Gideon` | `Gideon,` | <@595328879397437463>')
         .addField('general', 'General helpful Arrowverse commands')  
         .addField('fun', 'Fun and interactive Arrowverse commands')  
         .addField('admin', 'Commands for people with higher roles then the average Metahuman')  
@@ -27,7 +28,7 @@ module.exports.run = async (gideon, message, args) => {
 
     if (args[0].match(/(?:syntax)/i)) {
         const help = Util.CreateEmbed('__Command Syntax:__')
-        .setDescription('Arguments wrapped in `<>` are variables. _do not actually add brackets_\nArguments seperated by `/` mean `this or(/) this`.\nArguments wrapped in `[]` are optional arguments.\nCommands marked with :warning: are potentially dangerous.\nCommands marked with <:timevault:686676561298063361> are Time Vault only.\nCommands marked with <:gideon:686678560798146577> are application owner only.\nCommands marked with <:perms:686681300156940349> require certain permissions.\nCommands marked with `@role` require the mentioned role.')  
+        .setDescription('Gideon\'s prefixes are: `!` | `Gideon` | `Gideon,` | <@595328879397437463>\nArguments wrapped in `<>` are variables. _do not actually add brackets_\nArguments seperated by `/` mean `this or(/) this`.\nArguments wrapped in `[]` are optional arguments.\nCommands marked with :warning: are potentially dangerous.\nCommands marked with <:timevault:686676561298063361> are Time Vault only.\nCommands marked with <:gideon:686678560798146577> are application owner only.\nCommands marked with <:perms:686681300156940349> require certain permissions.\nCommands marked with `@role` require the mentioned role.')  
         .addField('Feature Suggestions:', `**[Click here to suggest a feature](${fsurl} 'Time Vault - #feature-suggestions')**`);
 
         return message.channel.send(help);
@@ -60,7 +61,7 @@ module.exports.run = async (gideon, message, args) => {
 
         for (let i = 0; i < arrs.length; i++) {
             const embed = Util.CreateEmbed('__List of available "' + type + '" commands below:__');
-            embed.setDescription('Use `!help syntax` for command syntax explanations')
+            embed.setDescription('Use `!help syntax` for command syntax explanations\nGideon\'s prefixes are: `!` | `Gideon` | `Gideon,` | <@595328879397437463>')
             for (item of arrs[i]) embed.addField(item[0].toLowerCase().startsWith("gideon") ? item : Util.config.prefixes[0] + item[0], item[1]);
             embed.addField('Feature Suggestions:', `**[Click here to suggest a feature](${fsurl} 'Time Vault - #feature-suggestions')**`);
             pages.push(embed);
@@ -77,7 +78,7 @@ module.exports.run = async (gideon, message, args) => {
 
     else {
         const embed = Util.CreateEmbed('__List of available "' + type + '" commands below:__');
-        embed.setDescription('Use `!help syntax` for command syntax explanations')
+        embed.setDescription('Use `!help syntax` for command syntax explanations\nGideon\'s prefixes are: `!` | `Gideon` | `Gideon,` | <@595328879397437463>')
         for (let item in commands) embed.addField(item[0].toLowerCase().startsWith("gideon") ? item : Util.config.prefixes[0] + item, commands[item]);
         embed.addField('Feature Suggestions:', `**[Click here to suggest a feature](${fsurl} 'Time Vault - #feature-suggestions')**`);
         message.channel.send(embed);
