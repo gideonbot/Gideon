b0a7ff610bbfec6e636ad8b136b75629c11bdddd
adrifcastr <adrifcastr@gmail.com>
2020-03-11 13:33:45 +0100

    phone commit - fix purge.js

--- cmds/admin/purge.js
+++ cmds/admin/purge.js
@@ -14,7 +14,7 @@ module.exports.run = async (gideon, message, args) => {
     const msgamt = args[0];
     if (!msgamt) return await Util.delay(200), message.channel.bulkDelete(2);
 
-    message.delete(200);
+    message.delete({ timeout: 200 });
     
     if (isNaN(msgamt)) return message.reply('you must supply a valid number!');
     if (msgamt > 100) return message.reply('max value is `100`.');
