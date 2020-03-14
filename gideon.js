8f50ba97a08e62cdeb123424a8732283c2937d3d
adrifcastr <adrian.f.castro.mail@gmail.com>
2020-03-14 17:34:35 +0100

    add welcome prompt (and threat)

--- gideon.js
+++ gideon.js
@@ -150,6 +150,15 @@ gideon.on("shardReady", (id, unavailableGuilds) => {
     else Util.log(`Shard \`${id}\` is connected!\n\nThe following guilds are unavailable due to network outage:\n${unavailableGuilds.map(x => x).join('\n')}`);
 });
 
+gideon.on("guildMemberAdd", member => {
+    if (member.guild.id !== '595318490240385037') return;
+    const logos = '<a:flash360:686326039525326946> <a:arrow360:686326029719306261> <a:supergirl360:686326042687832123> <a:constantine360:686328072529903645> <a:lot360:686328072198160445> <a:batwoman360:686326033783193631>';
+    const channel = gideon.guilds.cache.get('595318490240385037').channels.cache.get('595318490240385043');
+    const welcome = `\`Greatings Earth-Prime-ling\` ${member.user.toString()}\`!\`\n\`Welcome to the Time Vault\`<:timevault:686676561298063361>\`!\`\n\`If you want full server access make sure to read\` <#595935345598529546>\`!\`\n\`Ignoring this will probably get you kicked!\`\n${logos}`;
+    channel.send(welcome);
+    member.send(welcome).catch(ex => console.log(ex));
+});
+
 gideon.on("voiceStateUpdate", (oldState, newState) => {
     let newChannel = newState.channel;
     let oldChannel = oldState.channel;
