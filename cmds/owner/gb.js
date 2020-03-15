38a6ece29ed9f885ab395229f23ddc954f7132d0
adrifcastr <adrian.f.castro.mail@gmail.com>
2020-03-15 16:22:50 +0100

    migrate bl cmds to db

--- cmds/owner/gb.js
+++ cmds/owner/gb.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const Util = require("../../Util");
-const fs = require('fs');
 
 /**
  * @param {Discord.Client} gideon
@@ -21,37 +20,25 @@ module.exports.run = async (gideon, message, args) => {
     if (noid && !args[0].length >= 18) return message.channel.send(as);
 
     try {
-        const path = './data/JSON/guildblacklist.json';
-
-        if (!fs.existsSync(path)) {
-            fs.writeFileSync(path, JSON.stringify([]));
+        let gb = gideon.getGBL.get(args[0]);
+        if (!gb) {
+            gb = {
+                guild: args[0],
+                guildval: 0,
+            }
         }
 
-        let blacklist = JSON.parse(fs.readFileSync(path));
-
-        let command = message.content.toLowerCase().split(' ')[0];
-
-        if (command.endsWith('rm')) {
-            let pos = blacklist.indexOf(args[0]);
-            blacklist.splice(pos, 1); 
-            return message.reply(`guild \`${args[0]}\` has been un-blacklisted!`);                                        
-        }
-
-        else {
-            if (blacklist.map(x => x.guildid).includes(args[0])) return message.reply('you have already blacklisted this guild!');
-
-            let obj = {
-                guildid: args[0]
-            };
-            
-            blacklist.push(obj);
-            
-            fs.writeFileSync(path, JSON.stringify(blacklist, null, 2));
-
+        if (gb.guildval === 0) {
+            gb.guildval = 1;
+            gideon.setGBL.run(gb);
             message.reply(`guild \`${args[0]}\` has been blacklisted!`);
         }
 
-        
+        else {
+            gb.guildval = 0;
+            gideon.setGBL.run(gb);
+            message.reply(`guild \`${args[0]}\` has been un-blacklisted!`); 
+        }
     }
 
     catch (ex) {
