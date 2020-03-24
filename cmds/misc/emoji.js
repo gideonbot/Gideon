d42e6d29695631c4b3ce9f4c8b4d3583c7ddc655
adrifcastr <adrian.f.castro.mail@gmail.com>
2020-03-24 18:06:57 +0100

    add emoji.js

---
+++ cmds/misc/emoji.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+const Util = require("../../Util");
+
+/**
+ * @param {Discord.Client} gideon
+ * @param {Discord.Message} message
+ * @param {string[]} args
+ */
+module.exports.run = async (gideon, message, args) => {
+    const as = Util.CreateEmbed("You must supply valid input!");
+    if (!args[0]) return message.channel.send(as);  
+    if (!args[0].match(/<?(a)?:?(\w{2,32}):(\d{17,19})>?/)) return message.channel.send(as);  
+
+    const emoji = gideon.emojis.cache.get(args[0].match(/<?(a)?:?(\w{2,32}):(\d{17,19})>?/)[3])
+
+    const embed = Util.CreateEmbed(`Info about \`${emoji.name}\` (ID: \`${emoji.id}\`)`, {
+        thumbnail: emoji.url,
+        fields: [
+            {
+                name: `❯ Info`,
+                value: 
+                `• Identifier: \`${emoji}\`
+                 • Creation Date: \`${emoji.createdAt.toUTCString()}\`
+                 • URL: ${emoji.url}
+                `
+            }
+        ]
+    })
+
+    message.channel.send(embed);
+}
+
+module.exports.help = {
+    name: ["emoji", "emote"],
+    type: "misc",
+    help_text: "emoji <emoji>",
+    help_desc: "Displays a GuildEmoji's info"
+}
