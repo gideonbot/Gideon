ec30eab34987443b0fb014a9dac987a20b553b64
adrifcastr <adrian.f.castro.mail@gmail.com>
2020-03-28 21:48:24 +0100

    add type.js

---
+++ cmds/owner/type.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+
+/**
+ * @param {Discord.Client} gideon
+ * @param {Discord.Message} message
+ * @param {string[]} args
+ */
+module.exports.run = async (gideon, message) => {
+    if (gideon.user.typingIn(message.channel)) message.channel.stopTyping(true);
+    else message.channel.startTyping();
+}
+
+module.exports.help = {
+    name: ["type", "typing"],
+    type: "owner",
+    help_text: "type <:gideon:686678560798146577>",
+    help_desc: "Toggles typing",
+    owner: true,
+    voice: false,
+    timevault: false,
+    roles: [],
+    user_perms: [],
+    bot_perms: []
+}
