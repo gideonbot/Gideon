d0bac1327b0e6cf0c5fe890d8dbe991f11dfab7e
adrifcastr <adrian.f.castro.mail@gmail.com>
2020-05-15 20:04:12 +0200

    port eval

---
+++ commands/owner/eval.js
@@ -0,0 +1,50 @@
+import Discord from 'discord.js';
+import Util from '../../Util.js';
+import Akairo from 'discord-akairo';
+const Command = Akairo.Command;
+
+/**
+ * @param {Discord.Client} gideon
+ * @param {Discord.Message} message
+ * @param {string[]} args
+ */
+
+class Eval extends Command {
+    constructor() {
+        super('eval', {
+            aliases: ['eval', 'exec'],
+            category: 'owner',
+            ownerOnly: true,
+            args: [ { id: 'code' } ],
+        });
+    }
+
+    async exec(message, args) {
+        try {
+            const code = args.code;
+            // eslint-disable-next-line semi
+            const returnedValue = await eval('(async () => { return ' + code + '})()').catch(e => message.channel.send(Util.CreateEmbed('An error occured while processing your request:', {description: '```\n' + Util.truncate(e.stack, 400, true) + '```'}, message.member)));
+    
+            if (typeof returnedValue === 'undefined') {
+                message.channel.send('The evaluated code returned nothing.');
+                return;
+            }
+    
+            let printValue = '';
+    
+            if (typeof returnedValue === 'string') printValue = returnedValue;
+            else if (typeof returnedValue === 'object') printValue = JSON.stringify(returnedValue, null, 2);
+            else printValue = new String(returnedValue);
+    
+            if (printValue == '{}') return;
+    
+            message.channel.send(Util.truncate(printValue, 1900, true), {
+                code: true
+            });
+        } catch (e) {
+            return message.channel.send(Util.CreateEmbed('An error occured while processing your request:', {description: '```\n' + Util.truncate(e.stack, 400, true) + '```'}, message.member));
+        }
+    }
+}
+
+export default Eval;
