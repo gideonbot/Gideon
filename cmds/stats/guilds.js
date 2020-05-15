f8a92bdd95692f792e91459c0b1044f75b4d1f0c
MBR <buildmachineubuntu@gmail.com>
2020-05-15 19:47:13 +0200

    guilds cleanup

--- cmds/stats/guilds.js
+++ cmds/stats/guilds.js
@@ -8,12 +8,11 @@ export async function run(message) {
     try {
         const embed = Util.CreateEmbed('Guilds:', null, message.member);
 
-        let shard = process.gideon.shard ? await process.gideon.shard.broadcastEval('this.shard.ids').then(results => {return results;}).catch(console.error) : [0];
-        let guilds = process.gideon.shard ? await process.gideon.shard.broadcastEval('this.guilds.cache.size').then(results => {return results;}).catch(console.error) : [process.gideon.guilds.cache.size];
+        let cache = process.gideon.guilds.cache;
+        
+        let g = `\`${cache.size}\` guilds on shard: \`${cache.first().shardID}\``;
 
-        let g = [`\`${guilds[0]}\` guilds on shard: \`${shard[0]}\``];
-
-        embed.setDescription(g.join('\n'));
+        embed.setDescription(g);
         message.channel.send(embed);
     }
     catch (ex) {
