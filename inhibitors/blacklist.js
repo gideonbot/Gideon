bfef9a169a3d51cb28518fd8012ae8f00c0f2fe3
adrifcastr <adrian.f.castro.mail@gmail.com>
2020-05-16 13:44:53 +0200

    add blacklist inhibitor

---
+++ inhibitors/blacklist.js
@@ -0,0 +1,18 @@
+import Akairo from 'discord-akairo';
+const Inhibitor = Akairo.Inhibitor;
+
+class Blacklist extends Inhibitor {
+    constructor() {
+        super('blacklist', {
+            reason: 'blacklist'
+        });
+    }
+
+    exec(message) {
+        const ubl = this.client.getUser.get(message.author.id);
+        if (!ubl || !ubl.blacklist) return null;
+        return ubl.blacklist === 1;
+    }
+}
+
+export default Blacklist;
