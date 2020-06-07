8c1c983c8ac5359e7de835454033b87ef28b862e
adrifcastr <adrifcastr@gmail.com>
2020-06-07 19:35:10 +0200

    add smallville and titans wiki

--- cmds/general/wiki.js
+++ cmds/general/wiki.js
@@ -31,6 +31,14 @@ export async function run(message, args) {
             url: 'doompatrol.fandom.com',
             title: 'Doom Patrol'
         },
+        {
+            url: 'titans.fandom.com',
+            title: 'Doom Patrol'
+        },
+        {
+            url: 'smallville.fandom.com',
+            title: 'Doom Patrol'
+        },
     ];
     
     let wiki = wikis[-1];
@@ -43,6 +51,8 @@ export async function run(message, args) {
     else if (command.endsWith('kr')) wiki = wikis[3];
     else if (command.endsWith('lu')) wiki = wikis[4];
     else if (command.endsWith('dp')) wiki = wikis[5];
+    else if (command.endsWith('t')) wiki = wikis[6];
+    else if (command.endsWith('sv')) wiki = wikis[7];
     else return message.channel.send('Supply a valid Wiki!');
 
     let search_term = args.join(' ');
@@ -60,8 +70,10 @@ export async function run(message, args) {
 
         const body = await Util.fetchJSON(api);
 
-        //stargirl, doom patrol & krypton do some weird stuff, therefore the actual result is the 2nd element
-        const article = Object.values(body.items)[wikis.indexOf(wiki) == 1 || wikis.indexOf(wiki) == 3 ? 1 : 0 || wikis.indexOf(wiki) == 5 ? 1 : 0];
+        //new wikis do some weird stuff, therefore the actual result is the 2nd element
+        const article = Object.values(body.items)[wikis.indexOf(wiki) == 1 ||
+             wikis.indexOf(wiki) == 3 ? 1 : 0 || wikis.indexOf(wiki) == 5 ? 1 : 0 || 
+             wikis.indexOf(wiki) == 6 ? 1 : 0];
 
         if (Object.keys(body.items).length < 1) return message.channel.send(Util.CreateEmbed(`There was no result for ${search_term} on the ${wiki.title} Wiki!`, null, message.member));
         
@@ -87,7 +99,7 @@ export async function run(message, args) {
 }
 
 export const help = {
-    name: ['wiki', 'wikistg', 'wikidc', 'wikikr', 'wikilu', 'wikidp'],
+    name: ['wiki', 'wikistg', 'wikidc', 'wikikr', 'wikilu', 'wikidp', 'wikit', 'wikisv'],
     type: 'general',
     help_text: 'wiki[stg|kr|lu|dc] <term>',
     help_desc: 'Searches the specified wiki for the given term | stg - Stargirl | kr - Krypton | lu - Lucifer | dc - DC | dp - Doom Patrol',
