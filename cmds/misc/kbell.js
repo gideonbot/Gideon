68e1897c992fad793d9d5fae9136161bb824974f
adrifcastr <adrifcastr@gmail.com>
2020-06-30 18:04:29 +0200

    MORE KBELL

--- cmds/misc/kbell.js
+++ cmds/misc/kbell.js
@@ -12,7 +12,14 @@ export async function run(message) {
         'https://cdn.discordapp.com/attachments/727230121358000188/727232391399342150/thanks_babe.mp4',
         'https://cdn.discordapp.com/attachments/727230121358000188/727232815577694238/Waiting_For_A_Sloth.mp4',
         'https://cdn.discordapp.com/attachments/727230121358000188/727232928412598332/were_almost_french-kissing.mp4',
-        'https://cdn.discordapp.com/attachments/727230121358000188/727233239533486170/ya_basic.mp4'
+        'https://cdn.discordapp.com/attachments/727230121358000188/727233239533486170/ya_basic.mp4',
+        'https://cdn.discordapp.com/attachments/727230121358000188/727538108152807508/Sexy_skyscraper.mp4',
+        'https://cdn.discordapp.com/attachments/727230121358000188/727538387241926726/I_have_a_vagina.mp4',
+        'https://cdn.discordapp.com/attachments/727230121358000188/727538568263893002/Ive_had_sex.mp4',
+        'https://cdn.discordapp.com/attachments/727230121358000188/727539028332773479/Its_Annas_movie.mp4',
+        'https://cdn.discordapp.com/attachments/727230121358000188/727543784484896788/Love_Ballad_To_Your_Therapist.mp4',
+        'https://cdn.discordapp.com/attachments/727230121358000188/727548985153159230/Some_Things_Never_Change.mp4',
+        'https://cdn.discordapp.com/attachments/727230121358000188/727549971293012038/Tell_Me_How_Long_Music_Video.webm'
     ];
 
     message.channel.send(clips[Math.floor(Math.random() * clips.length)]);
