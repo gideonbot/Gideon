240d23e2d399a28d99dc87ae7617a0260135af65
adrifcastr <adrifcastr@gmail.com>
2020-12-07 15:12:45 +0100

    fix wrong repo push

--- cmds/misc/mdn.js
+++ cmds/misc/mdn.js
@@ -4,17 +4,12 @@ import Util from '../../Util.js';
  * @param {Discord.Message} message
  */
 export async function run(message, args) {
-   /* const searchtearm = encodeURIComponent(args.join(''));
+    const searchtearm = encodeURIComponent(args.join(''));
     const embed = await Util.fetchJSON('https://mdn.gideonbot.com/embed?q=' + searchtearm).catch(ex => Util.log(ex));
     if (embed.code === 404) return message.reply('There was no result for `' + args.join('') + '`.');
-    message.channel.send({ embed: embed });*/
-
-    let string = '[enumerable](/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/propertyIsEnumerable) [own properties](/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/hasOwnProperty)';
-    for (const match of string.matchAll(/\[(.*?)\]\((.*?)\)/g)) {
-        string = string.replace(match[2], 'https://developer.mozilla.org' + match[2]);
-    }
-    console.log(string);
+    message.channel.send({ embed: embed });
 }
+
 export const help = {
     name: 'mdn',
     type: 'misc',
