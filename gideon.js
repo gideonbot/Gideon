3cfe7594a908ebead4309ea4fc3c3c1366a3a0f4
adrifcastr <adrifcastr@gmail.com>
2020-12-16 19:05:20 +0100

    fixi fix

--- gideon.js
+++ gideon.js
@@ -361,7 +361,3 @@ gideon.on('commandRefused', (interaction, reason) => {
     if (process.env.CI) return;
     Util.log(`Command Refused:\n\n${interaction.member.user?.tag} attempted to use \`${interaction?.commandName}\`\nCommand failed due to: \`${reason}\`\nOrigin: \`#${interaction?.channel?.name}\` at \`${interaction?.guild?.name}\``);
 });
-
-gideon.on('voiceStateUpdate', (oldState, newState) => {
-    Util.Checks.VCCheck(oldState, newState);
-});
