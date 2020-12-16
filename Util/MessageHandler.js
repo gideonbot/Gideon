614ae01e6c3160e0caae826ec37df922b6f9f40d
Adrian Castro <adrifcastr@gmail.com>
2020-12-16 22:51:41 +0100

    Fix typo

--- Util/MessageHandler.js
+++ Util/MessageHandler.js
@@ -56,7 +56,7 @@ class MsgHandler {
         Util.Checks.CSD(message, Util); //eastereggs
  
         const usedCustom = lowercaseContent.startsWith(currentguild.prefix.toLowerCase());
-        if (usedCustom) return message.reply('This usage is deprecated.\nPlease use the slash commands that are built-in to the Discord client.\nType `/` in chat to get started.\nPlease update your Discord client if you do not see the slash comands UI.\nAdditionally for slash commands to work please authorize the `applications.commands` scope via this oauth url:\n<https://discord.com/oauth2/authorize?client_id=595328879397437463&permissions=37088321&scope=bot&scope=applications.commands>\nIt may take up to an hour for the slash commands to show up.\nIf you require assistance, please join the Time Vault\nhttps://discord.gg/h9SEQaU\nhttps://i.imgur.com/sBnNfkg.gif');
+        if (usedCustom) return message.reply('This usage is deprecated.\nPlease use the slash commands that are built-in to the Discord client.\nType `/` in chat to get started.\nPlease update your Discord client if you do not see the slash commands UI.\nAdditionally for slash commands to work please authorize the `applications.commands` scope via this oauth url:\n<https://discord.com/oauth2/authorize?client_id=595328879397437463&permissions=37088321&scope=bot&scope=applications.commands>\nIt may take up to an hour for the slash commands to show up.\nIf you require assistance, please join the Time Vault\nhttps://discord.gg/h9SEQaU\nhttps://i.imgur.com/sBnNfkg.gif');
     }
 }
 
