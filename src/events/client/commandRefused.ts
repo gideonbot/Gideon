e17f59ecba54a888ebb16ffd5f14ea034d729d72
adrifcastr <adrifcastr@gmail.com>
2021-03-24 17:50:20 +0100

    one more event

---
+++ src/events/client/commandRefused.ts
@@ -0,0 +1,11 @@
+import Util from '../../Util.js';
+import { Interaction } from 'discord.js';
+
+export default {
+    name: 'commandRefused',
+    async run(interaction: Interaction, reason: string): Promise<void> {
+        if (process.env.CI) return;
+        // @ts-expect-error do this until interaction typings are done
+        Util.log(`Command Refused:\n\n${interaction?.member?.user?.tag} attempted to use \`${interaction?.commandName}\`\nCommand failed due to: \`${reason}\`\nOrigin: \`#${interaction?.channel?.name}\` at \`${interaction?.guild?.name}\``);
+    }
+};
