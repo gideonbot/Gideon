595a211669d8b5a7f6d74a9c6e93649752145169
adrifcastr <adrifcastr@gmail.com>
2021-04-11 18:37:53 +0200

    finish subs.ts

---
+++ src/@types/open-subtitles-api.d.ts
@@ -0,0 +1,36 @@
+declare module 'opensubtitles-api' {
+	interface OSInterface {
+        useragent: string;
+        username: string;
+        password: string;
+        ssl: boolean;
+    }
+
+	interface SubResponse {
+        [key: string]: {
+			downloads: string;
+			encoding: string;
+			id: string;
+			lang: string;
+			langName: string;
+			score: number;
+			url: string;
+			filename: string;
+		}[]
+	}
+
+	interface OSSearch {
+        sublanguageid: string;       
+        season: number;
+        episode: number;
+        limit: string;                
+        imdbid: string;           
+    }
+
+	export class OS {
+		constructor(data: OSInterface);
+		search(data: OSSearch): Promise<SubResponse>;
+	}
+
+	export default OS
+}
--- src/cmds/global/subs.ts
+++ src/cmds/global/subs.ts
@@ -90,40 +90,40 @@ export async function run(interaction: CommandInteraction, args: CommandInteract
 
     let show = shows[-1];
 
-    if (args[0].options[0].value === 'show_fl') show = shows[0];
-    else if (args[0].options[0].value === 'show_ar') show = shows[1];
-    else if (args[0].options[0].value === 'show_sg') show = shows[2];
-    else if (args[0].options[0].value === 'show_lot') show = shows[3];
-    else if (args[0].options[0].value === 'show_co') show = shows[4];
-    else if (args[0].options[0].value === 'show_bw') show = shows[5];
-    else if (args[0].options[0].value === 'show_bl') show = shows[6];
-    else if (args[0].options[0].value === 'show_kr') show = shows[8];
-    else if (args[0].options[0].value === 'show_lu') show = shows[9];
-    else if (args[0].options[0].value === 'show_sl') show = shows[10];
-    else if (args[0].options[0].value === 'show_stg') show = shows[11];
-    else if (args[0].options[0].value === 'show_dp') show = shows[12];
-    else if (args[0].options[0].value === 'show_t') show = shows[13];
-    else if (args[0].options[0].value === 'show_sv') show = shows[14];
-    else if (args[0].options[0].value === 'show_tb') show = shows[15];
+    if (args[0].options?.[0].value === 'show_fl') show = shows[0];
+    else if (args[0].options?.[0].value === 'show_ar') show = shows[1];
+    else if (args[0].options?.[0].value === 'show_sg') show = shows[2];
+    else if (args[0].options?.[0].value === 'show_lot') show = shows[3];
+    else if (args[0].options?.[0].value === 'show_co') show = shows[4];
+    else if (args[0].options?.[0].value === 'show_bw') show = shows[5];
+    else if (args[0].options?.[0].value === 'show_bl') show = shows[6];
+    else if (args[0].options?.[0].value === 'show_kr') show = shows[8];
+    else if (args[0].options?.[0].value === 'show_lu') show = shows[9];
+    else if (args[0].options?.[0].value === 'show_sl') show = shows[10];
+    else if (args[0].options?.[0].value === 'show_stg') show = shows[11];
+    else if (args[0].options?.[0].value === 'show_dp') show = shows[12];
+    else if (args[0].options?.[0].value === 'show_t') show = shows[13];
+    else if (args[0].options?.[0].value === 'show_sv') show = shows[14];
+    else if (args[0].options?.[0].value === 'show_tb') show = shows[15];
     
     OS.search({
-        sublanguageid: args[0].options[1].value,       
-        season: args[0].options[2].value,
-        episode: args[0].options[3].value,
+        sublanguageid: args[0].options?.[1].value as string,       
+        season: args[0].options?.[2].value as number,
+        episode: args[0].options?.[3].value as number,
         limit: '5',                 
         imdbid: show.id,           
 
     }).then(subtitles => {
-        const embed = Util.Embed(`Subtitles for: ${show.title} ${args[0].options[2].value}x${Util.normalize(args[0].options[3].value)}`, {description: 'Here are the 5 best results from opensubtitles.org:'}, interaction.member);
+        const embed = Util.Embed(`Subtitles for: ${show.title} ${args[0].options?.[2].value}x${Util.normalize(args[0].options?.[3].value as number)}`, {description: 'Here are the 5 best results from opensubtitles.org:'}, interaction.member as GuildMember);
 
         for (let sub of Object.values(subtitles)[0]) {
             embed.addField(sub.filename, `**[Download SRT](${sub.url} '${sub.url}')** Lang: \`${sub.lang}\` Score: \`${sub.score}\``);
         }
         
         interaction.reply(embed);
-    }).catch(async err => {
+    }).catch(async (err: Error) => {
         Util.log('An error occurred while trying to fetch subtitles: ' + err);
-        return interaction.reply('There were no results for this episode on opensubtitles.org!\nTry another episode or another language !', { ephemeral: true, hideSource: true });
+        return interaction.reply('There were no results for this episode on opensubtitles.org!\nTry another episode or another language !', { ephemeral: true });
     });
 }
 
