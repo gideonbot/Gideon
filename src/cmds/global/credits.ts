9d7cd29e011a079c22eba378277b67d41d947c59
adrifcastr <adrifcastr@gmail.com>
2021-04-11 21:39:07 +0200

    minor fixes

--- src/cmds/global/credits.ts
+++ src/cmds/global/credits.ts
@@ -8,15 +8,15 @@ import Util from '../../Util.js';
 export async function run(interaction: CommandInteraction): Promise<void> {
 
     const fields = [
-        {name: 'adrifcastr', value: 'Development, maintenance & domain'},
-        {name: 'MBR#0001', value: 'Development, support, & hosting'},
-        {name: 'Klaus#5857', value: 'Website development'},
-        {name: 'Stevenson Johnson', value: 'Artwork'},
-        {name: 'AceFire6', value: 'Development & hosting of [arrowverse.info](https://arrowverse.info) and its [API](https://arrowverse.info/api)'},
-        {name: '7coil', value: 'PR [#24](https://github.com/adrifcastr/Gideon/pull/24) and [#25](https://github.com/adrifcastr/Gideon/pull/25)'}
+        { name: 'adrifcastr', value: 'Development, maintenance & domain' },
+        { name: 'MBR#0001', value: 'Development, support, & hosting' },
+        { name: 'Klaus#5857', value: 'Website development' },
+        { name: 'Stevenson Johnson', value: 'Artwork' },
+        { name: 'AceFire6', value: 'Development & hosting of [arrowverse.info](https://arrowverse.info) and its [API](https://arrowverse.info/api)' },
+        { name: '7coil', value: 'PR [#24](https://github.com/adrifcastr/Gideon/pull/24) and [#25](https://github.com/adrifcastr/Gideon/pull/25)' }
     ];
 
-    return interaction.reply(Util.Embed('Development Credits:', {fields: fields, thumbnail: process.gideon.user?.avatarURL() as string}, interaction.member as GuildMember));
+    return interaction.reply(Util.Embed('Development Credits:', { fields: fields, thumbnail: process.gideon.user?.avatarURL() as string }, interaction.member as GuildMember));
 }
 
 export let help: Command['help'] = {
