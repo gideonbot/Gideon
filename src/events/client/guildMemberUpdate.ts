bd9560654880989d7f8b82c5c97f0f0f0b1ba978
adrifcastr <adrifcastr@gmail.com>
2021-04-12 22:00:31 +0200

    fix big dum dum

--- src/events/client/guildMemberUpdate.ts
+++ src/events/client/guildMemberUpdate.ts
@@ -4,6 +4,7 @@ import { GuildMember } from 'discord.js';
 export default {
     name: 'guildMemberUpdate',
     async run(oldMember: GuildMember, newMember: GuildMember): Promise<void> {
+        if (oldMember.guild.id  !== '595318490240385037' && newMember.guild.id !== '595318490240385037') return;
         if (newMember.nickname !== oldMember.nickname) Util.Checks.NameCheck(newMember, null);
         if (newMember.user.username !== oldMember.user.username) Util.Checks.NameCheck(null, newMember.user);
         if (oldMember.pending && !newMember.pending && !newMember.roles.cache.has('688430418466177082')) await newMember.roles.add('688430418466177082');
