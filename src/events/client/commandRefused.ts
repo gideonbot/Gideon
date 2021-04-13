b631ec6159b0437cdf509900e2781908d57df539
adrifcastr <adrifcastr@gmail.com>
2021-04-13 18:40:19 +0200

    cleaner logging

--- src/events/client/commandRefused.ts
+++ src/events/client/commandRefused.ts
@@ -6,6 +6,6 @@ export default {
     async run(interaction: Interaction, reason: string): Promise<void> {
         if (process.env.CI) return;
         // @ts-expect-error do this until interaction typings are done
-        Util.log(`Command Refused:\n\n${interaction?.member?.user?.tag} attempted to use \`${interaction?.commandName}\`\nCommand failed due to: \`${reason}\`\nOrigin: \`#${interaction?.channel?.name}\` at \`${interaction?.guild?.name}\``);
+        Util.log(`Command Refused:\n\n\`${interaction?.member?.user?.tag}\` attempted to use \`${interaction?.commandName}\`\nCommand failed due to: \`${reason}\`\nOrigin: \`#${interaction?.channel?.name}\` at \`${interaction?.guild?.name}\``);
     }
 };
