4fea04678bbc552c7704d43e86bb8f7e5b4b2445
adrifcastr <adrifcastr@gmail.com>
2021-04-14 15:31:04 +0200

    fix stats

--- src/cmds/global/stats.ts
+++ src/cmds/global/stats.ts
@@ -9,16 +9,10 @@ export async function run(interaction: CommandInteraction): Promise<void> {
     const cmds = process.gideon.getStat.get('commands_ran').value + 1;
     const msgs = process.gideon.getStat.get('messages_sent').value + 1;
     const aimsgs =  process.gideon.getStat.get('ai_chat_messages_processed').value;
-    let guilds = process.gideon.shard ? await process.gideon.shard.fetchClientValues('guilds.cache').catch(ex => console.log(ex)) : [process.gideon.guilds.cache];
-
-    if (guilds) {
-        guilds = [].concat.apply([], guilds);
-    }
-    
-    const users = (guilds as unknown as Collection<string, Guild>).reduce((r, d) => r + d.memberCount, 0);
+    const users = process.gideon.guilds.cache.reduce((r, d) => r + d.memberCount, 0);
 
     return interaction.reply(Util.Embed('Gideon\'s stats:', 
-        {description: `Total guilds: \`${(guilds as unknown as string[]).length}\`\nTotal users: \`${users.toLocaleString('de-DE')}\`\nUsed commands: \`${cmds.toLocaleString('de-DE')}\`\nMessages sent: \`${msgs.toLocaleString('de-DE')}\`\nAI chat messages: \`${aimsgs.toLocaleString('de-DE')}\``
+        {description: `Total guilds: \`${process.gideon.guilds.cache.size}\`\nTotal users: \`${users.toLocaleString('de-DE')}\`\nUsed commands: \`${cmds.toLocaleString('de-DE')}\`\nMessages sent: \`${msgs.toLocaleString('de-DE')}\`\nAI chat messages: \`${aimsgs.toLocaleString('de-DE')}\``
         }, interaction.member as GuildMember)
     );  
 };
