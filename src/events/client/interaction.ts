7d3c3c99032b99c5ab03ed28dba02cbdfca65786
adrifcastr <adrifcastr@gmail.com>
2021-04-26 19:45:36 +0200

    alot of renaming

--- src/events/client/interaction.ts
+++ src/events/client/interaction.ts
@@ -4,6 +4,7 @@ import { Interaction } from 'discord.js';
 export default {
     name: 'interaction',
     async run(interaction: Interaction): Promise<void> {
+        console.log(interaction);
         if (!interaction.isCommand()) return;
         if (!interaction.guild) return interaction.reply('DM commands are not supported!', { ephemeral: true });
         Util.Interactions.SlashCommands(interaction);
