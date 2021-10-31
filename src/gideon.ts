744c39c8681e7937cc6c1a558cdce1585ffa63ea
adrifcastr <adrifcastr@gmail.com>
2021-10-31 16:45:21 +0100

    remove more bs

--- src/gideon.ts
+++ src/gideon.ts
@@ -67,10 +67,3 @@ Util.LoadEvents().then(() => {
         process.exit(1);
     }
 });
-
-setTimeout(() => {
-    if (process.env.CI) {
-        console.log('Exiting because CI was detected but cycle was not complete!');
-        process.exit(1);
-    }
-}, 120e3);
