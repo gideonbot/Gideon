8984e57b9dbc701cf15eec803a15f40d47ed0b60
adrifcastr <adrifcastr@gmail.com>
2021-10-31 15:58:11 +0100

    one more fix

--- src/events/client/interactionCreate.ts
+++ src/events/client/interactionCreate.ts
@@ -2,7 +2,7 @@ import Util from '../../Util.js';
 import { Interaction } from 'discord.js';
 
 export default {
-    name: 'interaction',
+    name: 'interactionCreate',
     async run(interaction: Interaction): Promise<void> {
         if (interaction.isCommand()) {
             if (!interaction.guild) return interaction.reply({ content: 'DM commands are not supported!', ephemeral: true });
